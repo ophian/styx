cf6e05a9c2552375b80a000c7f78e046438ba5d5
Ian <aristophian@googlemail.com>
2016-06-13 10:45:58 +0200

    remove lang from exclude files

--- lang/plugin_lang.php
+++ lang/plugin_lang.php
@@ -81,25 +81,3 @@ foreach($const['missing'] AS $file => $constants) {
     echo "---------------------------\n";
 }
 
-@define('SYNDICATION_PLUGIN_XML_DESC', 'Set to "none" if you only want to show a text link.');
-@define('MULTICHECK_NO_ITEM', 'No item selected, please check at least one. <a href="%s">Return to previous page</a>.');
-@define('MULTICHECK_NO_DIR', 'No directory selected, please choose one. <a href="%s">Return to previous page</a>.');
-@define('BULKMOVE_INFO', 'Bulk-move info');
-@define('BULKMOVE_INFO_DESC', 'You can select multiple files to bulk-move them to a new location. <strong>Note:</strong> This action cannot be undone, just like bulk-deletion of multiple files. All checked files will be physically moved, and referring blog entries are rewritten to point to the new location.');
-@define('FIRST_PAGE', 'First Page');
-@define('LAST_PAGE', 'Last Page');
-@define('MEDIA_PROPERTIES_DONE', 'Properties of #%d changed.');
-@define('DIRECTORY_INFO', 'Directory info');
-@define('DIRECTORY_INFO_DESC', 'Directories reflect their physical folder directory name. If you want to change or move directories which contain items, you have two choices. Either create the directory or subdirectory you want, then move the items to the new directory via the media library and afterwards, delete the empty old directory there. Or completely change the whole old directory via the edit directory button below and rename it to whatever you like (existing subdir/ + newname). This will move all directories and items and change referring blog entries.');
-@define('MEDIA_RESIZE_EXISTS', 'File dimensions already exist!');
-@define('USE_CACHE', 'Enable caching');
-@define('USE_CACHE_DESC', 'Enables an internal cache to not repeat specific database queries. This reduces the load on servers with medium to high traffic and improves page load time.');
-@define('CONFIG_PERMALINK_PATH_DESC', 'Please note that you have to use a prefix so that Serendipity can properly map the URL to the proper action. You may change the prefix to any unique name, but not remove it. This applies to all path prefix definitions.');
-
-@define('HIDE_SUBDIR_FILES', 'Hide Files of Subdirectories');
-@define('USERCONF_DASHBOARD_DEFAULT_WIDGETS', 'Default dashboard widgets?');
-@define('USERCONF_DASHBOARD_DEFAULT_WIDGETS_DESC', 'Show default and hardcoded dashboard widgets, like draft entries and last comments.');
-
-@define('UPDATE_NOTIFICATION_URL', 'Serendipity update RELEASE file URL');
-@define('UPDATE_NOTIFICATION_URL_DESC', 'Do not change, if not applying a different RELEASE file location for custom core downloads in combination with the Serendipity Autopudate plugin. The default value to apply here is "https://raw.githubusercontent.com/s9y/Serendipity/master/docs/RELEASE" and points to a file containing Serendipity stable and beta version numbers per line, eg. "stable:5.3.0".');
-
