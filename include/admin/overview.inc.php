c1d2c96bb33145425ff6bebd3fc6dc794160f6e7
Ian <aristophian@googlemail.com>
2016-06-18 08:55:57 +0200

    optimize load for default widgets no option

--- include/admin/overview.inc.php
+++ include/admin/overview.inc.php
@@ -4,8 +4,8 @@ if (IN_serendipity !== true) {
     die ("Don't hack!");
 }
 
-global $serendipity;
-$data = array();
+$data = array(); // init smartification data array
+$output = array(); // init backend_frontpage_display hook array
 
 switch($serendipity['POST']['adminAction']) {
     case 'publish':
@@ -23,6 +23,7 @@ switch($serendipity['POST']['adminAction']) {
             $data['error_publish'] = $success;
         }
         break;
+
     case 'updateCheckDisable':
         if ( !serendipity_checkFormToken() || !serendipity_checkPermission('blogConfiguration') ) {
             break;
@@ -31,17 +32,14 @@ switch($serendipity['POST']['adminAction']) {
         break;
 }
 
-
 $user = serendipity_fetchAuthor($serendipity['authorid']);
 // chrome-compatible, from Oliver Gassner, adapted from TextPattern. Hi guys, keep it up. :-)
 $bookmarklet = "javascript:var%20d=document,w=window,e=w.getSelection,k=d.getSelection,x=d.selection,s=(e?e():(k)?k():(x?x.createRange().text:0)),f='" . $serendipity['baseURL'] . "',l=d.location,e=encodeURIComponent,p='serendipity_admin.php?serendipity[adminModule]=entries&serendipity[adminAction]=new&serendipity[title]='+e(d.title)+'&serendipity[body]='+e(s)+'&serendipity[url]='+location.href,u=f+p;a=function(){%20%20if(!w.open(u,'t','toolbar=0,resizable=1,scrollbars=1,status=1,width=800,height=800'))%20%20%20%20l.href=u;};if(/Firefox/.test(navigator.userAgent))%20%20setTimeout(a,0);else%20%20a();void(0)";
-
 
 $data['bookmarklet'] = $bookmarklet;
 $data['username'] = $user[0]['realname'];
 $data['js_failure_file'] = serendipity_getTemplateFile('admin/serendipity_editor.js');
 
-$output = array();
 serendipity_plugin_api::hook_event('backend_frontpage_display', $output);
 $data['backend_frontpage_display'] = $output['more'];
 
@@ -52,81 +50,83 @@ $data['update']       = version_compare($data['usedVersion'], $data['curVersion'
 serendipity_plugin_api::hook_event('plugin_dashboard_updater', $output, $data['curVersion']);
 $data['updateButton'] = $output;
 
-// Can be set through serendipity_config_local.inc.php
-if (!isset($serendipity['dashboardCommentsLimit'])) {
-    $serendipity['dashboardCommentsLimit'] = 5;
-}
-if (!isset($serendipity['dashboardLimit'])) {
-    $serendipity['dashboardLimit'] = 5;
-}
-if (!isset($serendipity['dashboardDraftLimit'])) {
-    $serendipity['dashboardDraftLimit'] = 5;
-}
+if ($serendipity['default_widgets']) {
+    // Can be set through serendipity_config_local.inc.php
+    if (!isset($serendipity['dashboardCommentsLimit'])) {
+        $serendipity['dashboardCommentsLimit'] = 5;
+    }
+    if (!isset($serendipity['dashboardLimit'])) {
+        $serendipity['dashboardLimit'] = 5;
+    }
+    if (!isset($serendipity['dashboardDraftLimit'])) {
+        $serendipity['dashboardDraftLimit'] = 5;
+    }
 
-$cjoin  = ($serendipity['serendipityUserlevel'] == USERLEVEL_EDITOR) ? "
-        LEFT JOIN {$serendipity['dbPrefix']}authors a ON (e.authorid = a.authorid)
-            WHERE e.authorid = " . (int)$serendipity['authorid']
-        : '';
-$cquery = "SELECT c.*, e.title, e.authorid
-             FROM {$serendipity['dbPrefix']}comments c
-        LEFT JOIN {$serendipity['dbPrefix']}entries e ON (e.id = c.entry_id)
-        " . $cjoin ."
-         ORDER BY c.id DESC LIMIT " . (int)$serendipity['dashboardCommentsLimit'];
-$comments = serendipity_db_query($cquery);
+    $cjoin  = ($serendipity['serendipityUserlevel'] == USERLEVEL_EDITOR) ? "
+            LEFT JOIN {$serendipity['dbPrefix']}authors a ON (e.authorid = a.authorid)
+                WHERE e.authorid = " . (int)$serendipity['authorid']
+            : '';
+    $cquery = "SELECT c.*, e.title, e.authorid
+                 FROM {$serendipity['dbPrefix']}comments c
+            LEFT JOIN {$serendipity['dbPrefix']}entries e ON (e.id = c.entry_id)
+            " . $cjoin ."
+             ORDER BY c.id DESC LIMIT " . (int)$serendipity['dashboardCommentsLimit'];
+    $comments = serendipity_db_query($cquery);
 
-if (is_array($comments) && count($comments) > 0) {
-    foreach ($comments as &$comment) {
-        $comment['entrylink'] = serendipity_archiveURL($comment['entry_id'], 'comments', 'serendipityHTTPPath', true) . '#c' . $comment['id'];
+    if (is_array($comments) && count($comments) > 0) {
+        foreach ($comments as &$comment) {
+            $comment['entrylink'] = serendipity_archiveURL($comment['entry_id'], 'comments', 'serendipityHTTPPath', true) . '#c' . $comment['id'];
 
-        $comment['fullBody']  = $comment['body'];
-        $comment['summary']   = serendipity_mb('substr', $comment['body'], 0, 100);
+            $comment['fullBody']  = $comment['body'];
+            $comment['summary']   = serendipity_mb('substr', $comment['body'], 0, 100);
 
-        if (strlen($comment['fullBody']) > strlen($comment['summary']) ) {
-            $comment['excerpt'] = true;
+            if (strlen($comment['fullBody']) > strlen($comment['summary']) ) {
+                $comment['excerpt'] = true;
 
-            // When summary is not the full body, strip HTML tags from summary, as it might break and leave unclosed HTML.
-            $comment['fullBody'] = nl2br(serendipity_specialchars($comment['fullBody']));
-            $comment['summary']  = nl2br(strip_tags($comment['summary']));
+                // When summary is not the full body, strip HTML tags from summary, as it might break and leave unclosed HTML.
+                $comment['fullBody'] = nl2br(serendipity_specialchars($comment['fullBody']));
+                $comment['summary']  = nl2br(strip_tags($comment['summary']));
+            }
         }
     }
-}
 
-$data['comments'] = $comments;
+    $data['comments'] = $comments;
 
-$efilter = ($serendipity['serendipityUserlevel'] == USERLEVEL_EDITOR) ? ' AND e.authorid = ' . (int)$serendipity['authorid'] : '';
-$entries = serendipity_fetchEntries(
-                     false,
-                     false,
-                     (int)$serendipity['dashboardLimit'],
-                     true,
-                     false,
-                     'timestamp DESC',
-                     'e.timestamp >= ' . serendipity_serverOffsetHour() . $efilter
-                   );
+    $efilter = ($serendipity['serendipityUserlevel'] == USERLEVEL_EDITOR) ? ' AND e.authorid = ' . (int)$serendipity['authorid'] : '';
+    $entries = serendipity_fetchEntries(
+                        false,
+                        false,
+                        (int)$serendipity['dashboardLimit'],
+                        true,
+                        false,
+                        'timestamp DESC',
+                        'e.timestamp >= ' . serendipity_serverOffsetHour() . $efilter
+                    );
 
-$entriesAmount = count($entries);
-if ($entriesAmount < (int)$serendipity['dashboardDraftLimit']) {
-    // there is still space for drafts
-    $drafts = serendipity_fetchEntries(
-                     false,
-                     false,
-                     (int)$serendipity['dashboardDraftLimit'] - $entriesAmount,
-                     true,
-                     false,
-                     'timestamp DESC',
-                     "isdraft = 'true' AND e.timestamp <= " . serendipity_serverOffsetHour() . $efilter
-                   );
-    if (is_array($entries) && is_array($drafts)) {
-        $entries = array_merge($entries, $drafts);
-    } else {
-        if (is_array($drafts)) {
-            // $entries is not an array, thus empty
-            $entries = $drafts;
+    $entriesAmount = count($entries);
+    if ($entriesAmount < (int)$serendipity['dashboardDraftLimit']) {
+        // there is still space for drafts
+        $drafts = serendipity_fetchEntries(
+                        false,
+                        false,
+                        (int)$serendipity['dashboardDraftLimit'] - $entriesAmount,
+                        true,
+                        false,
+                        'timestamp DESC',
+                        "isdraft = 'true' AND e.timestamp <= " . serendipity_serverOffsetHour() . $efilter
+                    );
+        if (is_array($entries) && is_array($drafts)) {
+            $entries = array_merge($entries, $drafts);
+        } else {
+            if (is_array($drafts)) {
+                // $entries is not an array, thus empty
+                $entries = $drafts;
+            }
         }
     }
-}
 
-$data['entries'] = $entries;
+    $data['entries'] = $entries;
+} //end default_widgets
 
 $data['urltoken'] = serendipity_setFormToken('url');
 $data['token'] = serendipity_setFormToken();
