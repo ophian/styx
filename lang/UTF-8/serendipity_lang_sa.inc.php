d250929d72d1558b6771d43b6738c10decb3d4e8
Ian <aristophian@googlemail.com>
2016-08-05 12:14:07 +0200

    fix list

--- lang/UTF-8/serendipity_lang_sa.inc.php
+++ lang/UTF-8/serendipity_lang_sa.inc.php
@@ -5,7 +5,7 @@
 /* vim: set sts=4 ts=4 expandtab : */
 
 @define('LANG_CHARSET', 'UTF-8');
-@define('DATE_LOCALES', 'Arabic,ar , ar_SA.windows-1256');
+@define('DATE_LOCALES', 'Arabic, ar, ar_SA.windows-1256');
 @define('DATE_FORMAT_ENTRY', '%A, %B %e. %Y');
 @define('DATE_FORMAT_SHORT', '%Y-%m-%d %H:%M');
 @define('WYSIWYG_LANG', 'ar_SA');
