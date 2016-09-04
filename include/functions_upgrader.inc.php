4b6f5c59f2a847fc4ace66b80091273e3230f335
Ian <aristophian@googlemail.com>
2016-09-04 10:01:35 +0200

    Remove bundled-libs/docs directory since part of textwiki remove

--- include/functions_upgrader.inc.php
+++ include/functions_upgrader.inc.php
@@ -224,6 +224,7 @@ $dead_files_202 = array(
 /* A list of old non-empty directories which were removed with 2.1.0 */
 $dead_dirs_202 = array(
     $serendipity['serendipityPath'] . 'bundled-libs/Text',
+    $serendipity['serendipityPath'] . 'bundled-libs/docs',
     $serendipity['serendipityPath'] . 'htmlarea/ckeditor/ckeditor',
     $serendipity['serendipityPath'] . 'htmlarea/composer',
     $serendipity['serendipityPath'] . 'plugins/serendipity_event_gravatar/ycon/UTF-8'
