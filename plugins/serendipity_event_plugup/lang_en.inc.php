491dcd40657b7540614218bbdc11612c64c22121
Ian <aristophian@googlemail.com>
2016-10-20 19:33:19 +0200

    remove constant markup

--- plugins/serendipity_event_plugup/lang_en.inc.php
+++ plugins/serendipity_event_plugup/lang_en.inc.php
@@ -5,7 +5,7 @@
 @define('PLUGIN_EVENT_PLUGUP_SHOW_UPDATE_NOTIFIER', 'Show available Plugin updates?');
 @define('PLUGIN_EVENT_PLUGUP_SHOW_UPDATE_NOTIFIER_DESC', 'Will newly check every 6 hours');
 @define('PLUGIN_EVENT_PLUGUP_SPARTACUS_URL', 'Spartacus remote fetch url');
-@define('PLUGIN_EVENT_PLUGUP_SPARTACUS_URL_DESC', 'Please enable the remote plugin version information in SPARTACUS options and add here the "<em>secret</em>" key to the remote plugin version information! This is only readable for admin users in the Dashboard. Move the Plugup-plugin beneath the Spartacus-plugin in list.');
+@define('PLUGIN_EVENT_PLUGUP_SPARTACUS_URL_DESC', 'Please enable the remote plugin version information in SPARTACUS options and add here the "secret" key to the remote plugin version information! This is only readable for admin users in the Dashboard. Move the Plugup-plugin beneath the Spartacus-plugin in list.');
 @define('PLUGIN_DASHBOARD_PLUGUP_BOX_TITLE', 'Plugin Updates');
 @define('PLUGIN_DASHBOARD_PLUGUP_UP_TO_DATE', 'All Plugins are up to date!');
 @define('PLUGIN_DASHBOARD_PLUGUP_UP_AVAILABLE', '%s Plugin(s) are waiting for upgrade!');
