07f337881d054afdabba5c19f8fb41c80b583a2a
Ian <aristophian@googlemail.com>
2016-12-11 11:40:47 +0100

    changelog: Fix non mod_rewrite links

--- plugins/serendipity_event_changelog/serendipity_event_changelog.php
+++ plugins/serendipity_event_changelog/serendipity_event_changelog.php
@@ -20,7 +20,7 @@ class serendipity_event_changelog extends serendipity_plugin
         $propbag->add('description',    '');
         $propbag->add('stackable',      false);
         $propbag->add('author',        'Ian');
-        $propbag->add('version',       '1.22');
+        $propbag->add('version',       '1.23');
         $propbag->add('requirements',  array(
             'serendipity' => '2.0.2',
             'php'         => '5.3.0'
@@ -94,7 +94,7 @@ class serendipity_event_changelog extends serendipity_plugin
     <section id="maintenance_logview" class="quick_list">
         <h3><?php echo PLUGIN_CHANGELOG_MAINTAIN; ?></h3>
 
-        <a id="logview" class="button_link" href="<?php echo $serendipity['serendipityHTTPPath'] . (($serendipity['rewrite'] == 'rewrite') ? '' : 'index.php?') ?>plugin/changelog" title=""><span><?php echo PLUGIN_CHANGELOG_BUTTON; ?></span></a>
+        <a id="logview" class="button_link" href="<?php echo $serendipity['serendipityHTTPPath'] . (($serendipity['rewrite'] == 'rewrite') ? '' : 'index.php?/') ?>plugin/changelog" title=""><span><?php echo PLUGIN_CHANGELOG_BUTTON; ?></span></a>
         <button class="toggle_info button_link" type="button" data-href="#logview_info"><span class="icon-info-circled" aria-hidden="true"></span><span class="visuallyhidden"> <?php echo MORE; ?></span></button>
         <span id="logview_info" class="comment_status additional_info"><?php echo sprintf(PLUGIN_CHANGELOG_TITLE_DESC, $serendipity['version']); ?></span>
 <?php
@@ -111,7 +111,7 @@ class serendipity_event_changelog extends serendipity_plugin
 
         <div class="serendipity_logger">
         <?php echo $delOld; ?>
-        <a id="logger" class="button_link" href="<?php echo $serendipity['serendipityHTTPPath'] . (($serendipity['rewrite'] == 'rewrite') ? '' : 'index.php?') ?>plugin/logs" title=""><span><?php echo PLUGIN_LOGGER_BUTTON; ?></span></a>
+        <a id="logger" class="button_link" href="<?php echo $serendipity['serendipityHTTPPath'] . (($serendipity['rewrite'] == 'rewrite') ? '' : 'index.php?/') ?>plugin/logs" title=""><span><?php echo PLUGIN_LOGGER_BUTTON; ?></span></a>
         <button class="toggle_info button_link" type="button" data-href="#logger_info"><span class="icon-info-circled" aria-hidden="true"></span><span class="visuallyhidden"> <?php echo MORE; ?></span></button>
         <span id="logger_info" class="comment_status additional_info"><?php echo sprintf(PLUGIN_CHANGELOG_LOGGER_DESC, $serendipity['version']); ?></span>
         </div>
