532d03f196ab814a19b901c168706fe562b6e364
Ian <aristophian@googlemail.com>
2017-01-12 17:27:51 +0100

    Fix a1efb568 for first isset and 1933dcee for parameters

--- comment.php
+++ comment.php
@@ -11,13 +11,13 @@ header('Content-Type: text/html; charset=' . LANG_CHARSET);
 
 if (isset($serendipity['GET']['delete'], $serendipity['GET']['entry'], $serendipity['GET']['type'])) {
     serendipity_deleteComment($serendipity['GET']['delete'], $serendipity['GET']['entry'], $serendipity['GET']['type']);
-    if (serendipity_isResponseClean($_SERVER['HTTP_REFERER']) && preg_match('@^https?://' . preg_quote($_SERVER['HTTP_HOST'], '@') . '@imsU')) {
-        if (isset($_SERVER['HTTP_REFERER'])) {
+    if (isset($_SERVER['HTTP_REFERER'])) {
+        if (serendipity_isResponseClean($_SERVER['HTTP_REFERER']) && preg_match('@^https?://' . preg_quote($_SERVER['HTTP_HOST'], '@') . '@imsU', $_SERVER['HTTP_REFERER'])) {
             header('Status: 302 Found');
             header('Location: '. $_SERVER['HTTP_REFERER']);
         }
-        exit;
     }
+    exit;
 }
 
 if (isset($serendipity['GET']['switch'], $serendipity['GET']['entry'])) {
