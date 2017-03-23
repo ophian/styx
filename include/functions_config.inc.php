ff37fab6f07d01479f4412ad1fd81a48a096ace7
Ian <aristophian@googlemail.com>
2017-03-23 10:01:21 +0100

    Sync - Restore session id change on logout

--- include/functions_config.inc.php
+++ include/functions_config.inc.php
@@ -386,17 +386,8 @@ function serendipity_logout() {
 function serendipity_session_destroy() {
     $no_smarty = $_SESSION['no_smarty'];
     @session_destroy();
-    try {
-        // Code that may throw an Exception or Error.
-        session_regenerate_id();
-    } catch (Throwable $t) {
-        // Executed only in PHP 7, will not match in PHP 5.x
-        echo $t->getMessage(), "\n";
-    } catch (Exception $e) {
-        // Executed only in PHP 5.x, will not be reached in PHP 7
-        echo $e->getMessage(), "\n";
-    }
     session_start();
+    session_regenerate_id();
 
     $_SESSION['SERVER_GENERATED_SID'] = true;
     $_SESSION['no_smarty']            = $no_smarty;
