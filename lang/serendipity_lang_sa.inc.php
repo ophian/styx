982f43c43695fb6a218f8482c7b767906b691d5e
Ian <aristophian@googlemail.com>
2017-07-04 11:30:49 +0200

    lang [sa] - Try to fix missing (abd changed) constants
    still needs a native review though!

--- lang/serendipity_lang_sa.inc.php
+++ lang/serendipity_lang_sa.inc.php
@@ -3,8 +3,9 @@
 # All rights reserved.  See LICENSE file for licensing details
 # Arabic issue was translated by Way http://www.flyingway.com  cp1256
 /* vim: set sts=4 ts=4 expandtab : */
-
+∞
 @define('LANG_CHARSET', 'cp1256');
+@define('SQL_CHARSET', 'latin1');
 @define('DATE_LOCALES', 'Arabic, ar, ar_SA.windows-1256');
 @define('DATE_FORMAT_ENTRY', '%A, %B %e. %Y');
 @define('DATE_FORMAT_SHORT', '%Y-%m-%d %H:%M');
@@ -813,12 +814,92 @@
 @define('CATEGORY_PLUGIN_SHOWCOUNT', '√⁄—÷ ⁄œœ «·„Ê«÷Ì⁄ ·ﬂ  ’‰Ì›ø');
 @define('AUTHORS_SHOW_ARTICLE_COUNT', '√⁄—÷ ⁄œœ „Ê«÷Ì⁄ «·ﬂ« » »Ã«‰» √”„Âø');
 @define('AUTHORS_SHOW_ARTICLE_COUNT_DESC', '›Ì Õ«·  ‘€Ì· Â–« «·ŒÌ«— ›≈‰ ⁄œœ «·„Ê«÷Ì⁄ «·Œ«’… »Â–« «·ﬂ« » ”Ê› Ì „ ⁄—÷Â« »Ã«‰» ≈”„Â');
+@define('CUSTOM_ADMIN_INTERFACE', 'Custom admin interface');
+
+@define('INSTALL_TRACKREF', 'Enable referrer tracking?');
+@define('INSTALL_TRACKREF_DESC', 'Enabling the referrer tracking will show you which sites refer to your articles. Today this is often abused for spamming, so you can disable it if you want.');
+@define('CATEGORIES_HIDE_PARENT', 'Hide the selected parent category?');
+@define('CATEGORIES_HIDE_PARENT_DESC', 'If you restrict the listing of categories to a specific category, by default you will see that parent category within the output listing. If you disable this option, the parent category name will not be displayed.');
+@define('WARNING_NO_GROUPS_SELECTED', 'Warning: You did not select any group memberships. This would effectively log you out of the usergroup management, and thus your group memberships were not changed.');
+@define('INSTALL_RSSFETCHLIMIT', 'Entries to display in Feeds');
+@define('INSTALL_RSSFETCHLIMIT_DESC', 'Number of entries to display for each page on the RSS Feed.');
+@define('INSTALL_DB_UTF8', 'Enable DB-charset conversion');
+@define('INSTALL_DB_UTF8_DESC', 'Issues a MySQL "SET NAMES" query to indicate the required charset for the database. Turn this on or off, if you see weird or missing characters in your blog.');
+@define('ONTHEFLYSYNCH', 'Enable on-the-fly media synchronization');
+@define('ONTHEFLYSYNCH_DESC', 'If enabled, Serendipity will compare the media database with the files stored on your server and synchronize the database and directory contents.');
+@define('USERCONF_CHECK_USERNAME_ERROR', 'The username cannot be left blank.');
+@define('FURTHER_LINKS', 'Further Links');
+@define('FURTHER_LINKS_S9Y', 'Serendipity Homepage');
+@define('FURTHER_LINKS_S9Y_DOCS', 'Serendipity Documentation');
+@define('FURTHER_LINKS_S9Y_BLOG', 'Official Blog');
+@define('FURTHER_LINKS_S9Y_FORUMS', 'Forums');
+@define('FURTHER_LINKS_S9Y_SPARTACUS', 'Spartacus');
+@define('COMMENT_IS_DELETED', '(Comment removed)');
+
+@define('CURRENT_AUTHOR', 'Current author');
+
+@define('WORD_NEW', 'New');
+@define('SHOW_MEDIA_TOOLBAR', 'Show toolbar within media selector popup?');
+@define('MEDIA_KEYWORDS', 'Media keywords');
+@define('MEDIA_KEYWORDS_DESC', 'Enter a list of ";" separated words that you want to use as pre-defined keywords for media items.');
+@define('MEDIA_EXIF', 'Import EXIF/JPEG image data');
+@define('MEDIA_EXIF_DESC', 'If enabled, existing EXIF/JPEG metadata of images will be parsed and stored in the database for display in the media gallery.');
+@define('MEDIA_PROP', 'Media properties');
+
+@define('GO_ADD_PROPERTIES', 'Go & enter properties');
+@define('MEDIA_PROPERTY_DPI', 'DPI');
+@define('MEDIA_PROPERTY_COPYRIGHT', 'Copyright');
+@define('MEDIA_PROPERTY_COMMENT1', 'Short Comment');
+@define('MEDIA_PROPERTY_COMMENT2', 'Long Comment');
+@define('MEDIA_PROPERTY_TITLE', 'Title');
+@define('MEDIA_PROP_DESC', 'Enter a list of ";" separated property fields you want to define for each media file');
+@define('MEDIA_PROP_MULTIDESC', '(You can append ":MULTI" after any item to indicate that this item will contain long text instead of just some characters)');
+
+@define('STYLE_OPTIONS_NONE', 'This theme has no specific options. To see how your theme can specify options, read the Technical Documentation on www.s9y.org about "Configuration of Theme options".');
+@define('STYLE_OPTIONS', 'Theme options');
+
+@define('PLUGIN_AVAILABLE_COUNT', 'Total: %d plugins.');
+
+@define('SYNDICATION_RFC2616', 'Activate strict RFC2616 RSS-Feed compliance');
+@define('SYNDICATION_RFC2616_DESC', 'NOT Enforcing RFC2616 means that all Conditional GETs to Serendipity will return entries last modified since the time of the last request. With that setting to "false", your visitors will get all articles since their last request, which is considered a good thing. However, some Agents like Planet act weird, if that happens, at it also violates RFC2616. So if you set this option to "TRUE" you will comply with that RFC, but readers of your RSS feed might miss items in their holidays. So either way, either it hearts Aggregators like Planet, or it hurts actual readers of your blog. If you are facing complaints from either side, you can toggle this option. Reference: <a href="https://sourceforge.net/tracker/index.php?func=detail&amp;aid=1461728&amp;group_id=75065&amp;atid=542822" target="_blank" rel="nofollow">SourceForge</a>');
+@define('MEDIA_PROPERTY_DATE', 'Associated Date');
+@define('MEDIA_PROPERTY_RUN_LENGTH', 'Run-Length');
+@define('FILENAME_REASSIGNED', 'Automagically assigned new file name: %s');
+@define('MEDIA_UPLOAD_SIZE', 'Max. file upload size');
+@define('MEDIA_UPLOAD_SIZE_DESC', 'Enter the maximum filesize for uploaded files in bytes. This setting can be overruled by server-side settings in PHP.ini: upload_max_filesize, post_max_size, max_input_time all take precedence over this option. An empty string means to only use the server-side limits.');
+@define('MEDIA_UPLOAD_SIZEERROR', 'Error: You cannot upload files larger than %s bytes!');
+@define('MEDIA_UPLOAD_MAXWIDTH', 'Max. width of image files for upload');
+@define('MEDIA_UPLOAD_MAXWIDTH_DESC', 'Enter the maximum image width in pixels for uploaded images.');
+@define('MEDIA_UPLOAD_MAXHEIGHT', 'Max. height of image files for upload');
+@define('MEDIA_UPLOAD_MAXHEIGHT_DESC', 'Enter the maximum image height in pixels for uploaded images.');
+@define('MEDIA_UPLOAD_DIMERROR', 'Error: You cannot upload image files larger than %s x %s pixels!');
+
+@define('MEDIA_TARGET', 'Target for this link');
+@define('MEDIA_TARGET_JS', 'Popup window (via JavaScript, adaptive size)');
+@define('MEDIA_ENTRY', 'Isolated Entry');
+@define('MEDIA_TARGET_BLANK', 'Popup window (via target=_blank)');
+
+@define('MEDIA_DYN_RESIZE', 'Allow dynamic image resizing?');
+@define('MEDIA_DYN_RESIZE_DESC', 'If enabled, the media selector can return images in any requested size via a GET variable. The results are cached, and thus can create a large filebase if you make intensive use of it.');
+
+@define('MEDIA_DIRECTORY_MOVED', 'Directory and files were successfully moved to %s');
+@define('MEDIA_DIRECTORY_MOVE_ERROR', 'Directory and files could not be moved to %s!');
+@define('MEDIA_DIRECTORY_MOVE_ENTRY', 'On Non-MySQL databases, iterating through every article to replace the old directory URLs with new directory URLs is not possible. You will need to manually edit your entries to fix new URLs. You can still move your old directory back to where it was, if that is too cumbersome for you.');
+@define('MEDIA_DIRECTORY_MOVE_ENTRIES', 'Moved the URL of the moved directory in %s entries.');
+@define('MEDIA_FILE_RENAME_ENTRY', 'The filename was changed in %s entries.');
+@define('PLUGIN_ACTIVE', 'Active');
+@define('PLUGIN_INACTIVE', 'Inactive');
+@define('PREFERENCE_USE_JS', 'Enable advanced JS usage?');
+@define('PREFERENCE_USE_JS_DESC', 'If enabled, advanced JavaScript sections will be enabled for better usability, like in the Plugin Configuration section you can use drag and drop for re-ordering plugins.');
+@define('PREFERENCE_USE_JS_WARNING', '(This page uses advanced JavaScripting. If you are having functionality issues, please disable the use of advanced JS usage in your personal preferences or disable your browser\'s JavaScript)');
+
 @define('INSTALL_PERMALINK_COMMENTSPATH', 'Path to comments');
 @define('PERM_SET_CHILD', 'Set the same permissions on all child directories');
 @define('PERMISSION_FORBIDDEN_PLUGINS', 'Forbidden plugins');
 @define('PERMISSION_FORBIDDEN_HOOKS', 'Forbidden events');
 @define('PERMISSION_FORBIDDEN_ENABLE', 'Enable Plugin ACL for usergroups?');
 @define('PERMISSION_FORBIDDEN_ENABLE_DESC', 'If the option "Plugin ACL for usergroups" is enabled in the configuration, you can specify which usergroups are allowed to execute certain plugins/events.');
+
 @define('DELETE_SELECTED_ENTRIES', 'Delete selected entries');
 @define('PLUGIN_AUTHORS_MINCOUNT', 'Show only authors with at least X articles');
 @define('FURTHER_LINKS_S9Y_BOOKMARKLET', 'Bookmarklet');
@@ -835,7 +916,7 @@
 @define('CATEGORY_PLUGIN_HIDEZEROCOUNT', 'Hide archives link when no entries were made in that timespan (requires counting entries)');
 @define('RSS_IMPORT_WPXRSS', 'WordPress eXtended RSS import, requires PHP5 and might take up much memory');
 @define('SET_TO_MODERATED', 'Moderate');
-@define('COMMENT_MODERATED', 'Comment #%s has successfully been set as moderated');
+@define('COMMENT_MODERATED', 'Comment #%s has successfully been set back to state "moderate".');
 @define('CENTER', 'center');
 @define('FULL_COMMENT_TEXT', 'Yes, with full comment text');
 
@@ -880,10 +961,10 @@
 @define('PERMISSION_HIDDENGROUP', 'Hidden group / Non-Author');
 
 @define('SEARCH_FULLENTRY', 'Show full entry');
-@define('NAVLINK_AMOUNT', 'Enter number of links in the navbar (needs reload of the Manage Styles page)');
+@define('NAVLINK_AMOUNT', 'Enter number of links in the navbar (needs reload of the Manage Themes page)');
 @define('NAV_LINK_TEXT', 'Enter the navbar link text');
 @define('NAV_LINK_URL', 'Enter the full URL of your link');
-@define('MODERATE_SELECTED_COMMENTS', 'Accept selected comments');
+@define('MODERATE_SELECTED_COMMENTS', 'Approve selected comments');
 @define('WEBLOG', 'Weblog');
 @define('ACTIVE_COMMENT_SUBSCRIPTION', 'Subscribed');
 @define('PENDING_COMMENT_SUBSCRIPTION', 'Pending confirmation');
@@ -892,9 +973,10 @@
 
 // Next lines were added on 2012/05/29
 @define('ABOUT_TO_DELETE_FILES', 'You are about to delete a bunch of files at once.<br />If you are using these in some of your entries, it will cause dead links or images<br />Are you sure you wish to proceed?<br /><br />');
-
 @define('ARCHIVE_SORT_STABLE', 'Stable Archives');
 @define('ARCHIVE_SORT_STABLE_DESC', 'Sort the archive-pages descending, so they are stable and search-crawler do not have to reindex them.');
+
+// added 2012-11-29
 @define('PLAIN_ASCII_NAMES', '(no special characters, umlauts)');
 // New 2.0 constants
 @define('SIMPLE_FILTERS', 'Simplified filters');
@@ -911,7 +993,7 @@
 @define('MENU_DASHBOARD', 'Dashboard');
 @define('MENU_ACTIVITY', 'Activity');
 @define('MENU_SETTINGS', 'Settings');
-@define('MENU_TEMPLATES', 'Templates');
+@define('MENU_TEMPLATES', 'Themes');
 @define('MENU_PLUGINS', 'Plugins');
 @define('MENU_USERS', 'Users');
 @define('MENU_GROUPS', 'Groups');
@@ -933,10 +1015,10 @@
 @define('MOVE_DOWN', 'Move down');
 @define('INSTALL_NEW_SIDEBAR_PLUGIN', 'Install a new sidebar plugin');
 @define('INSTALL_NEW_EVENT_PLUGIN', 'Install a new event plugin');
-@define('TEMPLATE_OPTIONS', 'Template options');
-@define('CURRENT_TEMPLATE', 'Current Template');
-@define('TEMPLATE_INFO', 'Show template info');
-@define('AVAILABLE_TEMPLATES', 'Available Templates');
+@define('TEMPLATE_OPTIONS', 'Theme options');
+@define('CURRENT_TEMPLATE', 'Current Theme');
+@define('TEMPLATE_INFO', 'Show theme info');
+@define('AVAILABLE_TEMPLATES', 'Available Themes');
 @define('TIMESTAMP_RESET', 'The timestamp has been reset to the current time.');
 
 @define('CLEANCOMPILE_PASS', '[smarty clearCompiledTemplate(%s)]');
@@ -948,7 +1030,7 @@
 @define('CURRENT_TAB', 'Current tab: ');
 @define('PINGBACKS', 'Pingbacks');
 @define('NO_PINGBACKS', 'No Pingbacks');
-@define('GROUP_NAME_DESC', "Use as uppercased eg. 'EXAMPLE_GROUP' name, but not as a constant 'USERLEVEL_XYZ' group name.");
+@define('GROUP_NAME_DESC', "If you enter a string that is defined as a constant in any language file (e.g. 'USERLEVEL_EDITOR_DESC'), then the translated representation of this groupname will show up. You can also define your own constants and then enter their name here. If you do not want to have translatable group names, you can enter any other string you like.");
 @define('INSTALLER_CLI_TOOLS', 'Server-side command line tools');
 @define('INSTALLER_CLI_TOOLNAME', 'CLI tool');
 @define('INSTALLER_CLI_TOOLSTATUS', 'Executable?');
@@ -960,7 +1042,7 @@
 @define('FRONTEND', 'Frontend');
 @define('BACKEND', 'Backend');
 @define('MEDIA_UPLOAD_RESIZE', 'Resize before Upload');
-@define('MEDIA_UPLOAD_RESIZE_DESC', 'Resize images before the upload using Javascript. This will also change the uploader to use Ajax and thus remove the Property-Button');
+@define('MEDIA_UPLOAD_RESIZE_DESC', 'Resize images according to configured maximum/minimum dimensions before the upload using Javascript. This will also change the uploader to use Ajax and thus remove the Property-Button');
 @define('LOG_LEVEL', 'Log Level');
 @define('LOG_LEVEL_DESC', 'At certain places in the Serendipity code we have placed debugging breakpoints. If this option is set to "Debug", it will write this debug output to templates_c/logs/. You should only enable this option if you are experiencing bugs in those areas, or if you are a developer. Setting this option to "Error" will enable logging PHP errors, overwriting the PHP error_log setting.');
 @define('DEBUG', 'Debug');
@@ -1024,6 +1106,7 @@
 @define('JS_FAILURE', 'The Serendipity JavaScript-library could not be loaded. This can happen due to PHP or Plugin errors, or even a malformed browser cache. To check the exact error please open <a href="%1$s">%1$s</a> manually in your browser and check for error messages.');
 @define('THEMES_PREVIEW_BLOG', 'See demo on blog.s9y.org');
 @define('SYNDICATION_PLUGIN_XML_DESC', 'Set to "none" if you only want to show a text link.');
+
 @define('MULTICHECK_NO_ITEM', 'No item selected, please check at least one. <a href="%s">Return to previous page</a>.');
 @define('MULTICHECK_NO_DIR', 'No directory selected, please choose one. <a href="%s">Return to previous page</a>.');
 @define('BULKMOVE_INFO', 'Bulk-move info');
