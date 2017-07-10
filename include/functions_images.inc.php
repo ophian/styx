ophian/styx

commit dcb3e0e20562e5f7d7f1ba9c198964e74390b903

Fixed regression bug for HOTlinked media items
and the hideSubdirFiles(true) option in MediaLibrary BASE_DIRECTORY cases

include/functions_images.inc.php
@@ -76,7 +76,8 @@ function serendipity_fetchImagesFromDatabase($start=0, $limit=0, &$total=null, $
     }
 
     if ($hideSubdirFiles) {
-        $cond['parts']['directory'] = " AND i.path = '" . serendipity_db_escape_string($directory) . "'\n";
+        $hotlinked = ($directory == '') ? " OR i.path LIKE 'http://%' OR i.path LIKE 'https://%'" : '';
+        $cond['parts']['directory'] = " AND (i.path = '" . serendipity_db_escape_string($directory) . "'$hotlinked)\n";
     } elseif (!empty($directory)) {
         $cond['parts']['directory'] = " AND i.path LIKE '" . serendipity_db_escape_string($directory) . "%'\n";
     }

commit b52f594550124a3db66bb8c6d873bc9a95bc63ab

Optimise hotlink regression bugfix in dcb3e0e

include/functions_images.inc.php
@@ -76,7 +76,7 @@ function serendipity_fetchImagesFromDatabase($start=0, $limit=0, &$total=null, $
     }
 
     if ($hideSubdirFiles) {
-        $hotlinked = ($directory == '') ? " OR i.path LIKE 'http://%' OR i.path LIKE 'https://%'" : '';
+        $hotlinked = ($directory == '') ? " OR i.hotlink = 1" : '';
         $cond['parts']['directory'] = " AND (i.path = '" . serendipity_db_escape_string($directory) . "'$hotlinked)\n";
     } elseif (!empty($directory)) {
         $cond['parts']['directory'] = " AND i.path LIKE '" . serendipity_db_escape_string($directory) . "%'\n";