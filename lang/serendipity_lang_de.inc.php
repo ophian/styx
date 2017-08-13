3f49386b3adfa9686a9fe528249a4a89ae1bbaa3
Ian <aristophian@googlemail.com>
2017-08-13 13:25:57 +0200

    lang: Add missing parameter to REMOTE_FILE_INVALID

--- lang/serendipity_lang_de.inc.php
+++ lang/serendipity_lang_de.inc.php
@@ -1131,7 +1131,7 @@
 
 @define('CONFIG_ALLOW_LOCAL_URL', 'Erlaube, Daten über lokale URLs abzurufen');
 @define('CONFIG_ALLOW_LOCAL_URL_DESC', 'Standardmäßig ist es aus Sicherheitsgründen verboten, Daten über lokale URLs abzurufen, um Server Side Request Forgers (SSRF) zu vermeiden. Wenn Sie ein lokales Intranet verwenden, können sie die Datenabfrage über diese Option explizit erlauben.');
-@define('REMOTE_FILE_INVALID', 'Die angegebene URL '\'%s\' scheint eine lokale zu sein und darf nicht abgerufen werden. Sie können dies explizit gestatten, in dem Sie in der Konfiguration die Option "Erlaube, Daten über lokale URLs abzurufen" setzen.');
+@define('REMOTE_FILE_INVALID', 'Die angegebene URL "%s" scheint eine lokale zu sein und darf nicht abgerufen werden. Sie können dies explizit gestatten, in dem Sie in der Konfiguration die Option "Erlaube, Daten über lokale URLs abzurufen" setzen.');
 
 @define('INSTALLER_TOKEN_NOTE', 'Um eine abgesicherte Installation auszuführen, müssen Sie eine Datei namens "%s" mit dem String "<strong>&lt;?php $install_token = \'%s\'; ?&gt;</strong>" in diesem Verzeichnis erstellen. Sobald diese Datei existiert, müssen Sie die Installation innerhalb der nächsten %s Minuten ausführen und dürfen ihren Browser nicht schließen. Sie müssen Cookies erlauben.');
 @define('INSTALLER_TOKEN_MISMATCH', 'Sie sind nicht autorisiert die Installation auszuführen, da Ihr Install-Token (%s) nicht demjenigen aus der Datei "%s" entspricht. Stellen Sie sicher, dass Sie die Datei mit dem korrekten Inhalt erstellt haben. Sie können einen neuen Token erstellen in dem Sie die Datei löschen.');
