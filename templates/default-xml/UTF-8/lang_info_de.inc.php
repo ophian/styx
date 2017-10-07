c8636869f0adde48808406c65f31e79d8ba2f1b4
Ian <aristophian@googlemail.com>
2017-10-07 17:21:23 +0200

    themes: default-xml - Add translated info parts

---
+++ templates/default-xml/UTF-8/lang_info_de.inc.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * The info.txt UTF-8 charset (translation) file as an array.
+ * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['languages'].
+ * Convert or save as UTF-8 file without BOM.
+ **/
+
+$info['theme_info_summary'] = 'Reines XML Markup XML-Template Theme für das Serendipity Styx Blog system';
+
+$info['theme_info_desc'] = 'Einfaches Theme aus 2006, neu aufgefrischt um mit der Serendipity Styx Edition zusammenzuarbeiten.
+Dieses Theme gibt XML-Rohdaten auf der Frontseite aus, um den Inhalt für XML-Reader lesbar zu machen.
+Es wirkt sich immer noch auf einige unwesentliche Plugin-Integrationen auf der Backend-Seite aus,
+wie z. B. Linktrimmer oder Amazonchooser, und ist mehr oder weniger dazu gedacht,
+in einem nicht-zu viel erweiterten Core Release Blog verwendet zu werden,
+obwohl es mit einigen bekannten Plugin-Apps funktioniert, wie Statische Seiten, Freetags etc.';
---
+++ templates/default-xml/lang_info_en.inc.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * The info.txt native charset (translation) file as an array.
+ * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['languages'].
+ * Convert or save as ANSI (ISO-8859-1) or your native charset, analog to the non-UTF-8 lang constant files.
+ **/
+
+$info['theme_info_summary'] = 'Raw XML output XML-Template theme for the Serendipity Styx Blog system';
+
+$info['theme_info_desc'] = 'Simple theme from 2006, newly refurbished to work with Serendipity Styx Edition.
+This theme outputs raw XML on the frontside, to make the content readable by XML-Readers.
+It does still effect some minor plugin integrations on the backend side, like linktrimmer or amazonchooser,
+and is more or less meant to be used in a non-too-much-extended core release blog,
+though it does works with some well known plugin apps, like staticpages, freetags etc.';
