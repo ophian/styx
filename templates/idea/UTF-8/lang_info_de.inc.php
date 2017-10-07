91667bcae22ab0f2ba2e3338fad8c7be9e9f684d
Ian <aristophian@googlemail.com>
2017-10-07 17:22:19 +0200

    themes: idea - Add translated info parts

---
+++ templates/idea/UTF-8/lang_info_de.inc.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * The info.txt UTF-8 charset (translation) file as an array.
+ * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['languages'].
+ * Convert or save as UTF-8 file without BOM.
+ **/
+
+$info['theme_info_summary'] = 'Rein auf CSS basierendes Theme.';
+
+$info['theme_info_desc'] = 'Einfaches Beispiel Theme aus 2004 das alle Template Dateien des alten Default Fallback (Serendipity 2.3) Themes nutzt. Styling nur mit CSS.';
---
+++ templates/idea/lang_info_en.inc.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * The info.txt native charset (translation) file as an array.
+ * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['languages'].
+ * Convert or save as ANSI (ISO-8859-1) or your native charset, analog to the non-UTF-8 lang constant files.
+ **/
+
+$info['theme_info_summary'] = 'CSS complete theme only.';
+
+$info['theme_info_desc'] = 'Simple example theme from 2004 using all the template files from the old default fallback (Serendipity 2.3) theme. Styled by CSS only.';
