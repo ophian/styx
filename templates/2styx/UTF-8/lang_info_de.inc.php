acc0db4c7c778e3c863e98473fa84ad1a3109f44
Ian <aristophian@googlemail.com>
2017-10-07 17:13:36 +0200

    themes: 2Styx - Add translated info parts

---
+++ templates/2styx/UTF-8/lang_info_de.inc.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * The info.txt UTF-8 charset (translation) file as an array.
+ * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['languages'].
+ * Convert or save as UTF-8 file without BOM.
+ **/
+
+$info['theme_info_summary'] = 'Das Styx Backend testing Theme';
+
+$info['theme_info_desc'] = '<u>ACHTUNG&colon;</u> Dieses Theme hat kein eigenes Frontend!<br>
+Dieses Backend Beispiel-Theme verweist auf die Styx Kern Backend Templates im "2k11/admin" Unterverzeichnis.
+Als Beispiel zeigt es die Möglichkeit, ein eigenes und mehr oder weniger leeres Backend Theme zu erstellen, dass als "Fallback" die Standard "2k11" Backend Template Dateien nutzt.
+Mit dies im Hinterkopf, sind Sie in der Lage, nur diejenigen Dateien zu ändern und hinzuzufügen, die notwendig sind, um Ihre momentanen Bedürfnisse zu erfüllen.';
+
+$info['theme_info_backend'] = 'Dieses Backend Beispiel-Theme verweist auf die Styx Kern Backend Templates im "2k11/admin" Unterverzeichnis.
+Als Beispiel zeigt es die Möglichkeit, ein eigenes und mehr oder weniger leeres Backend Theme zu erstellen, dass als "Fallback" die Standard "2k11" Backend Template Dateien nutzt.
+Mit dies im Hinterkopf, sind Sie in der Lage, nur diejenigen Dateien zu ändern und hinzuzufügen, die notwendig sind, um Ihre momentanen Bedürfnisse zu erfüllen.
+Dieses Backend-Beispiel kann sich in Zukunft ändern, um mehr echte Dateien für die Backend-Generierung vorzuhalten.
+Momentan enthält es nur eine Index-Template-Datei, die relevante Informationen und Assets auf der Login-Seite entfernt, wenn Sie nicht angemeldet sind.<br>
+* 2017-08-21 - Neu hinzugekommen ist ein Bugfix für die Backend-Ansichten mit "rtl" (right-to-left), auf rechts gedrehten Schrift Attributen im &lt;html&gt; Element.<br><br>
+
+DAS FOLGENDE IST DIE BESCHREIBUNG DES FALLBACK BACKENDS&colon;<br>
+Dieses Theme beherbergt die Styx-Kern Backend Templates im Unterverzeichnis "2k11/admin".
+Die Template-Dateien in diesem Verzeichnis bilden und erstellen das Aussehen der kompletten Admin-Oberfläche.
+Sie enthalten auch einige Workflow- und Logikfunktionen sowie eigene Javascript-Bibliotheken.
+Wenn Sie ein eigenes Backend-Theme verwenden möchten, kopieren Sie das Verzeichnis "admin" in ihr Theme.
+Ändern Sie dort die Datei "info.txt", um eine Zeile "Backend&colon; Yes" hinzuzufügen, und wählen Sie das neue Backend-Theme in der neu geladenen Themenliste aus.
+Ab sofort können Sie die Dateien und Stile des eigenen Backend-Themes bearbeiten und an Ihre erweiterten Bedürfnisse anpassen.<br>
+<u><b>Bitte beachten Sie:</b></u> Dies ist nur für erfahrene Benutzer empfehlenswert und komplett abseits der Update-Funktionen!';
---
+++ templates/2styx/lang_info_en.inc.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * The info.txt native charset (translation) file as an array.
+ * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['languages'].
+ * Convert or save as ANSI (ISO-8859-1) or your native charset, analog to the non-UTF-8 lang constant files.
+ **/
+
+$info['theme_info_summary'] = 'The Styx Backend testing Theme';
+
+$info['theme_info_desc'] = '<u>Please Note&colon;</u> This theme has no existing frontend!<br>
+This example backend theme relegates to the Styx core backend templates in the "2k11/admin" subdirectory.
+As an example it proves the ability that you can simply add a more or less empty backend theme, which uses the fallback to the default "2k11" backend template files.
+With this in mind you are able to just change and add those files only, which are necessary to fill your needs.';
+
+$info['theme_info_backend'] = 'This example backend theme relegates to the Styx core backend templates in the "2k11/admin" subdirectory.
+As an example it proves the ability that you can simply add a more or less empty backend theme, which uses the fallback to the default "2k11" backend template files.
+With this in mind you are able to just change and add those files only, which are necessary to fill your needs.
+This example may change in future to hold some more real files for the backend generation.
+For the moment it solely includes an index template file, which removes relevant informations and assets when not logged in.<br>
+* 2017-08-21 - Newly fixes a UI breaking issue with the rtl (right-to-left) direction attribute in the &lt;html&gt; element.<br><br>
+
+THE FOLLOWING IS THE DESCRIPTION OF THE RELEGATED BACKEND&colon;<br>
+The template files in that directory build and create the look of the complete admin interface.
+They also contain some workflow and logic, as well as the javascript libraries.
+If you want to use your own backend theme, copy the "admin" directory to a theme.
+In there, change the "info.txt" file to add a "Backend&colon; Yes" line and select the new backend theme in the newly loaded theme list.
+From now on, you can edit the files and styles of own backend theme and adapt it to your extending needs.<br>
+<u><b>Please Note&colon;</b></u> This only is recommended for highly experienced users and completely off the update track!';
