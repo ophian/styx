cd0bb5a7147cc31b0900d352479f71e06a1cef7e
Ian <aristophian@googlemail.com>
2017-10-07 17:18:06 +0200

    themes: default - Add translated info parts

---
+++ templates/default/UTF-8/lang_info_de.inc.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * The info.txt UTF-8 charset (translation) file as an array.
+ * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['languages'].
+ * Convert or save as UTF-8 file without BOM.
+ **/
+
+$info['theme_info_summary'] = 'Das Styx Default Template, vormals Serendipity v2.3 genannt (in der Numerierung ohne Bezug auf die Serendipity Version).';
+
+$info['theme_info_desc'] = 'Als Theme neu überarbeitet für Styx 2.1+, um es voll "Responsiv" zu machen (3-2-1),
+ohne viel von seinem alten HTML(4) Markup zu ändern.
+Es arbeitet außerdem als vollwertiges "Fallback" für die PHP- und XML-Engine
+und als allgemeiner Datei-Reserve-Pool für alle sonstigen Themes.
+Dieses Template war zuvor nicht dazu bestimmt, eine Vorlage gleich vielen anderen zu sein.
+Da "Bulletproof" und später "2k11" die Serendipity <b>Standard</b> Templates wurden,
+bekam dieses Template seine Aufgabe als Backup- und Fehler-Reserve-Theme zugewiesen,
+wenn etwas zur Smarty-Kompilierung gesucht und nicht in der üblichen Theme
+oder Fallback-Theme-Kaskade gefunden wurde. Es wird aber auch gewählt,
+wenn keine "Engine" (in der info.txt) und keine eigene Stylesheet-Datei (style.css)
+gesetzt sind, wie es zB. für die Default-php und/oder Default-xml-Theme der Fall ist.';
---
+++ templates/default/lang_info_en.inc.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * The info.txt native charset (translation) file as an array.
+ * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['languages'].
+ * Convert or save as ANSI (ISO-8859-1) or your native charset, analog to the non-UTF-8 lang constant files.
+ **/
+
+$info['theme_info_summary'] = 'The Styx default template. Previously named Serendipity v2.3 (w/o relation to core version number).';
+
+$info['theme_info_desc'] = 'New revisited for Styx 2.1 to make it fully responsive (3-2-1),
+without changing much of the "old school" html. Works additionally as a full styled fallback
+for the PHP and XML Engine and as a file fallback pool for several other themes.
+This theme previously wasn\'t meant to be a theme out of hundrets.
+Since "bulletproof" and later on "2k11" became the Serendipity <b>standard</b> template(s),
+this theme was used as a backup and error fallback theme, when something was searched for compile
+and not in your usual theme or the fallback theme cascade. It also is chosen
+if no engine and no style is set, like for the default-php and/or default-xml themes.';
