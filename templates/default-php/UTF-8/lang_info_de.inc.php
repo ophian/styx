80e7ee7d340d30169b24f085930996dfa22cca83
Ian <aristophian@googlemail.com>
2017-10-07 17:19:16 +0200

    themes: default-php - Add translated info parts

---
+++ templates/default-php/UTF-8/lang_info_de.inc.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * The info.txt UTF-8 charset (translation) file as an array.
+ * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['languages'].
+ * Convert or save as UTF-8 file without BOM.
+ **/
+
+$info['theme_info_summary'] = 'Default PHP-Template und responsives HTML5 Theme für das Serendipity Styx Blog System';
+
+$info['theme_info_desc'] = 'Einfaches Theme aus 2006 basierend auf dem alten Default (Serendipity 2.3) Theme,
+neu aufgearbeitet und stark erweitert für die Serendipity Styx Edition.
+(Trotzdem kann es immer noch einige wenige PHP Template Dateien vermissen lassen.)
+Lesen Sie die Theme-Eigene IMPORTANT-TODO Datei für mehr.<br>
+
+<u><b>ACHTUNG&colon;</b></u><br>
+Je weniger (Smarty erweiternde) Plugins verwendet werden, desto mehr funktioniert dies fehlerfrei.<br>
+
+Derzeit ist keine nennenswerte Einschränkung bekannt!';
---
+++ templates/default-php/lang_info_en.inc.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * The info.txt native charset (translation) file as an array.
+ * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['languages'].
+ * Convert or save as ANSI (ISO-8859-1) or your native charset, analog to the non-UTF-8 lang constant files.
+ **/
+
+$info['theme_info_summary'] = 'Default PHP-Template Responsive HTML5 Theme for the Serendipity Styx Blog system';
+
+$info['theme_info_desc'] = 'Simple theme based on the old default theme from 2006, newly refurbished and strongly extended to work with the Serendipity Styx Edition. (May still miss some template files converted to PHP.)
+Read the themes IMPORTANT-TODO file for more.<br>
+
+<u><b>Please note&colon;</b></u><br>
+The less (Smarty extending) plugins used - the more will this work without errors.<br>
+
+Currently there is no known notable restriction!';
