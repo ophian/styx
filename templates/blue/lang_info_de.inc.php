e8c1a4a951f66022dd23371126b4121f1ae505c5
Ian <aristophian@googlemail.com>
2017-10-07 17:14:57 +0200

    themes: blue - Add translated info parts

---
+++ templates/blue/lang_info_de.inc.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * The info.txt charset (translation) file as an array.
+ * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['languages'].
+ * Convert or save as ANSI (ISO-8859-1) or your native charset
+ **/
+
+$info['theme_info_summary'] = 'Rein auf CSS basierendes Theme.';
+
+$info['theme_info_desc'] = 'Einfaches Beispiel Theme aus 2004 das alle Template Dateien des alten Default Fallback (Serendipity 2.3) Themes nutzt. Styling nur mit CSS.';
