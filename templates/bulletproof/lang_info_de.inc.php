250ae29876d7f41f78cc834e68f3e54d456cbee1
Ian <aristophian@googlemail.com>
2017-10-07 17:16:23 +0200

    themes: bulletproof - Add translated info parts

---
+++ templates/bulletproof/lang_info_de.inc.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * The info.txt charset (translation) file as an array.
+ * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['languages'].
+ * Convert or save as ANSI (ISO-8859-1) or your native charset
+ **/
+
+$info['theme_info_summary'] = 'Erweitertes Optionen Theme für optionale 3, 2, oder 1-Spalten Layouts mit mannigfachen Seitenleisten-Kombinationen.';
+
+$info['theme_info_desc'] = 'Dieses Theme war über lange Jahre das Serendipity Standard Theme seit Serendipity v. 1.2.
+Es erlaubt die Einstellung von 4 verschiedenen Farbsätzen und verwendet verschiedene Benutzeroptionen,
+um bestimmte Teile des Frontends, wie Footer oder Formulare, kombinatorisch zu deaktivieren oder zu setzen/verschieben,
+ohne den eigentlich Code von Hand zu berühren.
+Auch erfüllte es über lange Zeit seinen Zweck als Standard-Backend-Theme. Dies ist seit Serendipity 2.0 nicht mehr nutzbar.<br>
+* 2017-04-19 - Removed dead cocomment option and scripting.';
---
+++ templates/bulletproof/lang_info_en.inc.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * The info.txt native charset (translation) file as an array.
+ * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['languages'].
+ * Convert or save as ANSI (ISO-8859-1) or your native charset, analog to the non-UTF-8 lang constant files.
+ **/
+
+$info['theme_info_summary'] = 'Advanced option theme in optional 3, 2, or 1 -column layouts with various sidebar combinations.';
+
+$info['theme_info_desc'] = 'This theme - for long years - was the Serendipity Standard theme since Serendipity v. 1.2.
+It allows to set 4 different colorsets and uses various user options to differently disable or set/move certain parts of the frontend, like footers or forms, without touching code by hand.
+Among these it was used as the default backend theme for a long time. This latter is no longer usable since Serendipity 2.0.<br>
+* 2017-04-19 - Removed dead cocomment option and scripting.';
