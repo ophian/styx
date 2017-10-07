7fe3778c276240ec99173c51e5aebcf22377dbbb
Ian <aristophian@googlemail.com>
2017-10-07 17:11:44 +0200

    themes: 2k11 - Add translated info parts

---
+++ templates/2k11/UTF-8/lang_info_de.inc.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * The info.txt UTF-8 charset (translation) file as an array.
+ * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['languages'].
+ * Convert or save as UTF-8 file without BOM.
+ **/
+
+$info['theme_info_summary'] = 'Das Styx Backend/Frontend Standard Theme';
+
+$info['theme_info_desc'] = 'Dieses Theme definiert den augenblicklichen Standard, der im Serendipity Styx Kern definiert und benutzt wird.
+In Bezug auf die unendlichen Möglichkeiten von Serendipity, ist es das augenblicklich am besten verknüpfte und verzahnte Theme,
+welches die meisten dieser Möglichkeiten so einfach wie möglich innerhalb des eigenen Rahmens abdeckt, so dass man es leicht durchschauen kann.
+<br><br>
+<u><b>Eigenes Theme erstellen</b></u><br>
+Kopieren Sie ein vorhandenes Theme oder fügen Sie einen neuen und eindeutigen Verzeichnisnamen in das Verzeichnis "templates/" ein, zB. "example".
+Fügen Sie eine <b>info.txt</b> Datei mit den Komponenten von 2k11 hinzu, um damit zu beginnen.
+Ändern Sie den Namen, zB. zu "mein Example" und fügen Sie das aktuelle Datum hinzu.<br>
+Wenn Sie ihr neues Theme auf 2k11 zurückgreifen lassen wollen, geben Sie der info.txt Datei nun eine "Engine&colon; 2k11"-Zeile hinzu.
+Wenn Sie diese Zeile nicht nutzen, muss das eigene Theme entweder alle Template-Dateien selbst enthalten, oder ihr Theme wird auf das "Serendipity Default" Template zurückgreifen.<br><br>
+Setzen Sie ensprechende Zeilen "Require Serendipity&colon; 2.0" für eine Voraussetzung, "Backend&colon; Yes",
+wenn Sie ein eigenes Backend nutzen und "Recommended&colon; Yes", wenn Sie es zur empfohlenen Theme-Sektion hinzufügen wollen.<br><br>
+Zurück in der Template-Liste, laden Sie die Seite neu und wählen Sie ihr neues Theme anhand des gegebenen Namens.';
+
+$info['theme_info_backend'] = 'Dieses Theme beherbergt die Styx-Kern Backend Templates im Unterverzeichnis "2k11/admin".
+Die Template-Dateien in diesem Verzeichnis bilden und erstellen das Aussehen der kompletten Admin-Oberfläche.
+Sie enthalten auch einige Workflow- und Logikfunktionen sowie eigene Javascript-Bibliotheken.
+Wenn Sie ein eigenes Backend-Theme verwenden möchten, kopieren Sie das Verzeichnis "admin" in ihr Theme.
+Ändern Sie dort die Datei "info.txt", um eine Zeile "Backend&colon; Yes" hinzuzufügen, und wählen Sie das neue Backend-Theme in der neu geladenen Themenliste aus.
+Ab sofort können Sie die Dateien und Stile des eigenen Backend-Themes bearbeiten und an Ihre erweiterten Bedürfnisse anpassen.<br>
+<u><b>Bitte beachten Sie:</b></u> Dies ist nur für erfahrene Benutzer empfehlenswert und komplett abseits der Update-Funktionen!';
---
+++ templates/2k11/lang_info_en.inc.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * The info.txt native charset (translation) file as an array.
+ * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['languages'].
+ * Convert or save as ANSI (ISO-8859-1) or your native charset, analog to the non-UTF-8 lang constant files.
+ **/
+
+$info['theme_info_summary'] = 'The Styx Backend/Frontend Standard Theme';
+
+$info['theme_info_desc'] = 'This theme defines the current standard, defined and used by the core Serendipity Styx blog system.
+Regarding Serendipitys infinite possibilities, it is the currently best-concatenated and interlocked theme,
+covering most of them as simple as possible in a wide frame, made easy to look through.
+<br><br>
+<u><b>Theme building</b></u><br>
+Copy an existing theme or add a new and unique directory name to the "templates/" directory, eg. "example".
+Add an <b>info.txt</b> file with the components of 2k11 to start with. Change the name, eg. to "my Example" and add the current date.<br>
+Give it an "Engine&colon; 2k11" line if you want it to fall back to 2k11, if not covering all template files itself.
+If not having this line, you either need all template files for yourself, or regard your theme to fall back to the "Serendipity default" theme templates.<br><br>
+Set additional lines "Require Serendipity&colon; 2.0", "Backend&colon; Yes",
+if having an extra backend and "Recommended&colon; Yes", if wanting it to apply to the recommended template section.<br><br>
+Back in your template list, reload the list page and select the new created theme.';
+
+$info['theme_info_backend'] = 'This theme accommodates the Styx core backend templates in the "2k11/admin" subdirectory.
+The template files in that directory build and create the look of the complete admin interface.
+They also contain some workflow and logic, as well as the javascript libraries.
+If you want to use your own backend theme, copy the "admin" directory to a theme.
+In there, change the "info.txt" file to add a "Backend&colon; Yes" line and select the new backend theme in the newly loaded theme list.
+From now on, you can edit the files and styles of own backend theme and adapt it to your extending needs.<br>
+<u><b>Please Note&colon;</b></u> This only is recommended for highly experienced users and completely off the update track!';
