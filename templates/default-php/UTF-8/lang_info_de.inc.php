0d49e25d10cf54c5c7bcb7ea19e5736bb88c775f
Ian <aristophian@googlemail.com>
2017-10-07 18:14:37 +0200

    Correct wrong commited lang variable

--- templates/default-php/UTF-8/lang_info_de.inc.php
+++ templates/default-php/UTF-8/lang_info_de.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * The info.txt UTF-8 charset (translation) file as an array.
- * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['languages'].
+ * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['lang'].
  * Convert or save as UTF-8 file without BOM.
  **/
 
