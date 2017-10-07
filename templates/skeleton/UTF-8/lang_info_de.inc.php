5b4806ba2c2911b48139cd5f2a3a1573dc0a315e
Ian <aristophian@googlemail.com>
2017-10-07 17:24:57 +0200

    themes: skeleton - Add translated info parts

---
+++ templates/skeleton/UTF-8/lang_info_de.inc.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * The info.txt UTF-8 charset (translation) file as an array.
+ * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['languages'].
+ * Convert or save as UTF-8 file without BOM.
+ **/
+
+$info['theme_info_summary'] = 'Ein CSS Theme basierend auf dem CSS Framework Skeleton.';
+
+$info['theme_info_desc'] = 'Skeleton ist schlicht & einfach, eher typografisch orientiert.
+Das Theme nutzt HTML5 und ist Responsiv. Es besitzt nur eine einzige Seitenleiste (auf der rechten Seite) für größere Bildschirmgrößen und verwendet den Kern Navigation.';
---
+++ templates/skeleton/lang_info_en.inc.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * The info.txt native charset (translation) file as an array.
+ * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['languages'].
+ * Convert or save as ANSI (ISO-8859-1) or your native charset, analog to the non-UTF-8 lang constant files.
+ **/
+
+$info['theme_info_summary'] = 'CSS theme based on the CSS framework Skeleton.';
+
+$info['theme_info_desc'] = 'Skeleton is plain & simple, kind of typography-focussed.
+The theme is HTML5 and responsive, it has a single sidebar (on the right) on screens large enough and uses the core nav.';
