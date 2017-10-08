4e0c4cd08c8ef3209a998997d14caca59bc65dae
Ian <aristophian@googlemail.com>
2017-10-08 14:55:45 +0200

    themes: Fix 2styx info [de] translation

--- templates/2styx/UTF-8/lang_info_de.inc.php
+++ templates/2styx/UTF-8/lang_info_de.inc.php
@@ -10,11 +10,11 @@ $info['theme_info_summary'] = 'Das Styx Backend testing Theme';
 $info['theme_info_desc'] = '<u>ACHTUNG&colon;</u> Dieses Theme hat kein eigenes Frontend!<br>
 Dieses Backend Beispiel-Theme verweist auf die Styx Kern Backend Templates im "2k11/admin" Unterverzeichnis.
 Als Beispiel zeigt es die Möglichkeit, ein eigenes und mehr oder weniger leeres Backend Theme zu erstellen, dass als "Fallback" die Standard "2k11" Backend Template Dateien nutzt.
-Mit dies im Hinterkopf, sind Sie in der Lage, nur diejenigen Dateien zu ändern und hinzuzufügen, die notwendig sind, um Ihre momentanen Bedürfnisse zu erfüllen.';
+Dies bedenkend, sind Sie in der Lage, nur diejenigen Dateien zu ändern und hinzuzufügen, die notwendig sind, um Ihre momentanen Bedürfnisse zu erfüllen.';
 
 $info['theme_info_backend'] = 'Dieses Backend Beispiel-Theme verweist auf die Styx Kern Backend Templates im "2k11/admin" Unterverzeichnis.
 Als Beispiel zeigt es die Möglichkeit, ein eigenes und mehr oder weniger leeres Backend Theme zu erstellen, dass als "Fallback" die Standard "2k11" Backend Template Dateien nutzt.
-Mit dies im Hinterkopf, sind Sie in der Lage, nur diejenigen Dateien zu ändern und hinzuzufügen, die notwendig sind, um Ihre momentanen Bedürfnisse zu erfüllen.
+Dies bedenkend, sind Sie in der Lage, nur diejenigen Dateien zu ändern und hinzuzufügen, die notwendig sind, um Ihre momentanen Bedürfnisse zu erfüllen.
 Dieses Backend-Beispiel kann sich in Zukunft ändern, um mehr echte Dateien für die Backend-Generierung vorzuhalten.
 Momentan enthält es nur eine Index-Template-Datei, die relevante Informationen und Assets auf der Login-Seite entfernt, wenn Sie nicht angemeldet sind.<br>
 * 2017-08-21 - Neu hinzugekommen ist ein Bugfix für die Backend-Ansichten mit "rtl" (right-to-left), auf rechts gedrehten Schrift Attributen im &lt;html&gt; Element.<br><br>
