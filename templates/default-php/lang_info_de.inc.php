ed088cb6860ef7c5134625bb1a4473a6af986028
Ian <aristophian@googlemail.com>
2017-10-08 14:56:21 +0200

    themes: Fix default-php info header fix

--- templates/default-php/lang_info_de.inc.php
+++ templates/default-php/lang_info_de.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * The info.txt charset (translation) file as an array.
- * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['languages'].
+ * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['lang'].
  * Convert or save as ANSI (ISO-8859-1) or your native charset
  **/
 
