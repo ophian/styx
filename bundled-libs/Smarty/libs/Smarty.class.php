e537e1fea94048e2069def49a9ac286a4d82f39a
Ian <aristophian@googlemail.com>
2017-10-26 11:23:47 +0200

    Smarty: Sync Smarty 3.1.32-dev-28

--- bundled-libs/Smarty/libs/Smarty.class.php
+++ bundled-libs/Smarty/libs/Smarty.class.php
@@ -100,7 +100,7 @@ class Smarty extends Smarty_Internal_TemplateBase
     /**
      * smarty version
      */
-    const SMARTY_VERSION = '3.1.32-dev-27';
+    const SMARTY_VERSION = '3.1.32-dev-28';
     /**
      * define variable scopes
      */
