fe847ee575365c980324f35f687985acd1711fcc
Ian <aristophian@googlemail.com>
2017-11-04 13:18:14 +0100

    plugins: Remove core plugin templatedropdown to Spartacus

--- plugins/serendipity_plugin_templatedropdown/UTF-8/lang_de.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-@define('PLUGIN_TEMPLATEDROPDOWN_NAME', 'Theme dropdown');
-@define('PLUGIN_TEMPLATEDROPDOWN_DESC', 'Zeigt eine Box um Themes zu ändern');
-@define('PLUGIN_TEMPLATEDROPDOWN_SUBMIT', 'Submit-Button?');
-@define('PLUGIN_TEMPLATEDROPDOWN_SUBMIT_DESC', 'Einen Submit-Button anzeigen?');
-
--- plugins/serendipity_plugin_templatedropdown/lang_zh.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-@define('PLUGIN_TEMPLATEDROPDOWN_NAME', '主题选择');
-@define('PLUGIN_TEMPLATEDROPDOWN_DESC', '显示一个选择主题的下拉框');
-@define('PLUGIN_TEMPLATEDROPDOWN_SUBMIT', '提交按钮');
-@define('PLUGIN_TEMPLATEDROPDOWN_SUBMIT_DESC', '是否显示一个提交按钮（用于切换所选择的主题）？');
-
