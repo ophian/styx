ffb3809140c366d59b35945fa30e7765102c2c73
Ian <aristophian@googlemail.com>
2018-04-14 12:54:03 +0200

    changelog: Fix typo

--- plugins/serendipity_event_changelog/lang_en.inc.php
+++ plugins/serendipity_event_changelog/lang_en.inc.php
@@ -2,7 +2,7 @@
 
 @define('PLUGIN_CHANGELOG_TITLE', 'Serendipity-log Reader');
 @define('PLUGIN_CHANGELOG_DESC', 'Be able to easily read the Serendipity ChangeLog and optionally active set Error/Debug-Log files.');
-@define('PLUGIN_CHANGELOG_TITLE_DESC', 'Please carefully read the Serendipity ChangeLog for the current version: "%s", to asure yourself that there is nothing to do for you within custom themes or plugins...');
+@define('PLUGIN_CHANGELOG_TITLE_DESC', 'Please carefully read the Serendipity ChangeLog for the current version: "%s", to assure yourself that there is nothing to do for you within custom themes or plugins...');
 
 @define('PLUGIN_CHANGELOG_MAINTAIN', 'View Serendipity Logfiles');
 @define('PLUGIN_CHANGELOG_BUTTON', 'Open Changelog');
