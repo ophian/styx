7e90c532f87920caad2a274de2d8c216d701b135
Ian <aristophian@googlemail.com>
2018-05-12 18:11:07 +0200

    skeleton: Improve meta info

--- templates/skeleton/lang_info_de.inc.php
+++ templates/skeleton/lang_info_de.inc.php
@@ -5,7 +5,7 @@
  * Convert or save as ANSI (ISO-8859-1) or your native charset
  **/
 
-$info['theme_info_summary'] = 'Ein CSS Theme basierend auf dem CSS Framework Skeleton.';
+$info['theme_info_summary'] = 'Ein CSS Theme basierend auf dem CSS Framework Skeleton http://getskeleton.com.';
 
 $info['theme_info_desc'] = 'Skeleton ist schlicht & einfach, eher typografisch orientiert.
 Das Theme nutzt HTML5 und ist Responsiv. Es besitzt nur eine einzige Seitenleiste (auf der rechten Seite) für größere Bildschirmgrößen und verwendet den Kern Navigation.';
--- templates/skeleton/lang_info_en.inc.php
+++ templates/skeleton/lang_info_en.inc.php
@@ -5,7 +5,7 @@
  * Convert or save as ANSI (ISO-8859-1) or your native charset, analog to the non-UTF-8 lang constant files.
  **/
 
-$info['theme_info_summary'] = 'CSS theme based on the CSS framework Skeleton.';
+$info['theme_info_summary'] = 'CSS theme based on the CSS framework Skeleton http://getskeleton.com.';
 
 $info['theme_info_desc'] = 'Skeleton is plain & simple, kind of typography-focussed.
 The theme is HTML5 and responsive, it has a single sidebar (on the right) on screens large enough and uses the core nav.';
