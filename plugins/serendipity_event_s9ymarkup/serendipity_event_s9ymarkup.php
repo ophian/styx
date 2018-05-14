ophian/styx

commit 154f8a0491844e9cee6e12c7caeaaf5f8dae4e9f

Try to fix non-working s9ymarkup asterix-bold conversion

plugins/serendipity_event_s9ymarkup/serendipity_event_s9ymarkup.php
@@ -18,7 +18,7 @@ class serendipity_event_s9ymarkup extends serendipity_event
         $propbag->add('description',   PLUGIN_EVENT_S9YMARKUP_DESC);
         $propbag->add('stackable',     false);
         $propbag->add('author',        'Serendipity Team');
-        $propbag->add('version',       '1.4');
+        $propbag->add('version',       '1.5');
         $propbag->add('requirements',  array(
             'serendipity' => '1.6',
             'smarty'      => '2.6.7',
@@ -124,12 +124,13 @@ class serendipity_event_s9ymarkup extends serendipity_event
         $text = str_replace(chr(1), '\_', $text);
 
         // bold
-        $text = str_replace('\*',chr(1), $text);
-        $text = str_replace('**',chr(2), $text);
+        /*$text = str_replace('\*', chr(1), $text);
+        $text = str_replace('**', chr(2), $text);
         $text = preg_replace('/(\S)\*(\S)/','\1' . chr(1) . '\2', $text);
         $text = preg_replace('/\B\*([^*]+)\*\B/','<strong>\1</strong>', $text);
-        $text = str_replace(chr(2),'**', $text);
-        $text = str_replace(chr(1),'\*', $text);
+        $text = str_replace(chr(2), '**', $text);
+        $text = str_replace(chr(1), '\*', $text);*/
+        $text = preg_replace('/\*{1,2}(.*?)\*{1,2}/', '<strong>\1</strong>',  $text);
 
         // $text = preg_replace('/\|([0-9a-fA-F]+?)\|([\S ]+?)\|/', '<font color="\1">\2</font>',$text);
         $text = preg_replace('/\^([[:alnum:]]+?)\^/','<sup>\1</sup>', $text);

commit f794e644b86361fa54b250f9571374de2bf9b0bd

Fix a regression in 154f8a0

plugins/serendipity_event_s9ymarkup/serendipity_event_s9ymarkup.php
@@ -131,6 +131,7 @@ class serendipity_event_s9ymarkup extends serendipity_event
         $text = str_replace(chr(2), '**', $text);
         $text = str_replace(chr(1), '\*', $text);*/
         $text = preg_replace('/\*{1,2}(.*?)\*{1,2}/', '<strong>\1</strong>',  $text);
+        $text = str_replace('<strong></strong>', '**', $text);
 
         // $text = preg_replace('/\|([0-9a-fA-F]+?)\|([\S ]+?)\|/', '<font color="\1">\2</font>',$text);
         $text = preg_replace('/\^([[:alnum:]]+?)\^/','<sup>\1</sup>', $text);