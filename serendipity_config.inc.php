da88e70041d4d55dcbbc0284eacdabdbf97c3169
Ian <aristophian@googlemail.com>
2018-05-15 11:35:51 +0200

    Release 2.5.0

--- serendipity_config.inc.php
+++ serendipity_config.inc.php
@@ -47,7 +47,7 @@ if (defined('USE_MEMSNAP')) {
 }
 
 // The version string
-$serendipity['version'] = '2.5-beta1';
+$serendipity['version'] = '2.5.0';
 $serendipity['edition'] = 'Styx';
 
 // Setting this to 'false' will enable debugging output.
