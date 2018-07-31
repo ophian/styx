c6086fad8fa911c649a83147c0b0df5ec844f243
Ian <aristophian@googlemail.com>
2018-07-31 14:30:27 +0200

    installer: Extend 3677441

--- include/admin/installer.inc.php
+++ include/admin/installer.inc.php
@@ -104,6 +104,8 @@ $install_token_file = realpath(dirname(__FILE__) . '/../../') . '/install_token.
 if (file_exists($install_token_file) && is_readable($install_token_file) && filesize($install_token_file) > 0) {
     if (preg_match('@install_token\s*=\s*[\'"]([a-z\.0-9]+)[\'"];@imsU', file_get_contents($install_token_file), $tokenmatch)) {
         $install_token = $tokenmatch[1];
+    } else {
+        $data['install_blank'] = true;
     }
 } else {
     // does not exist yet
