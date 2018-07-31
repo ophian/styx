ophian/styx

commit ade7515dd68b40278df34f77d69fd2713938a3ba

Fix PHP 7.2 constant warning of future incompatibility

include/admin/overview.inc.php
@@ -9,7 +9,7 @@ $output = array(); // init backend_frontpage_display hook array
 $output['probe'] = '';
 
 // Alert non accessible SQLite database on login
-if (isset($serendipity['POST']['admin']['user']) && stristr($serendipity['dbType'], 'sqlite') && S9Y_DB_INCLUDED) {
+if (isset($serendipity['POST']['admin']['user']) && stristr($serendipity['dbType'], 'sqlite') && (defined('S9Y_DB_INCLUDED') && S9Y_DB_INCLUDED === true)) {
     $errs  = array();
     $probe = array('dbName' => $serendipity['dbName']);
     serendipity_db_probe($probe, $errs);

include/functions_installer.inc.php
@@ -667,7 +667,7 @@ function serendipity_checkInstallation() {
     // For shared installations, probe the file on include path
     //include_once(S9Y_INCLUDE_PATH . 'include/db/db.inc.php');
 
-    if (S9Y_DB_INCLUDED) {
+    if (defined('S9Y_DB_INCLUDED') && S9Y_DB_INCLUDED === true) {
         serendipity_db_probe($_POST, $errs);
     }
 

commit a5fd624bcd4d86834597ce0a6f87e71267918b27

installer: Fix regression in ade7515 which caused SQL install to fail

include/admin/overview.inc.php
@@ -9,7 +9,7 @@ $output = array(); // init backend_frontpage_display hook array
 $output['probe'] = '';
 
 // Alert non accessible SQLite database on login
-if (isset($serendipity['POST']['admin']['user']) && stristr($serendipity['dbType'], 'sqlite') && (defined('S9Y_DB_INCLUDED') && S9Y_DB_INCLUDED === true)) {
+if (isset($serendipity['POST']['admin']['user']) && stristr($serendipity['dbType'], 'sqlite') && @S9Y_DB_INCLUDED) {
     $errs  = array();
     $probe = array('dbName' => $serendipity['dbName']);
     serendipity_db_probe($probe, $errs);

include/functions_installer.inc.php
@@ -668,7 +668,7 @@ function serendipity_checkInstallation() {
     // For shared installations, probe the file on include path
     //include_once(S9Y_INCLUDE_PATH . 'include/db/db.inc.php');
 
-    if (defined('S9Y_DB_INCLUDED') && S9Y_DB_INCLUDED === true) {
+    if (@S9Y_DB_INCLUDED) {
         serendipity_db_probe($_POST, $errs);
     }
 