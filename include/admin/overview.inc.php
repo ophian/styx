ophian/styx

commit ade7515dd68b40278df34f77d69fd2713938a3ba

Fix PHP 7.2 constant warning of future incompatibility

include/admin/overview.inc.php
@@ -9,7 +9,7 @@ $output = array(); // init backend_frontpage_display hook array
 $output['probe'] = '';
 
 // Alert non accessible SQLite database on login
-if (isset($serendipity['POST']['admin']['user']) && stristr($serendipity['dbType'], 'sqlite') && S9Y_DB_INCLUDED) {
+if (isset($serendipity['POST']['admin']['user']) && stristr($serendipity['dbType'], 'sqlite') && (defined('S9Y_DB_INCLUDED') && S9Y_DB_INCLUDED === true)) {
     $errs  = array();
     $probe = array('dbName' => $serendipity['dbName']);
     serendipity_db_probe($probe, $errs);

include/functions_installer.inc.php
@@ -667,7 +667,7 @@ function serendipity_checkInstallation() {
     // For shared installations, probe the file on include path
     //include_once(S9Y_INCLUDE_PATH . 'include/db/db.inc.php');
 
-    if (S9Y_DB_INCLUDED) {
+    if (defined('S9Y_DB_INCLUDED') && S9Y_DB_INCLUDED === true) {
         serendipity_db_probe($_POST, $errs);
     }
 

commit 072d19518922a7ba8d511fe3265b5dd05a3fb41a

installer: Fix a5fd624 for ade7515 caused by unfinished cbe0771 simplification

include/admin/overview.inc.php
@@ -9,7 +9,7 @@ $output = array(); // init backend_frontpage_display hook array
 $output['probe'] = '';
 
 // Alert non accessible SQLite database on login
-if (isset($serendipity['POST']['admin']['user']) && stristr($serendipity['dbType'], 'sqlite') && @S9Y_DB_INCLUDED) {
+if (isset($serendipity['POST']['admin']['user']) && stristr($serendipity['dbType'], 'sqlite') && (defined('S9Y_DB_INCLUDED') && S9Y_DB_INCLUDED === true)) {
     $errs  = array();
     $probe = array('dbName' => $serendipity['dbName']);
     serendipity_db_probe($probe, $errs);

include/db/db.inc.php
@@ -7,13 +7,12 @@ if ($serendipity['dbType'] == 'mysql' && (version_compare(PHP_VERSION, '5.5.0') 
     $serendipity['dbType'] = 'mysqli';
 }
 
-
-if (@include(S9Y_INCLUDE_PATH . "include/db/{$serendipity['dbType']}.inc.php")) {
-    @define('S9Y_DB_INCLUDED', TRUE);
+if (!empty($serendipity['dbType']) && include(S9Y_INCLUDE_PATH . "include/db/{$serendipity['dbType']}.inc.php")) {
+    define('S9Y_DB_INCLUDED', true);
 }
 // DEV-NOTE: Has known issues in the generic wrapper file with some queries using attributes from serendipity_db_query(), eg fetching $template_option(s).
 #include_once(S9Y_INCLUDE_PATH . "include/db/generic.inc.php");
-#define('S9Y_DB_INCLUDED', TRUE);
+#define('S9Y_DB_INCLUDED', true);
 
 /**
  * Perform a query to update the data of a certain table row

include/functions.inc.php
@@ -13,7 +13,9 @@ if (defined('S9Y_FRAMEWORK_FUNCTIONS')) {
 
 $serendipity['imageList'] = array();
 
-include_once(S9Y_INCLUDE_PATH . 'include/db/db.inc.php');
+if (IS_installed === true) {
+    include_once(S9Y_INCLUDE_PATH . 'include/db/db.inc.php');
+}
 include_once(S9Y_INCLUDE_PATH . 'include/compat.inc.php');
 include_once(S9Y_INCLUDE_PATH . 'include/functions_config.inc.php');
 include_once(S9Y_INCLUDE_PATH . 'include/plugin_api.inc.php');

include/functions_installer.inc.php
@@ -662,13 +662,12 @@ function serendipity_checkInstallation() {
     }
 
     $serendipity['dbType'] = preg_replace('@[^a-z0-9-]@imsU', '', $_POST['dbType']);
-    // Probe database
-    // (do it after the dir stuff, as we need to be able to create the sqlite database)
-    include_once(S9Y_INCLUDE_PATH . "include/db/{$serendipity['dbType']}.inc.php");
+    // Include the database for the first time
     // For shared installations, probe the file on include path
-    //include_once(S9Y_INCLUDE_PATH . 'include/db/db.inc.php');
+    include_once(S9Y_INCLUDE_PATH . 'include/db/db.inc.php');
 
-    if (@S9Y_DB_INCLUDED) {
+    // possible failure
+    if (defined('S9Y_DB_INCLUDED') && S9Y_DB_INCLUDED === true) {
         serendipity_db_probe($_POST, $errs);
     }
 