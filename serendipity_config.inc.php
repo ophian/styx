8d6df35450aeaea80b6c860ab9635ec59ea4a802
Ian <aristophian@googlemail.com>
2018-09-04 20:04:36 +0200

    Internally release 2.6-rc1

--- serendipity_config.inc.php
+++ serendipity_config.inc.php
@@ -47,7 +47,7 @@ if (defined('USE_MEMSNAP')) {
 }
 
 // The version string
-$serendipity['version'] = '2.6-beta2';
+$serendipity['version'] = '2.6-rc1';
 $serendipity['edition'] = 'Styx';
 
 // Setting this to 'false' will enable debugging output.
