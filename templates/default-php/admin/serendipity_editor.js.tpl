ophian/styx

commit bd39d51a587768f9df069531c7c95d2271e50f5d

Sync: If (backend) js is broken, fallback to open configuration options

templates/default-php/admin/serendipity_editor.js.tpl
@@ -1432,7 +1432,7 @@ $(function() {
             e.preventDefault();
         });
 
-        $('.show_config_option_now').click();
+        $('.show_config_option:not(.show_config_option_now)').click();
     }
 
     $('.change_preview').change(function() {

templates/default/admin/serendipity_editor.js
@@ -1375,7 +1375,7 @@ $(function() {
             e.preventDefault();
         });
 
-        $('.show_config_option_now').click();
+        $('.show_config_option:not(.show_config_option_now)').click();
     }
 
     $('.change_preview').change(function() {

templates/default/admin/serendipity_editor.js.php
@@ -1431,7 +1431,7 @@ $(function() {
             e.preventDefault();
         });
 
-        $('.show_config_option_now').click();
+        $('.show_config_option:not(.show_config_option_now)').click();
     }
 
     $('.change_preview').change(function() {

templates/default/admin/serendipity_editor.js.tpl
@@ -1431,7 +1431,7 @@ $(function() {
             e.preventDefault();
         });
 
-        $('.show_config_option_now').click();
+        $('.show_config_option:not(.show_config_option_now)').click();
     }
 
     $('.change_preview').change(function() {

templates/2k11/admin/serendipity_editor.js.tpl
@@ -1431,7 +1431,7 @@ $(function() {
             e.preventDefault();
         });
 
-        $('.show_config_option_now').click();
+        $('.show_config_option:not(.show_config_option_now)').click();
     }
 
     $('.change_preview').change(function() {

templates/2k11/admin/config_template.tpl
@@ -22,7 +22,7 @@
             {/if}
         {/if}
 
-            <div id="el{$category@index}" class="config_optiongroup{if isset($config_groupkeys) AND $config_groupkeys@last} config_optiongroup_last{/if} additional_info option_list">
+            <div id="el{$category@index}" class="config_optiongroup{if isset($config_groupkeys) AND $config_groupkeys@last} config_optiongroup_last{/if} option_list">
                 <legend class="visuallyhidden">{$category.description}</legend>
         {foreach $category.items AS $item}
             {cycle assign='zebra_class' values='odd,even'}

commit c884d344f5f573e5dfbe94aed0f75c8dbf9fa764

backend: Fix another regression by bd39d51 for the full config group only

templates/default-php/admin/serendipity_editor.js.tpl
@@ -1400,6 +1400,7 @@ $(function() {
     // Collapsible configuration elements
     if ($('#serendipity_config_options, #serendipity_category, #image_directory_edit_form').length > 0) {
         var optsCollapsed = true;
+        var defaultConfigState = true;
 
         $('.show_config_option').click(function(e) {
             var $el = $(this);
@@ -1435,9 +1436,19 @@ $(function() {
             }
             $(this).toggleClass('active');
             e.preventDefault();
+            defaultConfigState = false;
         });
 
-        $('.show_config_option:not(.show_config_option_hide)').click();
+        if (defaultConfigState && $('#serendipity_config_options').length > 0) {
+            // Be strict to the default Serendipity configuration groups state only
+            $('#show_config_all').click(); // Yes, click twice to open and close the config groups,
+            $('#show_config_all').click(); // to remove and reset possible default wrong icon switches which occur with the else click part
+            // Now reopen and reset the icon-dir for the first group
+            $('#optionel1').find('span').removeClass('icon-right-dir').addClass('icon-down-dir');
+            $('#el0').removeClass('additional_info');
+        } else {
+            $('.show_config_option:not(.show_config_option_hide)').click();
+        }
     }
 
     $('.change_preview').change(function() {

templates/default/admin/serendipity_editor.js
@@ -1343,6 +1343,7 @@ $(function() {
     // Collapsible configuration elements
     if ($('#serendipity_config_options, #serendipity_category, #image_directory_edit_form').length > 0) {
         var optsCollapsed = true;
+        var defaultConfigState = true;
 
         $('.show_config_option').click(function(e) {
             var $el = $(this);
@@ -1378,9 +1379,19 @@ $(function() {
             }
             $(this).toggleClass('active');
             e.preventDefault();
+            defaultConfigState = false;
         });
 
-        $('.show_config_option:not(.show_config_option_hide)').click();
+        if (defaultConfigState && $('#serendipity_config_options').length > 0) {
+            // Be strict to the default Serendipity configuration groups state only
+            $('#show_config_all').click(); // Yes, click twice to open and close the config groups,
+            $('#show_config_all').click(); // to remove and reset possible default wrong icon switches which occur with the else click part
+            // Now reopen and reset the icon-dir for the first group
+            $('#optionel1').find('span').removeClass('icon-right-dir').addClass('icon-down-dir');
+            $('#el0').removeClass('additional_info');
+        } else {
+            $('.show_config_option:not(.show_config_option_hide)').click();
+        }
     }
 
     $('.change_preview').change(function() {

templates/default/admin/serendipity_editor.js.php
@@ -1399,6 +1399,7 @@ $(function() {
     // Collapsible configuration elements
     if ($('#serendipity_config_options, #serendipity_category, #image_directory_edit_form').length > 0) {
         var optsCollapsed = true;
+        var defaultConfigState = true;
 
         $('.show_config_option').click(function(e) {
             var $el = $(this);
@@ -1434,9 +1435,19 @@ $(function() {
             }
             $(this).toggleClass('active');
             e.preventDefault();
+            defaultConfigState = false;
         });
 
-        $('.show_config_option:not(.show_config_option_hide)').click();
+        if (defaultConfigState && $('#serendipity_config_options').length > 0) {
+            // Be strict to the default Serendipity configuration groups state only
+            $('#show_config_all').click(); // Yes, click twice to open and close the config groups,
+            $('#show_config_all').click(); // to remove and reset possible default wrong icon switches which occur with the else click part
+            // Now reopen and reset the icon-dir for the first group
+            $('#optionel1').find('span').removeClass('icon-right-dir').addClass('icon-down-dir');
+            $('#el0').removeClass('additional_info');
+        } else {
+            $('.show_config_option:not(.show_config_option_hide)').click();
+        }
     }
 
     $('.change_preview').change(function() {

templates/default/admin/serendipity_editor.js.tpl
@@ -1399,6 +1399,7 @@ $(function() {
     // Collapsible configuration elements
     if ($('#serendipity_config_options, #serendipity_category, #image_directory_edit_form').length > 0) {
         var optsCollapsed = true;
+        var defaultConfigState = true;
 
         $('.show_config_option').click(function(e) {
             var $el = $(this);
@@ -1434,9 +1435,19 @@ $(function() {
             }
             $(this).toggleClass('active');
             e.preventDefault();
+            defaultConfigState = false;
         });
 
-        $('.show_config_option:not(.show_config_option_hide)').click();
+        if (defaultConfigState && $('#serendipity_config_options').length > 0) {
+            // Be strict to the default Serendipity configuration groups state only
+            $('#show_config_all').click(); // Yes, click twice to open and close the config groups,
+            $('#show_config_all').click(); // to remove and reset possible default wrong icon switches which occur with the else click part
+            // Now reopen and reset the icon-dir for the first group
+            $('#optionel1').find('span').removeClass('icon-right-dir').addClass('icon-down-dir');
+            $('#el0').removeClass('additional_info');
+        } else {
+            $('.show_config_option:not(.show_config_option_hide)').click();
+        }
     }
 
     $('.change_preview').change(function() {