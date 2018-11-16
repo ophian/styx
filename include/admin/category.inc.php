ophian/styx

commit bd39d51a587768f9df069531c7c95d2271e50f5d

Sync: If (backend) js is broken, fallback to open configuration options

templates/default-php/admin/serendipity_editor.js.tpl
@@ -1432,7 +1432,7 @@ $(function() {
             e.preventDefault();
         });
 
-        $('.show_config_option_now').click();
+        $('.show_config_option:not(.show_config_option_now)').click();
     }
 
     $('.change_preview').change(function() {

templates/default/admin/serendipity_editor.js
@@ -1375,7 +1375,7 @@ $(function() {
             e.preventDefault();
         });
 
-        $('.show_config_option_now').click();
+        $('.show_config_option:not(.show_config_option_now)').click();
     }
 
     $('.change_preview').change(function() {

templates/default/admin/serendipity_editor.js.php
@@ -1431,7 +1431,7 @@ $(function() {
             e.preventDefault();
         });
 
-        $('.show_config_option_now').click();
+        $('.show_config_option:not(.show_config_option_now)').click();
     }
 
     $('.change_preview').change(function() {

templates/default/admin/serendipity_editor.js.tpl
@@ -1431,7 +1431,7 @@ $(function() {
             e.preventDefault();
         });
 
-        $('.show_config_option_now').click();
+        $('.show_config_option:not(.show_config_option_now)').click();
     }
 
     $('.change_preview').change(function() {

templates/2k11/admin/config_template.tpl
@@ -22,7 +22,7 @@
             {/if}
         {/if}
 
-            <div id="el{$category@index}" class="config_optiongroup{if isset($config_groupkeys) AND $config_groupkeys@last} config_optiongroup_last{/if} additional_info option_list">
+            <div id="el{$category@index}" class="config_optiongroup{if isset($config_groupkeys) AND $config_groupkeys@last} config_optiongroup_last{/if} option_list">
                 <legend class="visuallyhidden">{$category.description}</legend>
         {foreach $category.items AS $item}
             {cycle assign='zebra_class' values='odd,even'}

templates/2k11/admin/serendipity_editor.js.tpl
@@ -1431,7 +1431,7 @@ $(function() {
             e.preventDefault();
         });
 
-        $('.show_config_option_now').click();
+        $('.show_config_option:not(.show_config_option_now)').click();
     }
 
     $('.change_preview').change(function() {

commit 6e7916522328126c3fd59d885b21bc631f967f8b

backend: Fix semantic toggle regression by bd39d51
Enhances categories lids open/closed defaults, since not remembered by cookies.

include/admin/category.inc.php
@@ -12,6 +12,8 @@ if (!serendipity_checkPermission('adminCategories') && !serendipity_checkPermiss
 
 $admin_category = (!serendipity_checkPermission('adminCategoriesMaintainOthers') ? 'AND (authorid = 0 OR authorid = ' . (int)$serendipity['authorid'] . ')' : '');
 $data = array();
+$data['closed'] = false;
+
 /* Add a new category */
 if (isset($_POST['SAVE']) && serendipity_checkFormToken()) {
     $name = $serendipity['POST']['cat']['name'];
@@ -138,6 +140,7 @@ if ($serendipity['GET']['adminAction'] == 'delete') {
 if ($serendipity['GET']['adminAction'] == 'edit' || $serendipity['GET']['adminAction'] == 'new' || $serendipity['GET']['adminAction'] == 'newSub') {
     if ($serendipity['GET']['adminAction'] == 'edit') {
         $data['edit'] = true;
+        $data['closed'] = true;
         $cid = (int)$serendipity['GET']['cid'];
         $this_cat = serendipity_fetchCategoryInfo($cid);
         $data['category_name'] = $this_cat['category_name'];

include/admin/importers/serendipity.inc.php
@@ -20,7 +20,7 @@ class Serendipity_Import_Serendipity extends Serendipity_Import
     {
         // TODO: I18n!
         return 'Please set the correct settings for host, user, password, name, prefix and charset from your <b>import database</b>
-        <div id="serendipity_config_options"><div class="configuration_group"><button id="toggle" class="show_config_option show_config_option_now" type="button" data-href="#more" title="' . TOGGLE_OPTION . '"><span class="icon-right-dir" aria-hidden="true"></span> ' . MORE . '</button>.
+        <div id="serendipity_config_options"><div class="configuration_group"><button id="toggle" class="show_config_option show_config_option_hode" type="button" data-href="#more" title="' . TOGGLE_OPTION . '"><span class="icon-right-dir" aria-hidden="true"></span> ' . MORE . '</button>.
         <div id="more" class="config_optiongroup additional_info">
         <p>This importer is still a work-in-progress. It can currently import most things of the database. HOWEVER, it can <strong>NOT</strong> import previously installed plugins (including their configuration) or any database tables of installed plugins. Those must be migrated manually. Also, you must use FTP (or any other useful modern transferring protocol) to transfer your uploaded images to the new location.</p>
         <p>Please do a test-run first if you are SQL-savvy. If you encounter any errors, save the message output you get - it will definitely help debugging! This test-run will "Dupe-Check" (show) you two short example tables ("groups" and "authors"), check for an existing current "import_" table and skip the rest, lopping through the selected tables/groups. If you don\'t get any other errors, all should be well for the final run.</p>

templates/default-php/admin/serendipity_editor.js.tpl
@@ -1437,7 +1437,7 @@ $(function() {
             e.preventDefault();
         });
 
-        $('.show_config_option:not(.show_config_option_now)').click();
+        $('.show_config_option:not(.show_config_option_hide)').click();
     }
 
     $('.change_preview').change(function() {

templates/default/admin/category.inc.tpl
@@ -64,7 +64,7 @@
             </div>
 
             <h3 class="toggle_headline">
-                <button class="show_config_option icon_link{if NOT empty($newSub)} show_config_option_now{/if}" type="button" data-href="#category_subcats" title="{$CONST.TOGGLE_OPTION}"><span class="icon-right-dir" aria-hidden="true"></span> {$CONST.PARENT_CATEGORY}</button>
+                <button class="show_config_option icon_link{if NOT empty($newSub)} show_config_option_hide{/if}" type="button" data-href="#category_subcats" title="{$CONST.TOGGLE_OPTION}"><span class="icon-right-dir" aria-hidden="true"></span> {$CONST.PARENT_CATEGORY}</button>
             </h3>
 
             <div id="category_subcats" class="clearfix additional_info">
@@ -99,7 +99,7 @@
             </div>
 
             <h3 class="toggle_headline">
-                <button class="show_config_option icon_link" type="button" data-href="#category_permissions" title="{$CONST.TOGGLE_OPTION}"><span class="icon-right-dir" aria-hidden="true"></span> {$CONST.PERMISSIONS}</button>
+                <button class="show_config_option icon_link{if $closed} show_config_option_hide{/if}" type="button" data-href="#category_permissions" title="{$CONST.TOGGLE_OPTION}"><span class="icon-right-dir" aria-hidden="true"></span> {$CONST.PERMISSIONS}</button>
             </h3>
 
             <div id="category_permissions" class="clearfix additional_info">
@@ -125,7 +125,7 @@
             </div>
 
             <h3 class="toggle_headline">
-                <button class="show_config_option icon_link" type="button" data-href="#category_preview" title="{$CONST.TOGGLE_OPTION}"><span class="icon-right-dir" aria-hidden="true"></span> {$CONST.CATEGORY} {$CONST.IMAGE}</button>
+                <button class="show_config_option icon_link{if $closed} show_config_option_hide{/if}" type="button" data-href="#category_preview" title="{$CONST.TOGGLE_OPTION}"><span class="icon-right-dir" aria-hidden="true"></span> {$CONST.CATEGORY} {$CONST.IMAGE}</button>
             </h3>
 
             <div id="category_preview" class="clearfix additional_info">

templates/default/admin/config_template.tpl
@@ -15,7 +15,7 @@
             {if $allowToggle}
 
                 <h3 class="toggle_headline">
-                    <button id="optionel{$category@iteration}" class="show_config_option icon_link{if $category@first} show_config_option_now{/if}" type="button" data-href="#el{$category@index}" title="{$CONST.TOGGLE_OPTION}"><span class="icon-right-dir" aria-hidden="true"></span> {$category.title}</button>
+                    <button id="optionel{$category@iteration}" class="show_config_option icon_link{if $category@first} show_config_option_hide{/if}" type="button" data-href="#el{$category@index}" title="{$CONST.TOGGLE_OPTION}"><span class="icon-right-dir" aria-hidden="true"></span> {$category.title}</button>
                 </h3>
             {else}
                 <h3>{$category.title}</h3>

templates/default/admin/plugin_config.tpl
@@ -17,7 +17,7 @@
     {foreach $config_groups AS $config_header => $config_groupkeys}
 
         <div class="configuration_group">
-            <h3 class="toggle_headline"><button id="optionel{$config_groupkeys@iteration}" class="show_config_option show_config_option_now" type="button" data-href="#el{$config_groupkeys@iteration}" title="{$CONST.TOGGLE_OPTION}"><span class="icon-right-dir" aria-hidden="true"></span> {$config_header}</button></h3>
+            <h3 class="toggle_headline"><button id="optionel{$config_groupkeys@iteration}" class="show_config_option show_config_option_hide" type="button" data-href="#el{$config_groupkeys@iteration}" title="{$CONST.TOGGLE_OPTION}"><span class="icon-right-dir" aria-hidden="true"></span> {$config_header}</button></h3>
 
             <fieldset id="el{$config_groupkeys@iteration}" class="config_optiongroup{if $config_groupkeys@last} config_optiongroup_last{/if} additional_info">
             {foreach $config_groupkeys AS $config_groupkey}

templates/default/admin/serendipity_editor.js
@@ -1380,7 +1380,7 @@ $(function() {
             e.preventDefault();
         });
 
-        $('.show_config_option:not(.show_config_option_now)').click();
+        $('.show_config_option:not(.show_config_option_hide)').click();
     }
 
     $('.change_preview').change(function() {

templates/default/admin/serendipity_editor.js.php
@@ -1436,7 +1436,7 @@ $(function() {
             e.preventDefault();
         });
 
-        $('.show_config_option:not(.show_config_option_now)').click();
+        $('.show_config_option:not(.show_config_option_hide)').click();
     }
 
     $('.change_preview').change(function() {

templates/default/admin/serendipity_editor.js.tpl
@@ -1436,7 +1436,7 @@ $(function() {
             e.preventDefault();
         });
 
-        $('.show_config_option:not(.show_config_option_now)').click();
+        $('.show_config_option:not(.show_config_option_hide)').click();
     }
 
     $('.change_preview').change(function() {