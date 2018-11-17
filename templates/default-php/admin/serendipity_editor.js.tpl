ophian/styx

commit bd39d51a587768f9df069531c7c95d2271e50f5d

Sync: If (backend) js is broken, fallback to open configuration options

templates/default-php/admin/serendipity_editor.js.tpl
@@ -1432,7 +1432,7 @@ $(function() {
             e.preventDefault();
         });
 
-        $('.show_config_option_now').click();
+        $('.show_config_option:not(.show_config_option_now)').click();
     }
 
     $('.change_preview').change(function() {

templates/default/admin/serendipity_editor.js
@@ -1375,7 +1375,7 @@ $(function() {
             e.preventDefault();
         });
 
-        $('.show_config_option_now').click();
+        $('.show_config_option:not(.show_config_option_now)').click();
     }
 
     $('.change_preview').change(function() {

templates/default/admin/serendipity_editor.js.php
@@ -1431,7 +1431,7 @@ $(function() {
             e.preventDefault();
         });
 
-        $('.show_config_option_now').click();
+        $('.show_config_option:not(.show_config_option_now)').click();
     }
 
     $('.change_preview').change(function() {

templates/default/admin/serendipity_editor.js.tpl
@@ -1431,7 +1431,7 @@ $(function() {
             e.preventDefault();
         });
 
-        $('.show_config_option_now').click();
+        $('.show_config_option:not(.show_config_option_now)').click();
     }
 
     $('.change_preview').change(function() {

templates/2k11/admin/serendipity_editor.js.tpl
@@ -1431,7 +1431,7 @@ $(function() {
             e.preventDefault();
         });
 
-        $('.show_config_option_now').click();
+        $('.show_config_option:not(.show_config_option_now)').click();
     }
 
     $('.change_preview').change(function() {

templates/2k11/admin/config_template.tpl
@@ -22,7 +22,7 @@
             {/if}
         {/if}
 
-            <div id="el{$category@index}" class="config_optiongroup{if isset($config_groupkeys) AND $config_groupkeys@last} config_optiongroup_last{/if} additional_info option_list">
+            <div id="el{$category@index}" class="config_optiongroup{if isset($config_groupkeys) AND $config_groupkeys@last} config_optiongroup_last{/if} option_list">
                 <legend class="visuallyhidden">{$category.description}</legend>
         {foreach $category.items AS $item}
             {cycle assign='zebra_class' values='odd,even'}

commit b7f0d04a71100bd89fa99c9420523c31588a649f

backend: Fix another regression by bd39d51 for plugin/theme config groups only

templates/default-php/admin/serendipity_editor.js.tpl
@@ -1439,7 +1439,11 @@ $(function() {
             defaultConfigState = false;
         });
 
-        if (defaultConfigState && $('#serendipity_config_options').length > 0) {
+        // Since having changed the config_group default state behaviour to "open", to support non-js erroneousness pages - this is to catch all different pages to set the default state
+        if (defaultConfigState && ($('#plugin_options').length > 0 || $('#template_options').length > 0)) {
+            // Plugin and Theme config groups only
+            // void
+        } else if (defaultConfigState && $('#serendipity_config_options').length > 0) {
             // Be strict to the default Serendipity configuration groups state only
             $('#show_config_all').click(); // Yes, click twice to open and close the config groups,
             $('#show_config_all').click(); // to remove and reset possible default wrong icon switches which occur with the else click part

templates/default/admin/serendipity_editor.js
@@ -1382,7 +1382,11 @@ $(function() {
             defaultConfigState = false;
         });
 
-        if (defaultConfigState && $('#serendipity_config_options').length > 0) {
+        // Since having changed the config_group default state behaviour to "open", to support non-js erroneousness pages - this is to catch all different pages to set the default state
+        if (defaultConfigState && ($('#plugin_options').length > 0 || $('#template_options').length > 0)) {
+            // Plugin and Theme config groups only
+            // void
+        } else if (defaultConfigState && $('#serendipity_config_options').length > 0) {
             // Be strict to the default Serendipity configuration groups state only
             $('#show_config_all').click(); // Yes, click twice to open and close the config groups,
             $('#show_config_all').click(); // to remove and reset possible default wrong icon switches which occur with the else click part

templates/default/admin/serendipity_editor.js.php
@@ -1438,7 +1438,11 @@ $(function() {
             defaultConfigState = false;
         });
 
-        if (defaultConfigState && $('#serendipity_config_options').length > 0) {
+        // Since having changed the config_group default state behaviour to "open", to support non-js erroneousness pages - this is to catch all different pages to set the default state
+        if (defaultConfigState && ($('#plugin_options').length > 0 || $('#template_options').length > 0)) {
+            // Plugin and Theme config groups only
+            // void
+        } else if (defaultConfigState && $('#serendipity_config_options').length > 0) {
             // Be strict to the default Serendipity configuration groups state only
             $('#show_config_all').click(); // Yes, click twice to open and close the config groups,
             $('#show_config_all').click(); // to remove and reset possible default wrong icon switches which occur with the else click part

templates/default/admin/serendipity_editor.js.tpl
@@ -1438,7 +1438,11 @@ $(function() {
             defaultConfigState = false;
         });
 
-        if (defaultConfigState && $('#serendipity_config_options').length > 0) {
+        // Since having changed the config_group default state behaviour to "open", to support non-js erroneousness pages - this is to catch all different pages to set the default state
+        if (defaultConfigState && ($('#plugin_options').length > 0 || $('#template_options').length > 0)) {
+            // Plugin and Theme config groups only
+            // void
+        } else if (defaultConfigState && $('#serendipity_config_options').length > 0) {
             // Be strict to the default Serendipity configuration groups state only
             $('#show_config_all').click(); // Yes, click twice to open and close the config groups,
             $('#show_config_all').click(); // to remove and reset possible default wrong icon switches which occur with the else click part