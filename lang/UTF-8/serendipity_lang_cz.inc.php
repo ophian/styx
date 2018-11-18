b3dade4d5e5f84824f6125a4352fe1ff150ee1c9
Ian <aristophian@googlemail.com>
2018-11-18 15:11:59 +0100

    lang: Prep for Zombie Manager

--- lang/UTF-8/serendipity_lang_cz.inc.php
+++ lang/UTF-8/serendipity_lang_cz.inc.php
@@ -1196,3 +1196,9 @@ Run the <b>executor</b> [execute] task as long it appears, to fully convert the 
 
 @define('ADDITIONAL_PROPERTIES_BY_PLUGIN', 'Additional properties by Plugin: %s');
 
+@define('PLUGINMANAGER', 'Zombie Plugins Cleanup Manager');
+@define('PLUGINMANAGER_ZOMB_OK', 'Well done! Old plugin zombies purged!');
+@define('PLUGINMANAGER_LOCALPLUGINS', 'Check for local plugin zombies');
+@define('PLUGINMANAGER_SUBMIT', 'Purge plugin zombies');
+@define('PLUGINMANAGER_INFO', 'Often there are older systems with plugins that were never activated or stored as local zombies in the database and thus never synchronized. Such plugins appear in the list as local plugins when searching for new plugins to install, although a newer version might be found on Spartacus. Such plugin zombies should be deleted so that the latest version can always be accessed in case of need. Sometimes, however, these are developer versions that have only fallen into oblivion. So think carefully before deleting marked plugins.');
+
