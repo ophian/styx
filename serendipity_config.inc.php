96965cfc8bca6d615aafdb52ecd370ac4b51d357
Ian <aristophian@googlemail.com>
2018-11-19 18:28:07 +0100

    Add a temporary 2.6.3 version, though this is a 2.7-betaRC

--- serendipity_config.inc.php
+++ serendipity_config.inc.php
@@ -47,7 +47,7 @@ if (defined('USE_MEMSNAP')) {
 }
 
 // The version string
-$serendipity['version'] = '2.6.2-rc1';
+$serendipity['version'] = '2.6.3';
 $serendipity['edition'] = 'Styx';
 
 // Setting this to 'false' will enable debugging output.
