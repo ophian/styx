60f537b70f0a4adf89d7725f3103123aa07487d4
Ian <aristophian@googlemail.com>
2018-11-21 16:44:54 +0100

    trackback API: Fix uninitialzed variables offset check add to 8a43dd5

--- include/functions_trackbacks.inc.php
+++ include/functions_trackbacks.inc.php
@@ -121,7 +121,7 @@ function _serendipity_send($loc, $data, $contenttype = null) {
 
     if ($target['scheme'] == 'https' && empty($target['port'])) {
        $uri = $target['scheme'] . '://' . $target['host'] . $target['path'] . $target['query'];
-    } elseif (!is_numeric($target['port'])) {
+    } elseif (!isset($target['port']) || !is_numeric($target['port'])) {
        $target['port'] = 80;
        $uri = $target['scheme'] . '://' . $target['host'] . ':' . $target['port'] . $target['path'] . $target['query'];
     }
