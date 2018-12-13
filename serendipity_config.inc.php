d077f13de30eaf4531ad6c5feb7ee8e761657871
Ian <aristophian@googlemail.com>
2018-12-13 14:12:34 +0100

    Bump version internally, though this is a 2.7.0-beta1 in real

--- serendipity_config.inc.php
+++ serendipity_config.inc.php
@@ -47,7 +47,7 @@ if (defined('USE_MEMSNAP')) {
 }
 
 // The version string
-$serendipity['version'] = '2.6.4';
+$serendipity['version'] = '2.6.5';
 $serendipity['edition'] = 'Styx';
 
 // Setting this to 'false' will enable debugging output.
