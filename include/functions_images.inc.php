ophian/styx

commit f7ec657d4b45eac9592a07b1f6dbf8b34136da8f

media: Do not include GET keys with empty value to extraParems
which is used in:
1. Additional media upload button link in the media_toolbar
2. MediaLibrary upload form GET query string
3. As additional data in ML pagination links

This is almost always filter and sort metadata without value, now sorted out to shorten the appended query string.

include/functions_images.inc.php
@@ -2094,6 +2094,7 @@ function serendipity_generateImageSelectorParems($format = 'url') {
     }
 
     foreach($parems AS $param => $value) {
+        if (empty(trim($value))) continue;
         if ($format == 'form') {
             $extraParems .= '<input type="hidden" name="'. $param .'" value="'. serendipity_specialchars($value) .'">'."\n";
         } else {

commit 390b7c3c832c526ae0bf59be6720f81175074edf

ml: Fix regressional manifestation of wrong used rtrim() usage
caused by f7ec657 $extraParems optimization.

Since rtrim($extraParems, '&amp;') does NOT remove trailing '&amp;' only
 - it does as well remove all (trailing) single chars "&,a,m,p,;" too
and so removes the last "a" from "media" in [extraParems] => serendipity[adminModule]=media
after having removed the "&amp;".

include/functions_images.inc.php
@@ -2102,7 +2102,7 @@ function serendipity_generateImageSelectorParems($format = 'url') {
         }
     }
 
-    return rtrim($extraParems, '&amp;');
+    return preg_replace("/&amp;$/", '', $extraParems);
 }
 
 /**