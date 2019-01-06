ebe02543ca748bbf089a8a0f5e16e3116a56ebae
Ian <aristophian@googlemail.com>
2019-01-06 12:06:50 +0100

    spamblock: Move link into sidebar section SETTING, since being one
    Though, its a question of definition. It either is an activity, eg. to emergency stop spam floods, or a special highlighted plugin setting for a general backend behaviour.
    Since it was previously added next to comments it was seen more like a comment bundle action, but the comments page has its own button for spamblock quickjumps.

--- plugins/serendipity_event_spamblock/serendipity_event_spamblock.php
+++ plugins/serendipity_event_spamblock/serendipity_event_spamblock.php
@@ -25,7 +25,7 @@ class serendipity_event_spamblock extends serendipity_event
             'smarty'      => '3.1.0',
             'php'         => '5.3.0'
         ));
-        $propbag->add('version',       '2.12');
+        $propbag->add('version',       '2.13');
         $propbag->add('event_hooks',    array(
             'frontend_saveComment' => true,
             'external_plugin'      => true,
@@ -36,7 +36,7 @@ class serendipity_event_spamblock extends serendipity_event
             'backend_comments_top' => true,
             'backend_view_comment' => true,
             'frontend_display:html:per_entry'  => true,
-            'backend_sidebar_admin_appearance' => true,
+            'backend_sidebar_admin' => true,
         ));
         $propbag->add('configuration', array(
             'killswitch',
@@ -1478,7 +1478,7 @@ switch ($serendipity['GET']['cleanspamsg']) {
                     }
                     break;
 
-                case 'backend_sidebar_admin_appearance':
+                case 'backend_sidebar_admin': // this is section: settings
                     echo '<li><a href="serendipity_admin.php?serendipity[adminModule]=plugins&amp;serendipity[plugin_to_conf]=' . $this->instance . '">' . PLUGIN_EVENT_SPAMBLOCK_TITLE . "</a></li>\n";
                     break;
 
