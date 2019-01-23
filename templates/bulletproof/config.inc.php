5d4db65798dae0cab873136d051befe9e983d36f
Ian <aristophian@googlemail.com>
2019-01-23 11:22:55 +0100

    themes:  Note disabled, since used actually used

--- templates/bulletproof/config.inc.php
+++ templates/bulletproof/config.inc.php
@@ -279,7 +279,7 @@ $template_config = array(
 );
 
 $top = isset($serendipity['smarty_vars']['template_option']) ? $serendipity['smarty_vars']['template_option'] : '';
-#$template_config_groups = NULL;
+#$template_config_groups = NULL; // disabled, since used above
 $template_global_config = array('navigation' => true);
 $template_loaded_config = serendipity_loadThemeOptions($template_config, $top, true);
 serendipity_loadGlobalThemeOptions($template_config, $template_loaded_config, $template_global_config);
