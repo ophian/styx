336dce1927913c4218e68340cda1bf8ca0d7e0a9
Ian <aristophian@googlemail.com>
2019-03-02 12:42:40 +0100

    default: Change content/sidebar toogler
    to use the standard media screen size 1024px with.
    This is a need for engine and full fallback themes.

--- templates/default/default.js
+++ templates/default/default.js
@@ -1,13 +1,17 @@
-/* toggle content/left sidebar markup nodes for responsiveness */
+/* toggle content/left sidebar markup nodes for responsiveness
+ - DO NOT touch for this default engine, but if you need to avoid
+   to happen possible sibling sidebars on the right between 980 and
+   1024 px page width in your COPY theme, change both following sizes
+   to 980. */
 (function ($) {
-    if ($(window).width() < 980) {
+    if ($(window).width() < 1024) {
         $("#serendipityLeftSideBar").before($("#content"));
     }
     else {
         $("#content").before($("#serendipityLeftSideBar"));
     }
     $(window).resize(function() {
-        if ($(window).width() < 980) {
+        if ($(window).width() < 1024) {
             $("#serendipityLeftSideBar").before($("#content"));
             $("#serendipityRightSideBar").before($("#serendipityLeftSideBar"));
         }
