8206464b45cf6c6ae3fb70ee6c665d9600405f10
Ian <aristophian@googlemail.com>
2019-03-26 16:40:08 +0100

    Time to bump Version for upcoming 2.8 Series

--- serendipity_config.inc.php
+++ serendipity_config.inc.php
@@ -47,7 +47,7 @@ if (defined('USE_MEMSNAP')) {
 }
 
 // The version string
-$serendipity['version'] = '2.7.0';
+$serendipity['version'] = '2.8-alpha1';
 $serendipity['edition'] = 'Styx';
 
 // Setting this to 'false' will enable debugging output.
