ece44e9681c0992ccb1a0acfe7a90ac4aed4ccd2
Ian <aristophian@googlemail.com>
2019-04-24 11:57:49 +0200

    backend: Allow s*_setCookie() function to auto-set the HTTPOnly flag
    which was added with PHP 5.2.0.
    Covers author credentials like logins, tokens, etc.

--- include/functions_config.inc.php
+++ include/functions_config.inc.php
@@ -928,7 +928,7 @@ function serendipity_restoreVar(&$source, &$target) {
  * @param   int         Cookie validity (unix timestamp)
  * @return null
  */
-function serendipity_setCookie($name, $value, $securebyprot = true, $custom_timeout = false) {
+function serendipity_setCookie($name, $value, $securebyprot = true, $custom_timeout = false, $httpOnly = true) {
     global $serendipity;
 
     $host = $_SERVER['HTTP_HOST'];
@@ -939,6 +939,7 @@ function serendipity_setCookie($name, $value, $securebyprot = true, $custom_time
         }
     } else {
         $secure = false;
+        if ($httpOnly) $httpOnly = false;
     }
 
     // If HTTP-Hosts like "localhost" are used, current browsers reject cookies.
@@ -951,7 +952,7 @@ function serendipity_setCookie($name, $value, $securebyprot = true, $custom_time
         $custom_timeout = time() + 60*60*24*30;
     }
 
-    setcookie("serendipity[$name]", $value, $custom_timeout, $serendipity['serendipityHTTPPath'], $host, $secure);
+    setcookie("serendipity[$name]", $value, $custom_timeout, $serendipity['serendipityHTTPPath'], $host, $secure, $httpOnly);
     $_COOKIE[$name] = $value;
     $serendipity['COOKIE'][$name] = $value;
 }
