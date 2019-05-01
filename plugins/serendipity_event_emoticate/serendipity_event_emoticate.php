702af9c23a53a2a341b47188ddd81a4b06380b54
Ian <aristophian@googlemail.com>
2019-05-01 14:46:28 +0200

    emoticate: If possible, better not just silence uninitialized vars

--- plugins/serendipity_event_emoticate/serendipity_event_emoticate.php
+++ plugins/serendipity_event_emoticate/serendipity_event_emoticate.php
@@ -18,7 +18,7 @@ class serendipity_event_emoticate extends serendipity_event
         $propbag->add('description',   PLUGIN_EVENT_EMOTICATE_DESC);
         $propbag->add('stackable',     false);
         $propbag->add('author',        'Serendipity Team');
-        $propbag->add('version',       '1.12');
+        $propbag->add('version',       '1.13');
         $propbag->add('requirements',  array(
             'serendipity' => '2.0',
             'smarty'      => '3.1.0',
@@ -186,7 +186,7 @@ class serendipity_event_emoticate extends serendipity_event
                 case 'frontend_display':
                     foreach ($this->markup_elements AS $temp) {
                         if (serendipity_db_bool($this->get_config($temp['name'], 'true')) && isset($eventData[$temp['element']]) &&
-                            !@$eventData['properties']['ep_disable_markup_' . $this->instance] &&
+                            empty($eventData['properties']['ep_disable_markup_' . $this->instance]) &&
                             !isset($serendipity['POST']['properties']['disable_markup_' . $this->instance])) {
                             $element = &$eventData[$temp['element']];
 
