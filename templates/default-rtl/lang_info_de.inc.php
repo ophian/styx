d93ffe8724a2a36c37d3ffb2d4a5715970a92465
Ian <aristophian@googlemail.com>
2019-05-12 15:06:01 +0200

    default-rtl: Remove version from name to the theme info summary

--- templates/default-rtl/lang_info_de.inc.php
+++ templates/default-rtl/lang_info_de.inc.php
@@ -5,6 +5,6 @@
  * Convert or save as ANSI (ISO-8859-1) or your native charset
  **/
 
-$info['theme_info_summary'] = 'Sprachen abhängiges Recht-nach-Links basierendes RTL-Theme [still needs some love!]';
+$info['theme_info_summary'] = 'Sprachen abhängiges Recht-nach-Links basierendes RTL-Theme [still needs some love!] as v0.4';
 
 $info['theme_info_desc'] = 'Einfaches Beispiel Theme aus 2006, rechts-nach-links orientiert, basierend auf dem alten Default (Serendipity 2.3) Theme.';
--- templates/default-rtl/lang_info_en.inc.php
+++ templates/default-rtl/lang_info_en.inc.php
@@ -5,6 +5,6 @@
  * Convert or save as ANSI (ISO-8859-1) or your native charset, analogue to the non-UTF-8 lang constant files.
  **/
 
-$info['theme_info_summary'] = 'Language dependent Right-To-Left written RTL-Theme [still needs some love!]';
+$info['theme_info_summary'] = 'Language dependent Right-To-Left written RTL-Theme [still needs some love!] as v0.4';
 
 $info['theme_info_desc'] = 'Simple example theme from 2006 in right-to-left oriented manner, depending on the old styled default (Serendipity 2.3) theme.';
