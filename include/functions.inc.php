ddcd72363916cce34f6321d2cfee29b5621bbee2
Ian <aristophian@googlemail.com>
2019-05-30 16:36:23 +0200

    users: Improve fda36fa sorting for non-admin case too

--- include/functions.inc.php
+++ include/functions.inc.php
@@ -703,7 +703,7 @@ function serendipity_fetchUsers($user = '', $group = null, $is_count = false) {
                          WHERE " . (isset($group_sql) ? "g.id IN ($group_sql)" : '1=1') . "
                                $where
                                $query_group
-                      ORDER BY a.realname ASC";
+                      ORDER BY a.userlevel DESC, a.realname ASC";
     }
 
     return serendipity_db_query($querystring);
