cbb02a42b2b0ce0937c3b76e84b31d61a7253860
Ian <aristophian@googlemail.com>
2019-06-01 12:16:50 +0200

    Release Serendipity Styx 2.9.0

--- serendipity_config.inc.php
+++ serendipity_config.inc.php
@@ -47,7 +47,7 @@ if (defined('USE_MEMSNAP')) {
 }
 
 // The version string
-$serendipity['version'] = '2.8-rc1';
+$serendipity['version'] = '2.9.0';
 $serendipity['edition'] = 'Styx';
 
 // Setting this to 'false' will enable debugging output.
