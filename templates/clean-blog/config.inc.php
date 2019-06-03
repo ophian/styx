dd1abbefa835a2236051c06b27b7854eeca9033e
Ian <aristophian@googlemail.com>
2019-06-03 17:21:18 +0200

    templates: Use PHP 7 null coalescing operator where appropriate

--- templates/clean-blog/config.inc.php
+++ templates/clean-blog/config.inc.php
@@ -217,7 +217,7 @@ $template_config = array(
 );
 
 // Collapse template options into groups.
-$top = isset($serendipity['smarty_vars']['template_option']) ? $serendipity['smarty_vars']['template_option'] : '';
+$top = $serendipity['smarty_vars']['template_option'] ?? '';
 #$template_config_groups = NULL; // disabled, since used below
 $template_global_config = array('navigation' => true);
 $template_loaded_config = serendipity_loadThemeOptions($template_config, $top, true);
--- templates/timeline/config.inc.php
+++ templates/timeline/config.inc.php
@@ -226,7 +226,7 @@ $template_config = array(
 $FooterSidebarElements = serendipity_plugin_api::count_plugins('footer');
 $serendipity['smarty']->assignByRef('FooterSidebarElements', $FooterSidebarElements);
 
-$top = isset($serendipity['smarty_vars']['template_option']) ? $serendipity['smarty_vars']['template_option'] : '';
+$top = $serendipity['smarty_vars']['template_option'] ?? '';
 #$template_config_groups = NULL; // disabled, since used below
 $template_global_config = array('navigation' => true);
 $template_loaded_config = serendipity_loadThemeOptions($template_config, $top, true);
