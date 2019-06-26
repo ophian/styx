6d03e42a99235686ae081cf7787a5ff6cf31ba1d
Ian <aristophian@googlemail.com>
2019-06-26 11:02:21 +0200

    wysiwyg: Prepare CKEditor ACF allowance for upcoming picture element
    and source tag usage.

--- htmlarea/ckeditor_s9y_config.js
+++ htmlarea/ckeditor_s9y_config.js
@@ -1,6 +1,6 @@
 /**
  * @fileOverview The Serendipity CKEDITOR custom config file:
- *               ckeditor_s9y_config.js, v. 1.21, last modified 2017-04-29 by Ian
+ *               ckeditor_s9y_config.js, v. 1.22, last modified 2019-06-26 by Ian Styx
  */
 
 /**
@@ -59,6 +59,7 @@ CKEDITOR.editorConfig = function( config ) {
     /** SECTION: Extra Allowed Content
         Set placeholder tag cases - elements [attributes]{styles}(classes) to protect ACF removements.
           - Allowed <mediainsert>, <gallery>, <media> tags (imageselectorplus galleries) - which tells ACF to not touch the code!
+          - Allowed <picture> element and the <source> tag for viewport client access - which tells ACF to not touch the code!
           - Allowed <div> is a need for Media Library inserts - which tells ACF to not touch the code!
           - Allowed <p> custom classes - to easier style certain paragraphs!
           - Allowed <ul> listing for styles and classes and <span> to make life a bit easier!
@@ -66,7 +67,7 @@ CKEDITOR.editorConfig = function( config ) {
           - Allow <pre[*attributes](*classes)> for custom attributes/classes in code blocks
     */
     // protect
-    config.extraAllowedContent = 'mediainsert[*]{*}(*);gallery[*]{*}(*);media[*]{*}(*);script[*]{*}(*);audio[*]{*}(*);div[*]{*}(*);p(*);ul{*}(*);span[*]{*}(*);img[height,width];pre[*](*);';
+    config.extraAllowedContent = 'mediainsert[*]{*}(*);gallery[*]{*}(*);media[*]{*}(*);script[*]{*}(*);audio[*]{*}(*);div[*]{*}(*);p(*);ul{*}(*);span[*]{*}(*);picture;source[*]{*}(*);img[height,width];pre[*](*);';
     // Do not use auto paragraphs, added to these allowed tags (only!). Please regard that this was marked deprecated by CKE 4.4.5, but is a need for (our use of) extraAllowedContent - check this again by future versions!
     config.autoParagraph = false; // defaults(true)
 
