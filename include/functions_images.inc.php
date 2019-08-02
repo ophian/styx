84661438c2df2490077bad794452e0706aaf807e
Ian <aristophian@googlemail.com>
2019-08-02 12:25:44 +0200

    images API: Extend f0caadc with missing creating webp thumbs
    Strange!" I thought I had that before....

--- include/functions_images.inc.php
+++ include/functions_images.inc.php
@@ -1379,9 +1379,12 @@ function serendipity_generateVariations() {
             foreach($files AS $f => $file) {
                 if (!in_array($file['extension'], ['jpg', 'jpeg', 'png']) || $file['hotlink'] == 1) continue;
                 $infile  = $outfile = $serendipity['serendipityPath'] . $serendipity['uploadPath'] . $file['path'] . $file['name'] . (empty($file['extension']) ? '' : '.' . $file['extension']);
+                $infileTH  = $outfileTH = $serendipity['serendipityPath'] . $serendipity['uploadPath'] . $file['path'] . $file['name'] . (empty($file['thumbnail_name']) ? '' : '.' . $file['thumbnail_name']) . (empty($file['extension']) ? '' : '.' . $file['extension']);
                 $newfile = serendipity_makeImageVariationPath($outfile, 'webp');
+                $newfileTH = serendipity_makeImageVariationPath($outfileTH, 'webp');
                 $result  = serendipity_convertToWebPFormat($infile, $newfile['filepath'], $newfile['filename'], mime_content_type($outfile), true);
                 if ($result !== false && is_array($result) && $result[0] == 0) {
+                    serendipity_convertToWebPFormat($infileTH, $newfileTH['filepath'], $newfileTH['filename'], mime_content_type($outfileTH), true);
                     ++$i;
                 }
             }
