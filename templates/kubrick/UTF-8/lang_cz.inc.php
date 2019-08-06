962d4497e9f97d90847d377a698ebd062ea80998
Ian <aristophian@googlemail.com>
2019-08-06 09:38:11 +0200

    kubrick: Fixed a missing file cleanup

--- templates/kubrick/UTF-8/lang_cz.inc.php
+++ templates/kubrick/UTF-8/lang_cz.inc.php
@@ -1,10 +1,11 @@
-<?php # lang_cz.inc.php 1.0 2009-08-14 20:10:15 VladaAjgl $
+<?php
 
 /**
  *  @version 1.0
  *  @author Vladimír Ajgl <vlada@ajgl.cz>
  *  @translated 2009/08/14
- */@define('REPLY', 'Odpověď');
+ */
+@define('REPLY', 'Odpověď');
 @define('SAYS', 'říká');
 @define('LINK_TO_COMMENT', 'Odkaz ke komentáři č.%d');
 @define('ENTRY_POSTED_BY', 'Tento příspěvek napsal');
@@ -13,4 +14,4 @@
 @define('SUBJECT_TO_MODERATION', 'Všechny nové komentáře musí být před zobrazením schváleny.');
 @define('COMMENTS_CLOSED', 'Jak komentáře, tak odezvy jsou zakázány.');
 @define('PROUDLY_POWERED_BY', 'Hrdě hlásím, že můj blog běží na');
-?>
+
