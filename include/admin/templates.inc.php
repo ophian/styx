f5ee1f74f5131afe390cb1067d2d3068a6cb363e
Ian <aristophian@googlemail.com>
2019-08-14 08:54:30 +0200

    themes: Add Sliver to core themes list aaray

--- include/admin/templates.inc.php
+++ include/admin/templates.inc.php
@@ -175,7 +175,7 @@ $stack = array();
 serendipity_plugin_api::hook_event('backend_templates_fetchlist', $stack);
 $themes = serendipity_fetchTemplates();
 $data['templates'] = array();
-$core_templates = ['2k11', '2styx', 'bootstrap4', 'bulletproof', 'clean-blog', 'default', 'default-php', 'next', 'skeleton', 'timeline'];
+$core_templates = ['2k11', '2styx', 'bootstrap4', 'bulletproof', 'clean-blog', 'default', 'default-php', 'next', 'skeleton', 'sliver', 'timeline'];
 $data['core_templates'] = array();
 
 foreach($themes AS $theme) {
