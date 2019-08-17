bdfbc9350e664f07843f887a84f04bc4212c53b6
Ian <aristophian@googlemail.com>
2019-08-17 15:07:04 +0200

    sliver: Add theme multilang info property

---
+++ templates/sliver/lang_info_de.inc.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * The info.txt charset (translation) file as an array.
+ * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['lang'].
+ * Convert or save as ANSI (ISO-8859-1) or your native charset
+ **/
+
+$info['theme_info_summary'] = 'Silver (v.4.60) links/rechts, bis zu 4 Seitenleisten, responsives Coding-Theme für das Serendipity Styx Blog System ("ein weiterentwickeltes Bulletproof Theme")';
+
+$info['theme_info_desc'] = 'Seitenleisten Links, Seitenleisten Rechts und ohne Seitenleisten via Template Konfiguration. Zusätzliche Mittlere, Dach- und Fuß- Seitenleisten sind über die Backend Plugin Seitenleisten-Einstellungen konfigurierbar.';
---
+++ templates/sliver/lang_info_en.inc.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * The info.txt native charset (translation) file as an array.
+ * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['lang'].
+ * Convert or save as ANSI (ISO-8859-1) or your native charset, analogue to the non-UTF-8 lang constant files.
+ **/
+
+$info['theme_info_summary'] = 'Silver (v.4.60) slivered left/right, up to 4 Sidebars responsive Coding-Theme for the Serendipity Styx Blog system ("an extended bulletproof theme")';
+
+$info['theme_info_desc'] = 'Sidebars left, Sidebars right and no Sidebars via templates config. Additional middle, top and footer Sidebars via admin panels plugin section.';
