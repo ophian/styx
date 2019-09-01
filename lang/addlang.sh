9cc5627317652e172e371877e7fcb45aa75dfd5f
Ian <aristophian@googlemail.com>
2019-09-01 09:23:15 +0200

    lang: SYNC - addlang.sh: Move input to backup after run
    And check for input file before starting.

--- lang/addlang.sh
+++ lang/addlang.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# addlang.sh: Move input to backup after run.
+# And check for input file before starting.
 
 if [ "x$1" = "x" ] 
  then
@@ -8,7 +10,16 @@ if [ "x$1" = "x" ]
    echo "available language file."
    exit 1
  else
-   find . -maxdepth 1 -name \*.php ! -name recode.php ! -name plugin_lang.php -exec ./append.sh $1 {} \;
+   #find . -maxdepth 1 -name \*.php ! -name recode.php ! -name plugin_lang.php -exec ./append.sh $1 {} \;
+   if [ ! -f $1 ]; then
+    echo "$1 does not exist."
+    exit 10;
+   fi
+   find . -maxdepth 1 -name \*.inc.php -exec ./append.sh $1 {} \;
+   TIMESTAMP=$(date +"%Y%m%d%H%M")
+   mv $1 $1.$TIMESTAMP
    cd UTF-8
    ./recode.sh
+   echo "-------------------------------------------------"
+   echo "Please check the result and remove $1.$TIMESTAMP."
 fi
