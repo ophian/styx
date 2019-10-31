987f2d764780dc5bfd7395f9d65c889d57a3fdf1
Ian <aristophian@googlemail.com>
2019-10-31 18:09:32 +0100

    plugins: Add MLORPHANS plugin
    the main logic goes into core

---
+++ plugins/serendipity_event_mlorphans/UTF-8/lang_de.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+@define('PLUGIN_EVENT_MLORPHANS_NAME', 'Waisenmanager');
+@define('PLUGIN_EVENT_MLORPHANS_DESC', 'Erlaubt, die Mediathek ab 100 vorhandenen Bildern nach verwaisten Bild-Dateien zu durchsuchen. Platzieren Sie es in der Plugin-Liste am besten vor den anderen Wartungsplugins (spamblock, modemaintain, changelog).');
+@define('PLUGIN_EVENT_MLORPHANS_SUBMIT', 'Suche nach Bilder-Waisen');
+
+@define('MLORPHAN_MTASK_ML_REAL_IMAGES', 'Die Mediathek enthält %d echte lokale Bilder.');
+@define('MLORPHAN_MTASK_MAIN_PATTERN_MATCHES', '<b>%d</b> mit ID ausgezeichnete img src Inhalte wurden in den Blogeinträgen und Statischen Seiten gefunden und im ($im) array abgelegt (<em>Doubletten sind möglich</em>).');
+@define('MLORPHAN_MTASK_MAIN_PATTERN_ID_ERROR', 'Bitte korrigieren Sie umgehend: Eintrag: %d  [<em>%s</em> textarea Feld] auf ein <em>voraussichtlich</em> <b>falsch</b> genutztes s9ymdb: ID (%d) tag zu einem Bild src Pfad von "<em>%s</em>" und starten danach dieses Skript erneut. Für die gesuchte Bild ID, durchforsten Sie das Array in der untenstehenden Ausklappbox "view array" nach "<em>%s</em>", um die korrekte Bild ID [id] zu erfahren.');
+@define('MLORPHAN_MTASK_MAIN_PATTERN_RESULTCHECK_ACTION', '<em>Prüfe auf falsch gesetzte Bild-s9ymdb:IDs und/oder auf falsche Namen... und entferne gefundene Bilder aus dem Array...</em>');
+@define('MLORPHAN_MTASK_PNCASE_REVERSECHECK_ACTION', '<em>Letzte Gegenprüfung gegen Datenbankinhalte... und mögliche quickblog Bild tags...</em>');
+@define('MLORPHAN_MTASK_PNCASE_TITLE', 'Wiederholende Überprüfung der gefundenen verwaisten Bild-IDs');
+@define('MLORPHAN_MTASK_PNCASE_NOTE', '<em>die möglicherweise immer noch in Einträgen oder Statischen Seiten vorkommen. Dies sind wahrscheinlich unbekannte regex pattern Treffer, oder Pfade die nicht zu diesem Blog gehören.</em><br>Wenn Sie möchten, überprüfen Sie folgende Einträge und starten Sie dieses Skript erneut.');
+@define('MLORPHAN_MTASK_PNCASE_EXCLUDING', 'Ausschließen dieser Bild-IDs: (<b>%s</b>) vom Waisen-Array.');
+@define('MLORPHAN_MTASK_PNCASE_OK', 'Sieht gut für mich aus...');
+@define('MLORPHAN_MTASK_FOUND_IMAGE_ORPHANS', '<b>%d</b> verbliebene und wahrscheinlich unbenutzte Bild-Waisen-Array-Elemente bleiben übrig.');
+@define('MLORPHAN_MTASK_REMOVE_IMAGE_ORPHANS', 'Sie können nun %d Bilder-Waisen aus der Mediathek löschen!');
+@define('MLORPHAN_MTASK_LAST_ACTION_NOTE', '<b>Achtung:</b> <span>Es <b>können</b> immer noch Bilder in diesem Array und Ihren Einträgen enhalten sein, die <b>keinen</b> dazugehörenden <code>&lt;-- s9ymdb:N --&gt;</code> Mediathek ID tag besitzen. Wenn Sie sich dessen nicht sicher sind, vermeiden Sie die folgende Löschaktion, oder besser, tragen Sie die korrekten tag IDs in diese Einträge ein und starten anschließend dieses Skript neu! (<em>Empfohlen!</em>)</span><br>
+    <span>Dieser Waisenmanager kümmert sich ebenfalls nicht um Datenbank-gecachte Einträge aus den Erweiterte(n) Eigenschaften von Artikeln (plugin entryproperties). Sollten Sie also vorhandene Media tag IDs (siehe mögliche Fehlermeldungen ganz oben) nachträglich in Artikeln haben korrigieren müssen, gilt dies natürlich auch für gecachte Einträge. Wenn Sie diesen Cache also benötigen und benutzen (dies sollte heutzutage aber nicht vonnöten sein), starten sie nach der erfolgreichen Löschung die dazugehörige Wartungsaufgabe um die (EP) Cacheeinträge neu bilden zu lassen, was aber gegebenenfalls andere Probleme zB. mit NL2BR und Zeilenumbrüchen in sehr alten Artikeln nach sich ziehen kann.</span>');
+@define('MLORPHAN_MTASK_PURGED_SUCCESS', 'Erfolgreich gelöschte Bilder nach ID: (<b>%s</b>) in der Datenbank und dem Dateisystem der Mediathek.');
+
---
+++ plugins/serendipity_event_mlorphans/serendipity_event_mlorphans.php
@@ -0,0 +1,120 @@
+<?php
+
+if (IN_serendipity !== true) {
+    die ("Don't hack!");
+}
+
+@serendipity_plugin_api::load_language(dirname(__FILE__));
+
+class serendipity_event_mlorphans extends serendipity_event
+{
+    var $title = PLUGIN_EVENT_MLORPHANS_NAME;
+
+    function introspect(&$propbag)
+    {
+        global $serendipity;
+
+        $propbag->add('name',          PLUGIN_EVENT_MLORPHANS_NAME);
+        $propbag->add('description',   PLUGIN_EVENT_MLORPHANS_DESC);
+        $propbag->add('stackable',     false);
+        $propbag->add('author',        'Ian Styx');
+        $propbag->add('version',       '1.00');
+        $propbag->add('requirements',  array(
+            'serendipity' => '3.0.0',
+            'smarty'      => '3.1.0',
+            'php'         => '7.1.0'
+        ));
+        $propbag->add('event_hooks',    array(
+            'css_backend'         => true,
+            'backend_maintenance' => true
+        ));
+        $propbag->add('groups',         array('BACKEND_ADMIN', 'BACKEND_FEATURES', 'BACKEND_MAINTAIN', 'MAINTENANCE'));
+    }
+
+    function generate_content(&$title)
+    {
+        $title = $this->title;
+    }
+
+    function example()
+    {
+        return '<h3>' . PLUGIN_EVENT_MLORPHANS_NAME . '</h3>' .
+        '<span class="msg_notice">' . PLUGIN_EVENT_MLORPHANS_DESC ."</span>\n";
+    }
+
+    function event_hook($event, &$bag, &$eventData, $addData = null)
+    {
+        global $serendipity;
+
+        $hooks = &$bag->get('event_hooks');
+
+        if (isset($hooks[$event])) {
+
+            switch($event) {
+
+                case 'backend_maintenance':
+                    if (!serendipity_checkPermission('siteConfiguration')) {
+                        return false;
+                    }
+                    $countdImages = @serendipity_db_query("SELECT count(id) FROM {$serendipity['dbPrefix']}images WHERE mime LIKE 'image/%' AND hotlink IS NULL", true, 'num');
+                    $orphanTask   = (serendipity_checkPermission('siteConfiguration') && is_array($countdImages) && $countdImages[0] >= 100) ? true : false;
+
+                    if ($orphanTask) {
+?>
+
+    <section id="maintenance_orphanmanager" class="quick_list">
+        <h3><?=MEDIA_LIBRARY?>: <?=PLUGIN_EVENT_MLORPHANS_NAME?></h3>
+        <a class="button_link" href="?serendipity[action]=admin&amp;serendipity[adminModule]=maintenance&amp;serendipity[adminAction]=imageorphans" title="<?=strtolower(PLUGIN_EVENT_MLORPHANS_SUBMIT)?>"><span><?=PLUGIN_EVENT_MLORPHANS_SUBMIT?></span></a>
+    </section>
+
+<?php
+                    }
+                    break;
+
+                case 'css_backend':
+                    $eventData .= '
+
+/* plugin serendipity_event_mlorphans start */
+
+    #maintenance_orphaned_images { border: 1px solid #ddd; padding: 0 .5em; }
+    #maintenance_orphaned_images .checkbyid { background: #eee; box-sizing: border-box; border: 1px solid #aaa; padding: .5em; }
+    #maintenance_orphaned_images .checkbyid label { font-weight: 600; color: #666; }
+    #maintenance_orphaned_images .compact { display: inline; }
+    #maintenance_orphaned_images legend {
+        background: #ddd;
+        background-image: -webkit-linear-gradient(#fff, #ddd);
+        background-image: linear-gradient(#fff, #ddd);
+        border-bottom: 1px solid #aaa;
+        color: #666;
+        font-size: 1em;
+        margin: -.5em -.5em auto -.5em;
+        padding: .5em 1em; }
+    .msg_notice.orphan h3,
+    #maintenance_orphaned_images .msg_notice.no-margin { margin-top: .5em; margin-bottom: .5em; }
+    #maintenance_orphaned_images .msg_notice.no-margin { font-style: oblique; }
+    #maintenance_orphaned_images pre {
+        width: auto;
+        min-width: 24em;
+        height: 24em;
+        display: inline-grid;
+        overflow: auto;
+        background: #fefefe;
+        border: 1px solid #ccc;
+        margin: .5em auto;
+        padding: .5em; }
+
+/* plugin serendipity_event_mlorphans end */
+';
+                    break;
+
+                default:
+                    return false;
+
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
