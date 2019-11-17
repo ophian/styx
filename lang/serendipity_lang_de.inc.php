31da341c9aa8349ef795dcbb2304672f1b7934d3
Ian <aristophian@googlemail.com>
2019-11-17 12:17:53 +0100

    lang: Improve NOT_WRITABLE_SPARTACUS

--- lang/serendipity_lang_de.inc.php
+++ lang/serendipity_lang_de.inc.php
@@ -963,7 +963,7 @@
 @define('DELETE_FILE_FAIL', 'Kann das Bild namens <b>%s</b> nicht löschen');
 @define('INSTALL_OFFSET_ON_SERVER_TIME', 'Basiert die Zeitdifferenz auf der Server-Zeitzone?');
 @define('INSTALL_OFFSET_ON_SERVER_TIME_DESC', 'Eintragszeiten nach Server-Zeitzone eintragen oder nicht. YES wählt die Server-Zeitzone als Basis, NO wählt GMT.');
-@define('NOT_WRITABLE_SPARTACUS', ' (Nur notwendig wenn Spartacus zur Plugin-Installation über das Internet genutzt werden soll)');
+@define('NOT_WRITABLE_SPARTACUS', ' Empfohlen! (Notwendig, wenn Spartacus zur Plugin-Installation über das Internet genutzt werden soll.)');
 @define('MEDIA_ALT', 'ALT-Attribut (Bildinhalt oder Kurzbeschreibung)');
 @define('MEDIA_PROPERTY_ALT', 'Bildinhalt (im ALT-Attribut)');
 @define('MEDIA_TITLE', 'TITLE-Attribut (wird beim Überfahren mit der Maus angezeigt)');
