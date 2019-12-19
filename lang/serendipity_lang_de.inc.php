125e63c8d3817541021be6ab9efaca9df7abf36b
Ian <aristophian@googlemail.com>
2019-12-19 10:42:35 +0100

    lang: Improve [de] VIEW_EXTENDED_ENTRY

--- lang/serendipity_lang_de.inc.php
+++ lang/serendipity_lang_de.inc.php
@@ -464,7 +464,7 @@
 @define('COMMENT_ALREADY_APPROVED', 'Kommentar #%s wurde bereits bewilligt.');
 @define('COMMENT_EDITED', 'Der gewählte Kommentar wurde bearbeitet.');
 @define('HIDE', 'Ausblenden');
-@define('VIEW_EXTENDED_ENTRY', 'Weiter lesen: "%s"');
+@define('VIEW_EXTENDED_ENTRY', 'Weiter: "%s"');
 @define('TRACKBACK_SPECIFIC_ON_CLICK', 'Dieser Link ist nicht aktiv. Er enthält eine kopierbare Trackback-URI, um manuell ein Ping- und Trackback zu diesem Eintrag für ältere Blogsysteme zu generieren; zB (immer noch valide) über das zur Verfügung gestellte Eintragsfeld des serendipity_event_trackback Plugins. Serendipity und andere Blogsysteme erkennen die Trackback-URL heutzutage aber automatisch anhand der Artikel-URL. Die Trackback-URI für ihren Link des Sender-Eintrages lautet daher wie folgt:');
 @define('THIS_TRACKBACK_NEEDS_REVIEW', 'Hinweis: Dieses Trackback wird ohne Bewilligung nicht dargestellt.');
 @define('DELETE_TRACKBACK', 'Trackback löschen');
