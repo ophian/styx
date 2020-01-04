071e991e4fc844bffdb3ffe7fe234c5d2c0a4bd7
Ian <aristophian@googlemail.com>
2020-01-04 11:05:39 +0100

    plugins: Improve [en, de] ..S9YMARKUP_TRANSFORM for \_

--- plugins/serendipity_event_s9ymarkup/UTF-8/lang_de.inc.php
+++ plugins/serendipity_event_s9ymarkup/UTF-8/lang_de.inc.php
@@ -2,5 +2,5 @@
 
 @define('PLUGIN_EVENT_S9YMARKUP_NAME', 'Textformatierung: Serendipity');
 @define('PLUGIN_EVENT_S9YMARKUP_DESC', 'Standard Serendipity Textformatierung durchführen');
-@define('PLUGIN_EVENT_S9YMARKUP_TRANSFORM', 'Umschließende Sterne heben ein Wort hervor (*wort*), per _wort_ kann ein Wort unterstrichen werden.');
+@define('PLUGIN_EVENT_S9YMARKUP_TRANSFORM', 'Umschließende Sterne heben ein Wort hervor *wort*. Unterstriche für Unterstreichen _wort_. Anderenfalls \_.');
 
--- plugins/serendipity_event_s9ymarkup/lang_en.inc.php
+++ plugins/serendipity_event_s9ymarkup/lang_en.inc.php
@@ -8,5 +8,5 @@
 
 @define('PLUGIN_EVENT_S9YMARKUP_NAME', 'Markup: Serendipity');
 @define('PLUGIN_EVENT_S9YMARKUP_DESC', 'Apply basic serendipity markup to entry text');
-@define('PLUGIN_EVENT_S9YMARKUP_TRANSFORM', 'Enclosing asterisks marks text as bold (*word*), underscore are made via _word_.');
+@define('PLUGIN_EVENT_S9YMARKUP_TRANSFORM', 'Enclosing asterisks marks text as bold (*word*), underscore are made via (_word_), else escape with (\_).');
 
