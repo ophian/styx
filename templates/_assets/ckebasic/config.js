e50284f62bf4205570a06096e200acc807e03f36
Ian <aristophian@googlemail.com>
2020-02-07 10:07:16 +0100

    wysiwyg: Improve and fix new ckebasic core config init for staticpages
    and other normal backend nuggets.

--- templates/_assets/ckebasic/config.js
+++ templates/_assets/ckebasic/config.js
@@ -33,31 +33,37 @@ CKEDITOR.editorConfig = function( config ) {
 	// Dialog windows are also simplified.
 	config.removeDialogTabs = 'link:advanced';
 
-    // Backend Only Area
-    if ($('#serendipityEntry').length > 0) {
+    // Backend Only Area - check blog entries, staticpages and other backend related normal form area nuggets (ie. comment forms have different init need),
+    // like contactform, commentspice, downloadmanager, FAQ, DSGVO / GDPR, guestbook, html nugget, quicknotes, and more.
+    if ($('#serendipityEntry').length > 0 || $('#sp_main_data').length > 0 || $('#backend_sp_simple').length > 0 || $('#serendipity_admin_page .form_area').length > 0) {
+        console.log('STYX fired WYSIWYG: backend entries, staticpages or spawned nuggets');
         // Add Styx specific styles
         config.contentsCss = [ 'templates/_assets/ckebasic/contents.css', 'templates/_assets/wysiwyg-style.css' ];
 
-        // Plugin: Autogrow textareas configuration for Styx
+        config.entities = false; // defaults(true)
+        config.htmlEncodeOutput = false; // defaults(true)
+
+        // Plugin: Autogrow textarea default configuration for Styx
         config.autoGrow_minHeight = 120;
         config.autoGrow_maxHeight = 420;
         config.autoGrow_bottomSpace = 50;
         config.autoGrow_onStartup = true;
 
-        /** SECTION: Extra Allowed Content
-            Set placeholder tag cases - elements [attributes]{styles}(classes) to protect ACF suspensions.
-              - Allowed <mediainsert>, <gallery>, <media> tags (imageselectorplus galleries) - which tells ACF to not touch the code!
-              - Allowed <picture> element and the <source> tag for viewport client access - which tells ACF to not touch the code!
+        /** SECTION: Extra Allowed Content - which tells ACF to not touch the code!
+            Set placeholder tag cases to protect ACF suspensions:
+              - Allowed <mediainsert>, <gallery>, <media> tags (imageselectorplus galleries)
+            Normal ACF suspension tag protects:
+              - Allowed <picture> element and the <source> tag for viewport client access
               - Allowed <figure> styles and classes, <figcaption> classes for image comments
-              - Allowed <div> is a need for Media Library inserts - which tells ACF to not touch the code!
+              - Allowed <div> is a need for Media Library inserts
               - Allowed <p> custom classes - to easier style certain paragraphs!
-              - Allowed <ul> listing for styles and classes and <span> to make life a bit easier!
-              - Allowed <a> link tag attributes and classes for having to add data-* attributes (see picture element) - which tells ACF to not touch the code!
-              - Allow <img> [attributes]{styles}(classes) Media Library image inserts to protect ACF suspension - which tells ACF to not touch the code!
-              - Allow <code(*classes)>, <pre[*attributes](*classes)> for custom attributes/classes in code blocks - which tells ACF to not touch the code!
+              - Allowed <ul> listing for styles and classes, <hr> and <span> to make life a bit easier!
+              - Allowed <a> link tag attributes and classes for having to add data-* attributes (see picture element)
+              - Allowed <img> [attributes]{styles}(classes) Media Library image inserts to protect ACF suspension
+              - Allowed <code(*classes)>, <pre[*attributes](*classes)> for custom attributes/classes in code blocks
         */
-        // protect
-        config.extraAllowedContent = 'mediainsert[*]{*}(*);gallery[*]{*}(*);media[*]{*}(*);audio[*]{*}(*);div[*]{*}(*);p(*);ul{*}(*);a[*](*);span[*]{*}(*);figure{*}(*);figcaption(*);picture;source[*]{*}(*);img[*]{*}(*);code(*);pre[*](*);';
+        // protect - elements [attributes]{styles}(classes)
+        config.extraAllowedContent = 'mediainsert[*]{*}(*);gallery[*]{*}(*);media[*]{*}(*);audio[*]{*}(*);div[*]{*}(*);p(*);ul{*}(*);a[*](*);span[*]{*}(*);figure{*}(*);figcaption(*);picture;source[*]{*}(*);img[*]{*}(*);code(*);hr;pre[*](*);';
         // Do not use auto paragraphs, added to these allowed tags (only!). Please regard that this was marked deprecated by CKE 4.4.5, but is a need for (our use of) extraAllowedContent - check this again by future versions!
         config.autoParagraph = false; // defaults(true)
     }
