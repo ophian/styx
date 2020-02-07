bafef08c8e8991f04853fea6b9b780808f0e1c82
Ian <aristophian@googlemail.com>
2020-02-07 17:23:28 +0100

    history: Revert b093de4 to OR and improve intro/outro showup

--- plugins/serendipity_plugin_history/serendipity_plugin_history.php
+++ plugins/serendipity_plugin_history/serendipity_plugin_history.php
@@ -166,16 +166,15 @@ class serendipity_plugin_history extends serendipity_plugin
             foreach ($hyr[1] AS $trex) {
                 $startts = serendipity_serverOffsetHour((int)$trex[0], true);
                 $endts   = serendipity_serverOffsetHour((int)$trex[1], true);
-                $and    .= " AND ( e.timestamp >= $startts AND e.timestamp <= $endts )";
+                $and    .= " OR ( e.timestamp >= $startts AND e.timestamp <= $endts )";
             }
             $and .= ")";
-            $_and = str_replace('WHERE ( AND', 'WHERE (', $and);
+            $_and = str_replace('WHERE ( OR', 'WHERE (', $and);
 
             $e = serendipity_fetchEntries(array(0 => 'hyears', 1 => $_and), $full, $max_entries);
         }
 
         $serendipity['fetchLimit'] = $oldLim;
-        echo empty($intro) ? '' : '<div class="serendipity_history_intro">' . $intro . "</div>\n";
 
         if (!is_array($e)) {
             return false;
@@ -184,6 +183,8 @@ class serendipity_plugin_history extends serendipity_plugin
         if ( ($e_c = count($e)) == 0 ) {
             return false;
         }
+
+        echo empty($intro) ? '' : '<div class="serendipity_history_intro">' . $intro . "</div>\n";
 
         for($x=0; $x < $e_c; $x++) {
             $url = serendipity_archiveURL($e[$x]['id'],
@@ -213,6 +214,7 @@ class serendipity_plugin_history extends serendipity_plugin
                 echo '<div class="serendipity_history_body">' . strip_tags($e[$x]['body']) . "</div>\n";
             }
         }
+
         echo empty($outro) ? '' : '<div class="serendipity_history_outro">' . $outro . "</div>\n";
     }
 
