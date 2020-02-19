a56c486d43ffe0d2b42779fe5dd2a30a39788b11
Ian <aristophian@googlemail.com>
2020-02-19 18:07:31 +0100

    bulletproof: Improve lang info notes in ce2eac7 for not auto scale

--- templates/bulletproof/lang_info_en.inc.php
+++ templates/bulletproof/lang_info_en.inc.php
@@ -13,4 +13,4 @@ Among these it was used as the default backend theme for a long time. This latte
 * 2017-04-19 - Removed dead cocomment option and scripting<br>
 * 2019-01-18 - Removed inline styles, fixed staticpages without articleformat, updated CSS for current Serendipity Styx versions.<br>
 * 2019-10-06 - Removed references to dead (s9y-bulletproof.com).<br>
-* 2020-02-19 - Some Upgrades for image responsive fluids, to work with Styx 3.0; Without making it mobile ready.';
+* 2020-02-19 - Some Upgrades for image responsive fluids, to work with Styx 3.0; Without making it auto scale and mobile ready.';
