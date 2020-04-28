9b48f7d6b07b805c780ffd6c7ae1eef3c793d2a2
Ian <aristophian@googlemail.com>
2020-04-28 17:10:16 +0200

    spamblock: Bump version for 686d2784b1289e887bb1d52a7e62902b778c5a37 and 310ff4d4052def052ba12fd713f6da6a7097faf6

--- plugins/serendipity_event_spamblock/serendipity_event_spamblock.php
+++ plugins/serendipity_event_spamblock/serendipity_event_spamblock.php
@@ -25,7 +25,7 @@ class serendipity_event_spamblock extends serendipity_event
             'smarty'      => '3.1.0',
             'php'         => '7.0.0'
         ));
-        $propbag->add('version',       '2.25');
+        $propbag->add('version',       '2.26');
         $propbag->add('event_hooks',    array(
             'frontend_saveComment' => true,
             'external_plugin'      => true,
