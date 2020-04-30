690563205ef7d2a7d29a7dbb82bc84965ac375fa
Ian <aristophian@googlemail.com>
2020-04-30 12:56:18 +0200

    spamblock: Fix compilation failed error(s) .. thrown
    with regular Expressions in filter checks in comments list.

--- plugins/serendipity_event_spamblock/serendipity_event_spamblock.php
+++ plugins/serendipity_event_spamblock/serendipity_event_spamblock.php
@@ -25,7 +25,7 @@ class serendipity_event_spamblock extends serendipity_event
             'smarty'      => '3.1.0',
             'php'         => '7.0.0'
         ));
-        $propbag->add('version',       '2.26');
+        $propbag->add('version',       '2.27');
         $propbag->add('event_hooks',    array(
             'frontend_saveComment' => true,
             'external_plugin'      => true,
@@ -1666,7 +1666,6 @@ if (isset($serendipity['GET']['cleanspamsg'])) {
 
     } // function wordfilter end
 
-
     function &checkFilter($what, $match, $getItems = false)
     {
         $items = explode(';', $this->get_config('contentfilter_' . $what, $this->filter_defaults[$what]));
@@ -1683,7 +1682,7 @@ if (isset($serendipity['GET']['cleanspamsg'])) {
                     continue;
                 }
 
-                if (preg_match('@' . $item . '@', $match)) {
+                if (preg_match('@' . preg_quote($item) . '@', $match)) {
                     $filtered = true;
                     unset($items[$key]);
                 }
