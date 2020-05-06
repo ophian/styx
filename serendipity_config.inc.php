19dffa0cc45eb65bd8992fa0c39b624ec6b00061
Ian <aristophian@googlemail.com>
2020-05-06 08:18:26 +0200

    config: Bump to internal beta1

--- serendipity_config.inc.php
+++ serendipity_config.inc.php
@@ -52,7 +52,7 @@ if (defined('USE_MEMSNAP')) {
 }
 
 // The version string
-$serendipity['version'] = '3.0-alpha4';
+$serendipity['version'] = '3.0-beta1';
 $serendipity['edition'] = 'Styx';
 
 // Setting this to 'false' will enable debugging output.
