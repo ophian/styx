ef603091bdbaa383d55318da238046fb4bc6e546
Ian <aristophian@googlemail.com>
2020-05-21 10:27:23 +0200

    spartacus: Translate [de] 0b6a4981b4fc9dab8808631fe4d64fed21e54a99 ff

--- plugins/serendipity_event_spartacus/UTF-8/lang_de.inc.php
+++ plugins/serendipity_event_spartacus/UTF-8/lang_de.inc.php
@@ -8,7 +8,7 @@
 @define('PLUGIN_EVENT_SPARTACUS_NAME', 'Spartacus');
 @define('PLUGIN_EVENT_SPARTACUS_DESC', '[S]erendipity [P]lugin [A]ccess [R]epository [T]ool [A]nd [C]ustomization/[U]nification [S]ystem - Installiert Plugins direkt aus dem Netz.');
 @define('PLUGIN_EVENT_SPARTACUS_FETCH', 'Hier klicken um ein neues %s aus dem Netz zu installieren.');
-@define('PLUGIN_EVENT_SPARTACUS_FETCHERROR', 'Die URL %s konnte nicht geöffnet werden. Möglicherweise existieren Server- oder Netzwerkprobleme. Try to reload (F5) the page first.');
+@define('PLUGIN_EVENT_SPARTACUS_FETCHERROR', 'Die URL %s konnte nicht geöffnet werden. Möglicherweise existieren vorübergehende Server- oder Netzwerkprobleme. Versuchen Sie zuerst die Seite im Browser zu aktualisieren (F5).');
 @define('PLUGIN_EVENT_SPARTACUS_FETCHING', 'Versuche URL %s zu öffnen...');
 @define('PLUGIN_EVENT_SPARTACUS_FETCHED_BYTES_URL', '%s bytes von obiger URL geladen. Speichere Inhalt als %s...');
 @define('PLUGIN_EVENT_SPARTACUS_FETCHED_BYTES_CACHE', '%s bytes von bereits bestehender Datei geladen. Speichere Inhalt als %s...');
