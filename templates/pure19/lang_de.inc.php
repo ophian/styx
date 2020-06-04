7e1b2fc9c9e5e3bf8f596c240eec80a909984b97
Ian <aristophian@googlemail.com>
2020-06-04 08:51:19 +0200

    pure19: Remove unnecessary and untouched child files 4b543d1d113bdaa37d529c7c2a04cd054a1f75d5

--- templates/pure19/lang_de.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-@define('USE_CORENAV', 'Globale Navigation einbinden?');
-@define('NAV_MENU', 'navigation');
-@define('PURE_PLINK_TEXT', 'Link');
-@define('PURE_PLINK_TITLE', 'Permanenter Link zu diesem Kommentar');
-@define('PURE_REPLYORIGIN', 'Antwort auf');
--- templates/pure19/lang_en.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-@define('USE_CORENAV', 'Use global navigation?');
-@define('NAV_MENU', 'navigation');
-@define('PURE_PLINK_TEXT', 'Link');
-@define('PURE_PLINK_TITLE', 'Permanent link for this comment');
-@define('PURE_REPLYORIGIN', 'Origin');
