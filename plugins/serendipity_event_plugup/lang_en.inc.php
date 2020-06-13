34a1b1c676a67e6db45c21ce4889697daae0c590
Ian <aristophian@googlemail.com>
2020-06-13 14:54:00 +0200

    plugup: Change "Dashboard" to "backend startpage" where appropriate

--- plugins/serendipity_event_plugup/lang_en.inc.php
+++ plugins/serendipity_event_plugup/lang_en.inc.php
@@ -1,10 +1,10 @@
 <?php
 
 @define('PLUGIN_EVENT_PLUGUP_TITLE', 'Plugin-Update Notifier');
-@define('PLUGIN_EVENT_PLUGUP_TITLE_DESC', 'The Dashboard Plugin-Update Notifier');
+@define('PLUGIN_EVENT_PLUGUP_TITLE_DESC', 'The Backends startpage Plugin-Update Notifier');
 @define('PLUGIN_EVENT_PLUGUP_SHOW_UPDATE_NOTIFIER', 'Show available Plugin updates?');
 @define('PLUGIN_EVENT_PLUGUP_SHOW_UPDATE_NOTIFIER_DESC', 'Will newly check every 6 hours');
-@define('PLUGIN_EVENT_PLUGUP_SPARTACUS_URL', 'Spartacus remote fetch url');
+@define('PLUGIN_EVENT_PLUGUP_SPARTACUS_URL', 'Spartacus remote fetch URL');
 @define('PLUGIN_EVENT_PLUGUP_SPARTACUS_URL_DESC', 'Please enable the remote plugin version information in SPARTACUS options and add here the "secret" key to the remote plugin version information! This is only readable for admin users in the Dashboard. Move the Plugup-plugin beneath the Spartacus-plugin in list.');
 @define('PLUGIN_DASHBOARD_PLUGUP_BOX_TITLE', 'Plugin Updates');
 @define('PLUGIN_DASHBOARD_PLUGUP_UP_TO_DATE', 'All Plugins are up to date!');
