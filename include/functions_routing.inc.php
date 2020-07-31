4630969ad720809b05b5c52d160c65735c38c341
Ian <aristophian@googlemail.com>
2020-07-31 11:16:44 +0200

    routing API: Fix wrong bracket usage in serve404() embed case

--- include/functions_routing.inc.php
+++ include/functions_routing.inc.php
@@ -30,7 +30,7 @@ function serve404() {
 
     $serendipity['view'] = '404';
     $serendipity['viewtype'] = '404_4';
-    if (!isset($serendipity['embed']) || serendipity_db_bool($serendipity['embed'] === false)) {
+    if (!isset($serendipity['embed']) || serendipity_db_bool($serendipity['embed']) === false) {
         $serendipity['content_message'] = URL_NOT_FOUND;
     }
 
