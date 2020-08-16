09d69eb66cbece81eddd7055be75e1a9525e9168
Ian <aristophian@googlemail.com>
2020-08-16 18:32:49 +0200

    entries: Extend datetime-local datetime check against invalid user dates
    It is not advisable to force making this a GUI Elemment like Chrome and other Browsers using this engine do. There are too many user imponderabilities.
    https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/datetime-local

--- include/admin/entries.inc.php
+++ include/admin/entries.inc.php
@@ -86,7 +86,7 @@ switch($serendipity['GET']['adminAction']) {
             // The user changed the timestamp, now set the DB-timestamp to the user's date
             $entry['timestamp'] = strtotime($serendipity['POST']['new_timestamp']);
 
-            if ($entry['timestamp'] === false) {
+            if ($entry['timestamp'] === false || false === preg_match('@^([0-9]{4})\-([0-9]{2})\-([0-9]{2})T([0-9]{2}):([0-9]{2})@', $serendipity['POST']['new_timestamp']) || false === serendipity_validateDate(date('Y-m-d', $entry['timestamp']))) {
                 $data['switched_output'] = true;
                 $data['dateval'] = true; // date invalid message
                 // The date given by the user is not convertible. Reset the timestamp.
