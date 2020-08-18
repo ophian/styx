e18792feecb5adec561a1aa9968a5fb872028078
Ian <aristophian@googlemail.com>
2020-08-18 16:47:06 +0200

    history: Add PLUGIN_HISTORY_PLUGIN_MULTIYEARS_EMPTY constant

--- plugins/serendipity_plugin_history/lang_en.inc.php
+++ plugins/serendipity_plugin_history/lang_en.inc.php
@@ -34,4 +34,5 @@
 
 @define('PLUGIN_HISTORY_MULTIYEARS', 'Number of looped years');
 @define('PLUGIN_HISTORY_MULTIYEARS_DESC', 'Set a new number of years to loop through, when having selected the time range "one year ago". Default is 1 (the current year). Set upper "Min age" and "Max age" entries to exact 365 days. In case of multi years, all history sidebar output - inclusive possible intro/outro - will be cached for performance reasons inside a "templates_c/history_daylist.dat" file for the rest of the day.');
+@define('PLUGIN_HISTORY_PLUGIN_MULTIYEARS_EMPTY', 'Optional cached fallback text when multiyears empty');
 
