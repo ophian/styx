43c55987786d6e9f3d0876ff219c8ba4dc8958cc
Ian <aristophian@googlemail.com>
2020-09-25 16:55:05 +0200

    spamblock: Modernize default exclude ident.ca url regex string

--- plugins/serendipity_event_spamblock/serendipity_event_spamblock.php
+++ plugins/serendipity_event_spamblock/serendipity_event_spamblock.php
@@ -25,7 +25,7 @@ class serendipity_event_spamblock extends serendipity_event
             'smarty'      => '3.1.0',
             'php'         => '7.0.0'
         ));
-        $propbag->add('version',       '2.28');
+        $propbag->add('version',       '2.29');
         $propbag->add('event_hooks',    array(
             'frontend_saveComment' => true,
             'external_plugin'      => true,
@@ -475,7 +475,7 @@ class serendipity_event_spamblock extends serendipity_event
 
     function get_default_exclude_urls()
     {
-        return '^http://identi\.ca/notice/\d+$';
+        return '^https?://identi\.ca/notice/\d+$';
     }
 
     function htaccess_update($new_ip)
