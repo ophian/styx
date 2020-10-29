45d0e01f5a01cec26fc55eb16cd5046660e7391d
Ian <aristophian@googlemail.com>
2020-10-29 12:50:55 +0100

    b45: Initial skeleton structure info files

---
+++ templates/b45/UTF-8/lang_info_de.inc.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * The info.txt UTF-8 charset (translation) file as an array.
+ * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['lang'].
+ * Convert or save as UTF-8 file without BOM.
+ **/
+
+$info['theme_info_summary'] = 'Ein "Bootstrap 4.5.3" basierendes HTML5 Framework Template, das als Basis Engine "Bootstrap4" benutzt.';
+
+$info['theme_info_desc'] = '';
---
+++ templates/b45/lang_info_de.inc.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * The info.txt charset (translation) file as an array.
+ * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['lang'].
+ * Convert or save as ANSI (ISO-8859-1) or your native charset
+ **/
+
+$info['theme_info_summary'] = 'Ein "Bootstrap 4.5.3" basierendes HTML5 Framework Template, das als Basis Engine "Bootstrap4" benutzt.';
+
+$info['theme_info_desc'] = '';
