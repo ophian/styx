f622a8a73de0f38fe867a9cf2c7577f029c65813
Ian <aristophian@googlemail.com>
2020-10-29 12:50:14 +0100

    b45: Initial skeleton structure constant files

---
+++ templates/b45/lang_de.inc.php
@@ -0,0 +1,9 @@
+<?php
+@define('B45_INSTR', '<b>Hinweis:</b> "b45" ist ein Upgrade Engine:Template des "bootstrap4" Themes.<p>Für die Darstellung der Tags unter Einträgen, erlauben Sie die "Erweitertes Smarty" Option im Freetag Plugin.</p><p>Lassen sie das "#" in der URL eines Navigationsleisten-Links, so kann in der index.tpl ein popover Submenu manuell erstellt werden. Beispiel vorhanden.</p>');
+@define('B45_USE_CORENAV', 'Globale Navigation einbinden?');
+@define('B45_SEND_MAIL', 'Schicke Email');
+
+// If used within template files add previously defines here, since the lang constant engine:fallback will work only for the config.inc file, not the templates!
+@define('BS_PLINK_TEXT', 'Link');
+@define('BS_PLINK_TITLE', 'Permanenter Link zu diesem Kommentar');
+@define('BS_REPLYORIGIN', 'Antwort auf');
---
+++ templates/b45/lang_en.inc.php
@@ -0,0 +1,14 @@
+<?php
+// Theme options
+@define('B45_INSTR', '<b>NOTE:</b> "b45" is an upgrade Engine:Template of the "bootstrap4" theme.<p>For custom theme styling of entries freetags, enable the "Extended Smarty" option in the event freetag plugin.</p><p>If you leave the "#" for the URL of a navigation-link, a popover submenu can be created manually in the index.tpl file. Example inside.</p>');
+@define('B45_USE_CORENAV', 'Use global navigation?');
+
+// If used within template files add previously defines here, since the lang constant engine:fallback will work only for the config.inc file, not the templates!
+@define('BS_PLINK_TEXT', 'Link');
+@define('BS_PLINK_TITLE', 'Permanent link for this comment');
+@define('BS_REPLYORIGIN', 'Origin');
+
+// Lang constants non-translate
+@define('B45_PLACE_SEARCH', 'Search term(s)');
+@define('B45_SEND_MAIL', 'Send email');
+
