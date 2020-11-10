205e83a856d8ba3ad1862fa53af9fa7ff2796c7f
Ian <aristophian@googlemail.com>
2020-11-10 12:02:44 +0100

    b46: Add additional theme info language 'theme_info_desc'

--- templates/b46/lang_info_de.inc.php
+++ templates/b46/lang_info_de.inc.php
@@ -7,4 +7,6 @@
 
 $info['theme_info_summary'] = 'Ein "Bootstrap 4.5.3" basierendes HTML5 Framework Template, das als Basis Engine "Bootstrap4" benutzt.';
 
-$info['theme_info_desc'] = '';
+$info['theme_info_desc'] = 'Dieses Theme wurde nicht nur für die neuesten Bootstrap-Assets aktualisiert, sondern hat auch die Konfiguration und das Raster der Indexdatei zum Verschieben der Seitenleisten in den Seitenfuß vereinfacht.
+Auf der anderen Seite wurden die Haupt-Template-Dateien erweitert und eine einfache auto-"handgemachte" Unterstützung für Untermenüs der Navigationsleiste hinzugefügt.
+Abgesehen von der direkten Verwendung oder als Theme-Kopie, erlaubt dies die Verwendung als eine gut geeignete Zwischen-Schicht-Engine für mögliche einfache ENKEL-Themes durch "Engine: b45, bootstrap4", indem einfach einige zusätzliche Benutzerstile hinzugefügt werden und vielleicht das Strukturraster der Indexdatei geändert wird.';
--- templates/b46/lang_info_en.inc.php
+++ templates/b46/lang_info_en.inc.php
@@ -7,4 +7,6 @@
 
 $info['theme_info_summary'] = 'A "Bootstrap 4.5.3" based HTML5 Framework Template, based on fallback engine "Bootstrap4".';
 
-$info['theme_info_desc'] = '';
+$info['theme_info_desc'] = 'This theme has not only updated to latest bootstrap assets, it also simplified the config and the index file grid for moving the sidebars to bottom.
+On the other hand it extended the main template files and added an easy auto-"handmade" support for navigation bar submenues.
+Apart from direct or copy theme usage, this allows to be used as a good middle-layer-engine for possible simple GRAND-child themes by "Engine: b45, bootstrap4", just by adding some additional user styles and probably changing the index files structural grid.';
