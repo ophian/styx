5836cf976607cb054e6045b80d6290d62126344e
Ian <aristophian@googlemail.com>
2020-12-03 10:01:23 +0100

    s9ymarkup: Consistency fix for markup_element check

--- plugins/serendipity_event_s9ymarkup/serendipity_event_s9ymarkup.php
+++ plugins/serendipity_event_s9ymarkup/serendipity_event_s9ymarkup.php
@@ -91,7 +91,7 @@ class serendipity_event_s9ymarkup extends serendipity_event
 
                 case 'frontend_display':
                     foreach ($this->markup_elements AS $temp) {
-                        if (serendipity_db_bool($this->get_config($temp['name'], true)) && !empty($eventData[$temp['element']])
+                        if (serendipity_db_bool($this->get_config($temp['name'], 'true')) && !empty($eventData[$temp['element']])
                         &&  (!isset($eventData['properties']['ep_disable_markup_' . $this->instance]) || !$eventData['properties']['ep_disable_markup_' . $this->instance])
                         &&  !isset($serendipity['POST']['properties']['disable_markup_' . $this->instance])) {
                             $element = $temp['element'];
