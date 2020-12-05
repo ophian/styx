8c36ce56fe2bb9996c1b0c352eab467668a632ed
Ian <aristophian@googlemail.com>
2020-12-05 18:00:21 +0100

    config: Bump internal version to 3.2-beta2

--- serendipity_config.inc.php
+++ serendipity_config.inc.php
@@ -52,7 +52,7 @@ if (defined('USE_MEMSNAP')) {
 }
 
 // The version string
-$serendipity['version'] = '3.2-beta1';
+$serendipity['version'] = '3.2-beta2';
 $serendipity['edition'] = 'Styx';
 
 // Setting this to 'false' will enable debugging output.
