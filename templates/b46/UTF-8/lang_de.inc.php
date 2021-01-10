fb792ad006498780f373d7a9d30f08653d724bba
Ian <aristophian@googlemail.com>
2021-01-10 13:01:34 +0100

    b46: Add B46_USE_SEARCH

--- templates/b46/UTF-8/lang_de.inc.php
+++ templates/b46/UTF-8/lang_de.inc.php
@@ -2,6 +2,7 @@
 @define('B46_INSTR', '<b>Hinweis:</b> "b46" ist ein Upgrade Engine:Template des "bootstrap4" Themes.<p>Für die Darstellung der Tags unter Einträgen, erlauben Sie die "Erweitertes Smarty" Option im Freetag Plugin.</p><p>Lassen sie das "#" in der URL eines Navigationsleisten-Links, so kann in der index.tpl ein popover Submenu manuell erstellt werden. Beispiel vorhanden.</p>');
 @define('B46_USE_CORENAV', 'Globale Navigation einbinden?');
 @define('B46_SEND_MAIL', 'Schicke Email');
+@define('B46_USE_SEARCH', 'Zeige Suche im Kopf');
 
 // If used within template files add previously defines here, since the lang constant engine:fallback will work only for the config.inc file, not the templates! This is not true for Grandchild themes!
 @define('BS_RSS', 'RSS-Links in der Seitenleiste?');
--- templates/b46/lang_en.inc.php
+++ templates/b46/lang_en.inc.php
@@ -2,6 +2,7 @@
 // Theme options
 @define('B46_INSTR', '<b>NOTE:</b> "b46" is an upgrade Engine:Template of the "bootstrap4" theme.<p>For custom theme styling of entries freetags, enable the "Extended Smarty" option in the event freetag plugin.</p><p>If you leave the "#" for the URL of a navigation-link, a popover submenu can be created manually in the index.tpl file. Example inside.</p>');
 @define('B46_USE_CORENAV', 'Use global navigation?');
+@define('B46_USE_SEARCH', 'Use quicksearch in the navigation head bar');
 
 // If used within template files add previously defines here, since the lang constant engine:fallback will work only for the config.inc file, not the templates! This is not true for Grandchild themes!
 @define('BS_RSS', 'RSS links in sidebar?');
