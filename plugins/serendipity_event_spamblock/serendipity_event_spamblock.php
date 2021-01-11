f757f5d2c0d46fe17fac8215c2e0b114f3493a56
Ian <aristophian@googlemail.com>
2021-01-11 15:31:23 +0100

    spamblock: image API: Remove old mysql dbtype references

--- plugins/serendipity_event_spamblock/serendipity_event_spamblock.php
+++ plugins/serendipity_event_spamblock/serendipity_event_spamblock.php
@@ -25,7 +25,7 @@ class serendipity_event_spamblock extends serendipity_event
             'smarty'      => '3.1.0',
             'php'         => '7.0.0'
         ));
-        $propbag->add('version',       '2.41');
+        $propbag->add('version',       '2.42');
         $propbag->add('event_hooks',    array(
             'frontend_saveComment' => true,
             'external_plugin'      => true,
@@ -1998,7 +1998,6 @@ if (isset($serendipity['GET']['cleanspamsg'])) {
                     case 'postgres':
                         $sql = "VACUUM";
                         break;
-                    case 'mysql':
                     case 'mysqli':
                         $sql = "OPTIMIZE TABLE {$serendipity['dbPrefix']}spamblocklog";
                         break;
