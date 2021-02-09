61b78832fba997d0d861e9dcb67969aa71e70f8c
Ian <aristophian@googlemail.com>
2021-02-09 12:44:34 +0100

    trackbacks API: Revert 9e40f41b7c65c17257fe4eb06739936c603076bf for 9a25dfed01a9c2324e55eea572769036c6ee9eba

--- include/functions_trackbacks.inc.php
+++ include/functions_trackbacks.inc.php
@@ -529,7 +529,7 @@ function fetchPingbackData(&$comment) {
     }
 
     if (false === $fContent) {
-        if (isset($serendipity['logger']) && is_object($serendipity['logger'])) $serendipity['logger']->debug("Request url: $url failed in: " . __FUNCTION__ . ' with response Code: ' . $serendipity['last_http_request']['responseCode']);
+        if (is_object($serendipity['logger'])) $serendipity['logger']->debug("Request url: $url failed in: " . __FUNCTION__ . ' with response Code: ' . $serendipity['last_http_request']['responseCode']);
         // do what? Don't touch $comments
         return;
     }
@@ -646,7 +646,7 @@ function serendipity_handle_references($id, $author, $title, $text, $dry_run = f
     static $saved_urls = array();
     static $debug = false;
 
-    $debug = (isset($serendipity['logger']) && is_object($serendipity['logger']) && $debug); // ad hoc, case-by-case debugging
+    $debug = (is_object($serendipity['logger']) && $debug); // ad hoc, case-by-case debugging
 
     if ($debug) { $serendipity['logger']->debug("\n" . str_repeat(" <<< ", 10) . "DEBUG START serendipity_handle_references SEPARATOR" . str_repeat(" <<< ", 10) . "\n"); }
 
