8486ac44556459925073467e3efb15cf3b4a7c4a
Ian <aristophian@googlemail.com>
2021-02-17 16:40:06 +0100

    b46: Question consistency and hint that teaser is plain text only

--- templates/b46/UTF-8/lang_de.inc.php
+++ templates/b46/UTF-8/lang_de.inc.php
@@ -2,18 +2,18 @@
 @define('B46_INSTR', '<b>Hinweis:</b> "b46" ist ein Upgrade Engine:Template des "bootstrap4" Themes.<p>Für die Darstellung der Tags unter Einträgen, erlauben Sie die "Erweitertes Smarty" Option im Freetag Plugin.</p><p>Lassen Sie das "#" in der URL eines Navigationsleisten-Links, so kann in der index.tpl ein popover Submenu manuell erstellt werden. Beispiel vorhanden.</p>');
 @define('B46_USE_CORENAV', 'Globale Navigation einbinden?');
 @define('B46_SEND_MAIL', 'Schicke Email');
-@define('B46_USE_SEARCH', 'Zeige Suche im Kopf');
-@define('B46_JUMPSCROLL', 'Nutze scrolljump Button');
-@define('B46_HUGO', 'Artikel [body] als klappbarer Aufmacher mit Länge');
+@define('B46_USE_SEARCH', 'Zeige Suche im Kopf?');
+@define('B46_JUMPSCROLL', 'Nutze scrolljump Button?');
+@define('B46_HUGO', 'Artikel [text] als klappbarer Aufmacher mit Länge');
 @define('B46_HUGO_TTT', 'Öffnen und schließen mit Mausklick, oder, wenn Feld aktiv, mit Leertaste der Tastatur');
 @define('B46_HUGO_TITLE_ELSE', 'Zum Artikel');
 @define('B46_TEASE', '0 meint: Nicht aktiviert!');
 @define('B46_TEASE_COND', ' - Nur Ziffern & wähle Typ entw./oder!');
-@define('B46_CARD', 'Artikel [body] als Kachel Aufmacher mit der Länge');
+@define('B46_CARD', 'Artikel [text] als Kachel Aufmacher mit der Länge');
 @define('B46_CARD_TITLE_ELSE', 'Kein Artikel Vorschau Text vorhanden');
 @define('B46_LEAD', 'Bei Aufmacher, vorangestellter Feature Artikel');
 @define('B46_LEAD_DESC', ' Oder tragen Sie folgende urlartige "array" Konfiguration (bei unveränderten Schlüsseln) ein. Die Schlüssel "title" und "text" sind mindestens erforderlich: ');
-@define('B46_NAV_ONELINE', 'Navigation als eigenständige Zeile');
+@define('B46_NAV_ONELINE', 'Navigation als eigenständige Zeile?');
 
 // If used within template files add previously defines here, since the lang constant engine:fallback will work only for the config.inc file, not the templates! This is not true for Grandchild themes!
 @define('BS_RSS', 'RSS-Links in der Seitenleiste?');
--- templates/b46/lang_en.inc.php
+++ templates/b46/lang_en.inc.php
@@ -2,18 +2,18 @@
 // Theme options
 @define('B46_INSTR', '<b>NOTE:</b> "b46" is an upgrade Engine:Template of the "bootstrap4" theme.<p>For custom theme styling of entries freetags, enable the "Extended Smarty" option in the event freetag plugin.</p><p>If you leave the "#" for the URL of a navigation-link, a popover submenu can be created manually in the index.tpl file. Example inside.</p>');
 @define('B46_USE_CORENAV', 'Use global navigation?');
-@define('B46_USE_SEARCH', 'Use quicksearch in the navigation head bar');
-@define('B46_JUMPSCROLL', 'Use scroll button jumper');
-@define('B46_HUGO', 'Article [body] as toggle summary teaser with length');
+@define('B46_USE_SEARCH', 'Use quicksearch in the navigation head bar?');
+@define('B46_JUMPSCROLL', 'Use scroll button jumper?');
+@define('B46_HUGO', 'Article [text] as toggle summary teaser with length');
 @define('B46_HUGO_TTT', 'When over, click mouse or toggle by keyboards space-bar when set active');
 @define('B46_HUGO_TITLE_ELSE', 'Open entry');
 @define('B46_TEASE', '0 means: Not set!');
 @define('B46_TEASE_COND', ' - Integers only & choose type either/or!');
-@define('B46_CARD', 'Article [body] as a Grid-Card teaser with length');
+@define('B46_CARD', 'Article [text] as a Grid-Card teaser with length');
 @define('B46_CARD_TITLE_ELSE', 'No entry summary text available');
 @define('B46_LEAD', 'By teaser type, add a top featured article');
 @define('B46_LEAD_DESC', ' Or use this "URI" alike plain text string "array" representation, to fill in the text (with unchanged keys) - "title" and "text" keys are mandatory: ');
-@define('B46_NAV_ONELINE', 'Navigation as independent line');
+@define('B46_NAV_ONELINE', 'Navigation as independent line?');
 
 // If used within template files add previously defines here, since the lang constant engine:fallback will work only for the config.inc file, not the templates! This is not true for Grandchild themes!
 @define('BS_RSS', 'RSS links in sidebar?');
