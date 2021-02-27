911c2e5901f299a5cdd53be1d810155dc7fd85eb
Ian <aristophian@googlemail.com>
2021-02-27 17:10:50 +0100

    timeline: Add remaining [de] translation

--- templates/timeline/UTF-8/lang_de.inc.php
+++ templates/timeline/UTF-8/lang_de.inc.php
@@ -15,30 +15,30 @@
 @define('THEME_IDENTITY', 'Seiten-Identität');
 @define('THEME_EP_YES', '<p class="msg_success">Das Plugin "Erweiterte Eigenschaften von Artikeln" (serendipity_event_entryproperties) wird benötigt. Es ist installiert und aktiv.</p>');
 @define('THEME_EP_NO', '<p class="msg_error">Das Plugin "Erweiterte Eigenschaften von Artikeln" (serendipity_event_entryproperties) wird benötigt. Es ist entweder nicht installiert oder inaktiv. Bitte das Plugin installieren, um alle Features dieses Themes voll zu nutzen.</p>');
-@define('THEME_INSTRUCTIONS', '<p>This theme displays blog posts on a linear timeline. Each group of month titles can also be displayed or hidden on the timeline.</p><p>This theme uses a right and bottom sidebar. Either, or both sidebars can be disabled by deleting sidebar plugins or relocating sidebar plugins to the sidebar column labeled "hidden" in plugin configuration.</p><p>This theme can be configured to display categories and entry tags from the respective sidebar plugins on the archive page. See "' . THEME_PAGE_OPTIONS . '" below.</p><p>If using the avatar plugin (serendipity_event_gravatar), configure option "Produce Smarty tag = yes" for best display of comment avatars.</p><p>Configure serendipity_event_freetag option "Extended Smarty = yes" for a nicer display of tags in the entry footer.</p>');
+@define('THEME_INSTRUCTIONS', '<p>Dieses Theme zeigt Blogbeiträge auf einer linearen Zeitleiste an. Jede Gruppe von Monatsbeiträgen kann auch auf der Zeitleiste angezeigt oder ausgeblendet werden.</p><p>Dieses Theme verwendet eine rechte und eine untere Seitenleiste. Eine oder beide Seitenleisten können deaktiviert werden, indem Sie die Seitenleisten-Plugins löschen oder die Seitenleisten-Plugins in die Spalte "versteckt" in der Plugin-Konfiguration verschieben.</p><p>Dieses Theme kann so konfiguriert werden, dass Kategorien und Eintrags-Tags aus den jeweiligen Sidebar-Plugins auf der Archivseite angezeigt werden. Siehe "' . THEME_PAGE_OPTIONS . '" unten.</p><p>Wenn Sie das Avatar-Plugin (serendipity_event_gravatar) verwenden, konfigurieren Sie die Option "Smarty-Tag erzeugen = ja" für eine bessere Darstellung von Kommentar-Avataren.</p><p>Konfigurieren Sie die serendipity_event_freetag-Option "Erweitertes Smarty = ja" für eine schönere Darstellung von Tags in der Fußzeile des Eintrags.</p>');
 @define('THEME_CUSTOM_FIELD_HEADING', 'Freie Felder für Einträge');
-@define('THEME_CUSTOM_FIELD_DEFINITION', 'These optional field(s) are only available when using this theme (timeline). The event plugin serendipity_event_entryproperties ("Erweiterte Eigenschaften von Artikeln") must also be installed to use these field(s). The entry image will appear on both the timeline, and at the top of each detailed entry.');
-@define('THEME_ENTRY_IMAGE', 'Entry Image.');
+@define('THEME_CUSTOM_FIELD_DEFINITION', 'Diese optionalen Felder sind nur bei Verwendung dieses Themes (timeline) gesetzt. Das Event-Plugin serendipity_event_entryproperties ("Erweiterte Eigenschaften von Artikeln") muss ebenfalls installiert sein, um diese Felder zu verwenden. Das Eintragsbild wird sowohl in der Zeitleiste als auch oben in jedem detaillierten Eintrag angezeigt.');
+@define('THEME_ENTRY_IMAGE', 'Zugeordnetes Eintrags Bild.');
 @define('THEME_DEMO_AVAILBLE', 'Eine komplette englische Anleitung zur <a href="http://www.optional-necessity.com/demo/timeline/archives/13-Using-the-Timeline-theme.html">Konfiguration und Benutzung von Timeline</a> findet sich in der <a href="http://www.optional-necessity.com/demo/timeline/">Timeline theme demo</a>.');
 
 //Page Options
 @define('USE_GOOGLEFONTS', 'Google Webfonts einbinden?');
-@define('THEME_COLORSET', 'Colorset');
-@define('THEME_SKINSET', 'Theme skin');
-@define('HEADER_IMG', 'Optional Header Image. Leave blank to use Blog Name.');
-@define('HEADER_IMG_DESC', 'Header image recommended size: 150 x 40px.');
+@define('THEME_COLORSET', 'Farbsystem');
+@define('THEME_SKINSET', 'Theme-Skin');
+@define('HEADER_IMG', 'Optionales Kopfzeilenbild. Leer lassen, um den Blognamen zu verwenden.');
+@define('HEADER_IMG_DESC', 'Empfohlene Größe des Header-Bildes: 150 x 40px.');
 @define('ENTRY_DATE_FORMAT', 'Datumsformat für Einträge');
 @define('COMMENT_TIME_FORMAT', 'Zeitformat für Kommentare und Trackbacks');
 @define('WORDS', 'Textfassung');
 @define('TIMESTAMP', 'Zeitstempel');
 @define('DISPLAY_AS_TIMELINE', 'Timeline-Format verwenden');
 @define('DISPLAY_AS_TIMELINE_DESC', 'Timeline-Format für Blog-Posts verwenden. Falls Nein werden Blog-Posts im üblichen Blog-Format ausgegeben.');
-@define('MONTHS_ON_TIMELINE', 'Show month names on timeline');
-@define('MONTHS_ON_TIMELINE_DESC', 'Month name will appear as a heading on the timeline for each month of entries.');
-@define('MONTHS_ON_TIMELINE_FORMAT', 'Timeline month format');
+@define('MONTHS_ON_TIMELINE', 'Monatsnamen auf der Zeitleiste anzeigen');
+@define('MONTHS_ON_TIMELINE_DESC', 'Der Monatsname wird als Überschrift auf der Zeitleiste für jeden Monat der Einträge angezeigt.');
+@define('MONTHS_ON_TIMELINE_FORMAT', 'Timeline Monatsformat');
 @define('CATEGORIES_ON_ARCHIVE', 'Kategorien auf der Archivseite anzeigen');
 @define('CATEGORIES_ON_ARCHIVE_DESC', 'Das Seitenleisten-Plugin "Kategorien" (serendipity_plugin_categories) muss installiert und die Option "Smarty-Templating aktivieren?" aktiviert sein, damit Kategorien auf der Archiv-Seite angezeigt werden.');
-@define('CATEGORY_RSS_ON_ARCHIVE', 'Show RSS icon next to each category on archive page');
+@define('CATEGORY_RSS_ON_ARCHIVE', 'RSS-Symbol neben jeder Kategorie auf der Archivseite anzeigen');
 @define('TAGS_ON_ARCHIVE', 'Tags auf der Archivseite anzeigen');
 @define('TAGS_ON_ARCHIVE_DESC', 'Das Seitenleisten-Plugin "Getaggte Artikel" (serendipity_plugin_freetag) muss installiert sein und die Option "Sidebar template" muss auf "archive_freetag.tpl" gesetzt werden, damit Tags auf der Archiv-Seite angezeigt werden.');
 
@@ -46,9 +46,9 @@
 @define('USE_CORENAV', 'Globale Navigation verwenden?');
 
 //Social media
-@define('SOCIAL_ICONS_AMOUNT', 'Enter number of social media links');
-@define('SOCIAL_NETWORK_SERVICE', 'Select social media service for link');
-@define('SOCIAL_ICON_URL', 'URL for social media service link');
+@define('SOCIAL_ICONS_AMOUNT', 'Anzahl der Links zu sozialen Medien eingeben');
+@define('SOCIAL_NETWORK_SERVICE', 'Social-Media-Dienst für Link auswählen');
+@define('SOCIAL_ICON_URL', 'URL für Social-Media-Dienst-Link');
 @define('COPYRIGHT', 'Copyright');
 
 //time ago in words function
@@ -68,5 +68,5 @@
 //Static Pages
 @define('STATIC_SHOW_AUTHOR_TEXT', 'Autorennamen anzeigen');
 @define('STATIC_SHOW_DATE_TEXT', 'Datum anzeigen');
-@define('STATIC_SHOW_SIDEBARS_TEXT', 'Show sidebars?');
+@define('STATIC_SHOW_SIDEBARS_TEXT', 'Seitenleisten anzeigen?');
 
