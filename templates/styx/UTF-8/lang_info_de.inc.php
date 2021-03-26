297beba87464bada85b9b88361ff6058db309af4
Ian <aristophian@googlemail.com>
2021-03-26 11:35:40 +0100

    styx: Improve backend info changelog

--- templates/styx/UTF-8/lang_info_de.inc.php
+++ templates/styx/UTF-8/lang_info_de.inc.php
@@ -16,16 +16,19 @@ $info['theme_info_backend'] = 'Dieses Backend Beispiel-Theme verweist auf die St
 Als Beispiel zeigt es die Möglichkeit, ein eigenes und mehr oder weniger template-leeres Backend Theme zu erstellen das als "Fallback" die Standard "default" Backend Template Dateien nutzt.
 Dies bedenkend, sind Sie damit in der Lage, nur diejenigen Dateien zu ändern und hinzuzufügen, die notwendig sind, um Ihre momentanen Bedürfnisse zu erfüllen.
 Dieses Backend-Beispiel kann sich in Zukunft ändern, um mehr echte Dateien für die Backend-Generierung vorzuhalten.
-Momentan enthält es nur eine Index-Template-Datei, die relevante Informationen und Assets auf der Login-Seite entfernt, wenn Sie nicht angemeldet sind.<br>
-* 2017-08-21 - Neu hinzugekommen ist ein Bugfix für die Backend-Ansichten mit "rtl" (right-to-left), auf rechts gedrehtes Schrift-Attribut im &lt;html&gt; Element.<br>
-* 2019-09-08 - Die IE8/9 workarounds wurden entfernt.<br>
-* 2020-03-25 - Hervorhebung der Styx message styles.<br>
-* 2020-06-06 - Verkleinertes font-size der entry_info styles der Eintrags Liste.<br>
-* 2020-08-31 - Verbesserung der Login Seite und Update des fullsize preview.<br>
-* 2020-10-31 - SVG icon für die eingesprungenen Plugin fieldset legends des Eintragsformulares.<br>
-* 2020-12-06 - Erlaube Media Screen iPhone 5/SE mit 320px Größen für media filter HTML datetime Felder.<br>
-* 2021-02 / 2021-03 - Kleinere Verbesserungen im den CSS styles.<br>
-* 2021-03-26 - Filter Toolbars werden ab 360px Screen Breiten nach rechts verschoben.<br><br>
+Momentan enthält es nur eine Index-Template-Datei, die relevante Informationen und Assets auf der Login-Seite entfernt, wenn Sie nicht angemeldet sind.
+Und es verfügt über eine eigene styles Auszeichnung, analog zur "default/admin/styles.css" Datei, die folgende, fortlaufend aktualisierte Verbesserungen im Backend vornimmt:
+<pre>
+* 2017-08-21 - Neu hinzugekommen ist ein Bugfix für die Backend-Ansichten mit "rtl" (right-to-left), auf rechts gedrehtes Schrift-Attribut im &lt;html&gt; Element.
+* 2019-09-08 - Die IE8/9 workarounds wurden entfernt.
+* 2020-03-25 - Hervorhebung der Styx message styles.
+* 2020-06-06 - Verkleinertes font-size der entry_info styles der Eintrags Liste.
+* 2020-08-31 - Verbesserung der Login Seite und Update des fullsize preview.
+* 2020-10-31 - SVG icon für die eingesprungenen Plugin fieldset legends des Eintragsformulares.
+* 2020-12-06 - Erlaube Media Screen iPhone 5/SE mit 320px Größen für media filter HTML datetime Felder.
+* 2021-02 / 2021-03 - Kleinere Verbesserungen im den CSS styles.
+* 2021-03-26 - Filter Toolbars werden ab 360px Screen Breiten nach rechts verschoben.
+</pre>
 
 DAS FOLGENDE IST DIE BESCHREIBUNG DES FALLBACK BACKENDS&colon;<br>
 Dieses Theme beherbergt die Styx-Kern Backend Templates im Unterverzeichnis "default/admin".
--- templates/styx/lang_info_en.inc.php
+++ templates/styx/lang_info_en.inc.php
@@ -16,16 +16,19 @@ $info['theme_info_backend'] = 'This example backend theme relegates to the Styx 
 As an example it proves the ability that you can simply add a more or less empty backend theme, which uses the fallback to the "default" backend template files.
 With this in mind you are able to just change and add those files only, which are necessary to fill your needs.
 This example may change in future to hold some more real files for the backend generation.
-For the moment it solely includes an index template file, which removes relevant informations and assets when not logged in.<br>
-* 2017-08-21 - Newly fixes a UI breaking issue with the rtl (right-to-left) direction attribute in the &lt;html&gt; element.<br>
-* 2019-09-08 - Removed the IE8/9 workarounds.<br>
-* 2020-03-25 - Sign out Styx message styles with a "marked" left border.<br>
-* 2020-06-06 - Smaller font-size of entry_info styles of entries list.<br>
-* 2020-08-31 - Improved login page and update fullsize preview.<br>
-* 2020-10-31 - Add SVG icon to the plugin added fieldset legends of entryform.<br>
-* 2020-12-06 - Allow iPhone 5/SE with 320px sizes for media filter datetime pick-fields.<br>
-* 2021-02 / 2021-03 - Small Improvements to the CSS styles.<br>
-* 2021-03-26 - Filter toolbars are shifted to the right up from 360px screen width.<br><br>
+For the moment it solely includes an index template file, which removes relevant informations and assets when not logged in.
+And it has its own styles markup, equivalent to the "default/admin/styles.css" file, which makes the following continuously evolving enhancements to the backend:
+<pre>
+* 2017-08-21 - Newly fixes a UI breaking issue with the rtl (right-to-left) direction attribute in the &lt;html&gt; element.
+* 2019-09-08 - Removed the IE8/9 workarounds.
+* 2020-03-25 - Sign out Styx message styles with a "marked" left border.
+* 2020-06-06 - Smaller font-size of entry_info styles of entries list.
+* 2020-08-31 - Improved login page and update fullsize preview.
+* 2020-10-31 - Add SVG icon to the plugin added fieldset legends of entryform.
+* 2020-12-06 - Allow iPhone 5/SE with 320px sizes for media filter datetime pick-fields.
+* 2021-02 / 2021-03 - Small Improvements to the CSS styles.
+* 2021-03-26 - Filter toolbars are shifted to the right up from 360px screen width.
+</pre>
 
 THE FOLLOWING IS THE DESCRIPTION OF THE RELEGATED BACKEND&colon;<br>
 The template files in that directory build and create the look of the complete admin interface.
