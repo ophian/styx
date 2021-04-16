da7834b3a713e1d783a9c0452c28a0e964f84884
Ian <aristophian@googlemail.com>
2021-04-16 10:46:20 +0200

    clean-blog: Update info

---
+++ templates/clean-blog/lang_info_en.inc.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * The info.txt native charset (translation) file as an array.
+ * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['lang'].
+ * Convert or save as ANSI (ISO-8859-1) or your native charset, analogue to the non-UTF-8 lang constant files.
+ **/
+
+$info['theme_info_summary'] = 'Original bootstrap theme by Iron Summit Media Strategies, LLC.';
+
+$info['theme_info_desc'] = '';
