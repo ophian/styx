438b6c04f67cf1654226d634917d89987b7c6261
Ian <aristophian@googlemail.com>
2021-05-05 11:12:57 +0200

    entryproperties: Move 5db43e4d7f879adc4c9d624745c189e5d204fe66 styling into CSS

--- plugins/serendipity_event_entryproperties/serendipity_event_entryproperties.php
+++ plugins/serendipity_event_entryproperties/serendipity_event_entryproperties.php
@@ -679,7 +679,7 @@ class serendipity_event_entryproperties extends serendipity_event
                         <span class="wrap_legend"><legend><?php echo PLUGIN_EVENT_ENTRYPROPERTIES_TITLE; ?></legend></span>
                         <div id="edit_entry_submit" class="ep_entry_submit">
                             <a href="#top" class="x-button_link x-button_up" title="<?php echo UP; ?>">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#333333" class="bi bi-arrow-up-square-fill" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-arrow-up-square-fill" viewBox="0 0 16 16">
                                   <path d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z"/>
                                 </svg>
                             </a>
