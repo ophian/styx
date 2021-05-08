fdc1249625147bd1de5f664c47f163820669b446
Ian <aristophian@googlemail.com>
2021-05-08 18:26:56 +0200

    b5blog: Add skeleton bootstrap example blog congigurations

---
+++ templates/b5blog/UTF-8/lang_de.inc.php
@@ -0,0 +1,5 @@
+<?php
+@define('USE_CORENAV', 'Globale Navigation einbinden?');
+@define('B5EB_PLINK_TEXT', 'Link');
+@define('B5EB_PLINK_TITLE', 'Permanenter Link zu diesem Kommentar');
+@define('B5EB_REPLYORIGIN', 'Antwort auf');
