de82ad2586b9b2f82f3aa3a96eaa5173e1917c88
Ian <aristophian@googlemail.com>
2021-05-10 11:31:44 +0200

    b5blog: Remove partial fdc1249625147bd1de5f664c47f163820669b446 for e14319a14cf28eff501018f5717ad8864d159b87

--- templates/b5blog/lang_en.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-@define('USE_CORENAV', 'Use global navigation?');
-@define('B5EB_PLINK_TEXT', 'Link');
-@define('B5EB_PLINK_TITLE', 'Permanent link for this comment');
-@define('B5EB_REPLYORIGIN', 'Origin');
