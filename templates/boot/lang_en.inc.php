be25b01c9b36d2e686f75ecf583766172e09573c
Ian <aristophian@googlemail.com>
2021-05-14 12:02:09 +0200

    boot: [en] Add template used constants

--- templates/boot/lang_en.inc.php
+++ templates/boot/lang_en.inc.php
@@ -3,3 +3,7 @@
 @define('BS_PLINK_TEXT', 'Link');
 @define('BS_PLINK_TITLE', 'Permanent link for this comment');
 @define('BS_REPLYORIGIN', 'Origin');
+// Lang constants non-translate
+@define('BS_PLACE_SEARCH', 'Search term(s)');
+@define('BS_SEND_MAIL', 'Send email');
+
