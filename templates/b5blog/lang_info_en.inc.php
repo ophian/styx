f6d0184ebbfccb96528a80526abaa66b235a18d0
Ian <aristophian@googlemail.com>
2021-05-30 11:37:10 +0200

    b5blog: Remove startup lang info information regarding
      - optimized boot fallback pool templates
      - added theme configurations

--- templates/b5blog/lang_info_en.inc.php
+++ templates/b5blog/lang_info_en.inc.php
@@ -5,18 +5,7 @@
  * Convert or save as ANSI (ISO-8859-1) or your native charset, analogue to the non-UTF-8 lang constant files.
  **/
 
-$info['theme_info_summary'] = 'A "Bootstrap 5.0.0" based CSS HTML5 Framework Theme by Mark Otto, Jacob Thornton, and Bootstrap contributors.';
+$info['theme_info_summary'] = 'A "Bootstrap 5" based CSS HTML5 Framework Theme by Mark Otto, Jacob Thornton, and Bootstrap contributors.';
 
 $info['theme_info_desc'] = 'This is a basic interpretation of the official Bootstrap 5 Blog example theme.
-Easy to use, since you just need to change some basics in the index.tpl file <u>manually</u> by yourself, which are&colon;
-<pre>
- * Fill or remove the sidebars "About" box;
- * Fill or remove the top "Featured post" box;
- * Fill or remove the 2 grid card exemplary post pointers;
- * Add a starting Blog Posts title;
-</pre>
-Everything else regarding Serendipity Styx Navigation per theme config, blog posts or sidebar plugin items is working just like in every other theme.
-Most theme styles were just borrowed from Styx "B46" theme and might not even be in real need here.
-Most template files included here are just Styx-Barebone templates, not even tried to run further optimized
-for the Bootstrap framework class selectors, accept a basic adaptation in "entries.tpl", "sidebar.tpl" and mainly in the "index.tpl" file(s).
 An example for right-to-left (RTL) language styles is added in "blog.rtl.css".';
