2eda242660f32c60be199aa79fe1d71e6502557a
Ian <aristophian@googlemail.com>
2021-05-31 15:55:37 +0200

    config: 3.4-rc2

--- serendipity_config.inc.php
+++ serendipity_config.inc.php
@@ -52,7 +52,7 @@ if (defined('USE_MEMSNAP')) {
 }
 
 // The version string
-$serendipity['version'] = '3.4-rc1';
+$serendipity['version'] = '3.4-rc2';
 $serendipity['edition'] = 'Styx';
 
 // Setting this to 'false' will enable debugging output.
