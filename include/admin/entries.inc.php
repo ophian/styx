da7e19abd6d8a41d5579b52520cfcdce061e8966
Ian <aristophian@googlemail.com>
2021-06-08 18:45:36 +0200

    entries: Fix an uninitialied POST variable check for PHP 8

--- include/admin/entries.inc.php
+++ include/admin/entries.inc.php
@@ -499,7 +499,7 @@ switch($serendipity['GET']['adminAction']) {
         if (!serendipity_checkFormToken()) {
             return; // blank content page, but default token check parameter is presenting a XSRF message when false
         }
-        if (!is_array($serendipity['POST']['multiDelete'])) {
+        if (!isset($serendipity['POST']['multiDelete']) || !is_array($serendipity['POST']['multiDelete'])) {
             echo '<div class="msg_notice"><span class="icon-attention-circled" aria-hidden="true"></span> ' . sprintf(MULTICHECK_NO_ITEM, serendipity_specialchars($_SERVER['HTTP_REFERER'], ENT_QUOTES | ENT_HTML401)) . '</div>'."\n";
             break;
         }
