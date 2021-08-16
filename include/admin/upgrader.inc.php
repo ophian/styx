ophian/styx

commit ac1fd2ec8f537d05ffc72d893edb26eb8adb46ba

upgrader: Add informational upgrade task for 7e9881e4ebcb74fc4b78c02cfea5afe1e436540f

include/admin/upgrader.inc.php
@@ -489,6 +489,11 @@ $tasks = array(
                     'title'     => 'Removal of old dead files in v.3.4.0 (see also previous bootstrap assets note)',
                     'desc'      => 'The following old dead files will be removed from your system.<br><pre>' . implode(', ', $dead_files_340) . '</pre>'),
 
+            array(  'version'   => '3.5-alpha2',
+                    'type'      => 'IMPORTANT_CORE_NOTICE',
+                    'title'     => '<b>ADMINISTRATION_NOTICE:</b> We fixed a USER form permissive bug which was around since last Christmas (2020).',
+                    'desc'      => 'If you did submit a USER administration users form (NOT the "Personal Preferences", which does not have this field) in this time, without having explicitly set (checked) the "Access level" option, trusting its shown (select) value, please re-check to ensure it (now) has the correct Access Level (Standard editor, Chief editor, or Administrator) set for that user. This is an old (historic) permission flag, while user privileges should be handled by group memberships in Serendipity for long, but it is still used and could have done (downgrade) damage to Administrator or Chief permission levels! You (the Administrator) or your Chiefs for sure would have noticed a sudden reset to Standard editors very limited permissions right after. We do not add a (repair) task here, since it is just a number change in the database and assume nobody was hit by it yet. See this ADMINISTRATION upgrade NOTICE as an informational notice only.'),
+
 );
 // TODO: Do something meaningful with 'type', since having key type and the bold title (type) is redundant!
 

commit 471a23574f98970a16d0c684f4903c18ad9f4bc0

upgrader: Slightly shorten the title for desktop breakage in ac1fd2ec8f537d05ffc72d893edb26eb8adb46ba

include/admin/upgrader.inc.php
@@ -491,7 +491,7 @@ $tasks = array(
 
             array(  'version'   => '3.5-alpha2',
                     'type'      => 'IMPORTANT_CORE_NOTICE',
-                    'title'     => '<b>ADMINISTRATION_NOTICE:</b> We fixed a USER form permissive bug which was around since last Christmas (2020).',
+                    'title'     => '<b>ADMINISTRATION_NOTICE:</b> We fixed a USER form permissive bug which was around since late Dec. 2020.',
                     'desc'      => 'If you did submit a USER administration users form (NOT the "Personal Preferences", which does not have this field) in this time, without having explicitly set (checked) the "Access level" option, trusting its shown (select) value, please re-check to ensure it (now) has the correct Access Level (Standard editor, Chief editor, or Administrator) set for that user. This is an old (historic) permission flag, while user privileges should be handled by group memberships in Serendipity for long, but it is still used and could have done (downgrade) damage to Administrator or Chief permission levels! You (the Administrator) or your Chiefs for sure would have noticed a sudden reset to Standard editors very limited permissions right after. We do not add a (repair) task here, since it is just a number change in the database and assume nobody was hit by it yet. Consider this ADMINISTRATION upgrade NOTICE as an informational notice only.'),
 
 );