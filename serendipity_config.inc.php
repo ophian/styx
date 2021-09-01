68d75e0f6b1d1e9d412033aa3deca403c490292b
Ian <aristophian@googlemail.com>
2021-09-01 18:38:45 +0200

    config: Bump to beta-2 for 0954a7933f9037b61bd59e69c6a3816253a56602 e6338e812942351fb3e5f73384abb3d155f7ba63

--- serendipity_config.inc.php
+++ serendipity_config.inc.php
@@ -52,7 +52,7 @@ if (defined('USE_MEMSNAP')) {
 }
 
 // The version string
-$serendipity['version'] = '3.5-beta1';
+$serendipity['version'] = '3.5-beta2';
 $serendipity['edition'] = 'Styx';
 
 // Setting this to 'false' will enable debugging output.
