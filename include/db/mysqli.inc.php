3e9197aac49958dbd795c98be60433ee2ac861f2
Ian <aristophian@googlemail.com>
2021-09-02 17:02:58 +0200

    mysqli: Better check a variable on install with PHP 8

--- include/db/mysqli.inc.php
+++ include/db/mysqli.inc.php
@@ -274,7 +274,7 @@ function serendipity_db_reconnect() {
         $mysql_version = mysqli_get_server_info($serendipity['dbConn']);
         if (version_compare($mysql_version, '5.5.3', '>=')) {
             $serendipity['dbUtf8mb4_ready'] = true;
-            if (defined('IN_installer') || $use_charset == 'utf8mb4' || $serendipity['dbUtf8mb4_converted']) {
+            if (defined('IN_installer') || $use_charset == 'utf8mb4' || (isset($serendipity['dbUtf8mb4_converted']) && $serendipity['dbUtf8mb4_converted'])) {
                 $use_charset = 'utf8mb4';
                 $serendipity['dbUtf8mb4'] = true;
             }
