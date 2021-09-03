d56b373c615c6d30ac44733dff943e2187efec8b
Ian <aristophian@googlemail.com>
2021-09-03 11:22:42 +0200

    next: Remove required fields from config for 0998b381866e56601b472d037a777e88ca1c7cf5 and 177adaa77a1e885a925b092d05f220131dc74c2e

--- templates/next/config.inc.php
+++ templates/next/config.inc.php
@@ -6,24 +6,6 @@ if (IN_serendipity !== true) { die ("Don't hack!"); }
 $serendipity['smarty']->assign(array('currpage'  => "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'],
                                      'currpage2' => $_SERVER['REQUEST_URI']));
 
-if (class_exists('serendipity_event_spamblock')) {
-    $required_fieldlist = serendipity_db_query("SELECT value FROM {$serendipity['dbPrefix']}config WHERE name LIKE '%spamblock%required_fields'", true, 'assoc');
-}
-
-if (is_array($required_fieldlist)) {
-    $required_fields = explode(',', $required_fieldlist['value']);
-    $smarty_required_fields = array();
-
-    foreach($required_fields AS $required_field) {
-        $required_field = trim($required_field);
-
-        if (empty($required_field)) continue;
-            $smarty_required_fields[$required_field] = $required_field;
-        }
-
-    $serendipity['smarty']->assign('required_fields', $smarty_required_fields);
-}
-
 $template_config = array(
     array(
       'var'           => 'infonext',
