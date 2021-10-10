21c6c2a35049eca610d0846decadc284c1c86a47
Ian <aristophian@googlemail.com>
2021-10-10 10:24:41 +0200

    users: Extend additional users for c1b9baccce88d057f305e01844afc8d274db8d2e

--- include/admin/users.inc.php
+++ include/admin/users.inc.php
@@ -107,6 +107,7 @@ if (isset($_POST['SAVE_NEW']) && serendipity_checkFormToken()) {
                     }
                 }
             }
+            serendipity_set_config_var('enableBackendPopupGranular', 'categories,tags,links', $serendipity['POST']['authorid']);
 
             serendipity_plugin_api::hook_event('backend_users_add', $serendipity['POST']['user']);
             $data['user'] = $serendipity['POST']['user'];
