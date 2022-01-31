22752db59c9621082f76d671a0ea9bd959c85529
Ian <aristophian@googlemail.com>
2022-01-31 14:46:39 +0100

    genpage: Improve (reset) 0219409dff2c7b50fde9a1d2651b1d528786cbc7 for other archives cases

--- include/genpage.inc.php
+++ include/genpage.inc.php
@@ -65,6 +65,10 @@ switch (@$serendipity['GET']['action']) {
             serendipity_printEntries($entry, 1);
         } else {
             $range = $serendipity['range'] ?? null;
+            // reset summaryFetchLimit on 'C' (category), 'A' (author), 'W' (week), etc cases
+            if (isset($serendipity['uriArguments'][1]) && ($serendipity['uriArguments'][1] != 'summary' && !is_numeric($serendipity['uriArguments'][1][0]))) {
+                $serendipity['summaryFetchLimit'] = null;
+            }
             $fetchLimit = $serendipity['summaryFetchLimit'] ?? $serendipity['fetchLimit'];
             serendipity_printEntries(serendipity_fetchEntries($range, true, $fetchLimit));
         }
