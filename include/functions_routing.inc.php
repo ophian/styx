c77619c3c410ea3ba36418a67dfae402f706d78a
Ian <aristophian@googlemail.com>
2022-02-04 10:12:18 +0100

    routing API: Set most start timestamps to 1ms

--- include/functions_routing.inc.php
+++ include/functions_routing.inc.php
@@ -528,7 +528,7 @@ function serveArchives() {
 
             if (isset($week)) {
                 $tm = strtotime('+ '. ($week-2) .' WEEKS monday', mktime(0, 0, 0, 1, 1, $year));
-                $ts = mktime(0, 0, 0, date('m', $tm), date('j', $tm), $year);
+                $ts = mktime(0, 0, 1, date('m', $tm), date('j', $tm), $year);
                 $te = mktime(23, 59, 59, date('m', $tm), date('j', $tm)+7, $year);
                 $date = serendipity_formatTime(WEEK .' '. $week .', %Y', $ts, false);
             } else {
@@ -541,18 +541,18 @@ function serveArchives() {
                 } else {
                     // we have a full date order
                     if ($day) {
-                        $ts = mktime(1, 0, 1, $month, $day, $year);
+                        $ts = mktime(0, 0, 1, $month, $day, $year);
                         $te = mktime(23, 59, 59, $month, $day, $year);
                         $date = serendipity_formatTime(DATE_FORMAT_ENTRY, $ts, false);
                     // we have a year order only AND this can be either /archives/2018/summary.html OR /archives/2018.html
                     } elseif ($year && !isset($month)) {
-                        $ts = mktime(1, 0, 1, 1, 1, $year);
+                        $ts = mktime(0, 0, 1, 1, 1, $year);
                         $te = mktime(23, 59, 59, 12, 31, $year);
                         $date = $year;
                         $serendipity['summaryFetchLimit'] = $serendipity['fetchLimit'] != 25 ? 25 : 24; // check case to make it independently unique
                     // we have a month & year only order
                     } else {
-                        $ts = mktime(0, 0, 0, $month, $gday, $year);
+                        $ts = mktime(0, 0, 1, $month, $gday, $year);
                         if (!isset($gday2)) {
                             $gday2 = date('t', $ts);
                         }
@@ -588,16 +588,16 @@ function serveArchives() {
                 }
 
                 $tm = persian_mktime(0, 0, 0, $i, $day, $year);
-                $ts = persian_mktime(0, 0, 0, persian_date_utf('m', $tm), persian_date_utf('j', $tm), $year);
+                $ts = persian_mktime(0, 0, 1, persian_date_utf('m', $tm), persian_date_utf('j', $tm), $year);
                 $te = persian_mktime(23, 59, 59, persian_date_utf('m', $tm), persian_date_utf('j', $tm)+7, $year);
                 $date = serendipity_formatTime(WEEK .' '. $week .'، %Y', $ts, false);
             } else {
                 if ($day) {
-                    $ts = persian_mktime(0, 0, 0, $month, $day, $year);
+                    $ts = persian_mktime(0, 0, 1, $month, $day, $year);
                     $te = persian_mktime(23, 59, 59, $month, $day, $year);
                     $date = serendipity_formatTime(DATE_FORMAT_ENTRY, $ts, false);
                 } else {
-                    $ts = persian_mktime(0, 0, 0, $month, $gday, $year);
+                    $ts = persian_mktime(0, 0, 1, $month, $gday, $year);
                     if (!isset($gday2)) {
                         $gday2 = persian_date_utf('t', $ts);
                     }
