671258592c9a63d73823397eaf5844bcb0c47d8f
Ian <aristophian@googlemail.com>
2022-04-27 15:09:05 +0200

    entries: Fix regression of f264c2d30deafed2834e18596a8e6e1d45d64af8 in category filtered order

--- include/admin/entries.inc.php
+++ include/admin/entries.inc.php
@@ -251,7 +251,7 @@ switch($serendipity['GET']['adminAction']) {
         }
 
         // check entries list link for pinned entries
-        $pinned_entries = [];
+        $pinned_entries = $pinned = [];
         if ((!empty($serendipity['GET']['pinned_entries']) || !empty($serendipity['POST']['pinned_entries']))
         && isset($serendipity['matched_entry_pin']) && isset($serendipity['COOKIE']["entrylist_pin_entry_${serendipity['matched_entry_pin']}"])) {
             $pinned = isset($serendipity['POST']['pinned_entries']) ? explode(',', $serendipity['POST']['pinned_entries']) : explode(',', $serendipity['GET']['pinned_entries']);
