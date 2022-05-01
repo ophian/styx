e9618eb5949970286dbddfa0b95750c02ae63287
Ian <aristophian@googlemail.com>
2022-05-01 16:34:25 +0200

    deployment: Add DEPLOYMENT_PATH constant for install

--- deployment/serendipity_admin.php
+++ deployment/serendipity_admin.php
@@ -3,6 +3,7 @@
 # All rights reserved.  See LICENSE file for licensing details
 # Serendipity is provided in managed mode here
 
+@define('DEPLOYMENT_PATH', realpath(dirname(__FILE__)));
 require_once 's9y/serendipity_admin.php';
 
 /* vim: set sts=4 ts=4 expandtab : */
