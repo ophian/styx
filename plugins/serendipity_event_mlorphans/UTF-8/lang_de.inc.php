1d5964c5fe667090eabb2f5e680e026786f35629
Ian <aristophian@googlemail.com>
2022-06-12 18:03:09 +0200

    mlorphans: Improve some [de] lang constants

--- plugins/serendipity_event_mlorphans/UTF-8/lang_de.inc.php
+++ plugins/serendipity_event_mlorphans/UTF-8/lang_de.inc.php
@@ -1,14 +1,14 @@
 <?php
 
 @define('PLUGIN_EVENT_MLORPHANS_NAME', 'Waisenmanager');
-@define('PLUGIN_EVENT_MLORPHANS_DESC', 'Erlaubt, die Mediathek ab 100 vorhandenen Bildern nach verwaisten Bild-Dateien zu durchsuchen. Platzieren Sie es in der Plugin-Liste am besten vor den anderen Wartungsplugins (spamblock, modemaintain, changelog).');
+@define('PLUGIN_EVENT_MLORPHANS_DESC', 'Erlaubt, die Mediathek ab einer Größe von 100 vorhandenen Bildern nach verwaisten Bild-Dateien zu durchsuchen. Platzieren Sie es in der Plugin-Liste am besten vor den anderen Wartungsplugins (spamblock, modemaintain, changelog).');
 @define('PLUGIN_EVENT_MLORPHANS_SUBMIT', 'Suche nach Bilder-Waisen');
 
 @define('MLORPHAN_MTASK_ML_REAL_IMAGES', 'Die Mediathek enthält %d echte lokale Bilder.');
 @define('MLORPHAN_MTASK_MAIN_PATTERN_MATCHES', '<b>%d</b> mit ID ausgezeichnete img src Inhalte wurden in den Blogeinträgen und Statischen Seiten gefunden und im ($im) array abgelegt (<em>Doubletten sind möglich</em>).');
 @define('MLORPHAN_MTASK_MAIN_PATTERN_ID_ERROR', 'Bitte korrigieren Sie umgehend: Eintrag: %d  [<em>%s</em> textarea Feld] auf ein <em>voraussichtlich</em> <b>falsch</b> genutztes "<code>&lt;-- s9ymdb: %d --&gt;</code>" tag zu einem Bild src Pfad von "<em>%s</em>" und starten danach dieses Skript erneut. Für die gesuchte Bild-ID, suchen Sie in ihrer Mediathek, zB. im GET String ( &serendipity[fid]=XX ) der Medien Eigenschaften, nach der zugehörenden ID, oder durchforsten Sie das Waisen-Array in der untenstehenden Ausklappbox "view array" nach "<em>%s</em>", um die korrekte Bild ID [id] zu erfahren.');
 @define('MLORPHAN_MTASK_MAIN_PATTERN_RESULTCHECK_ACTION', '<em>Prüfe auf falsch gesetzte Bild-s9ymdb:IDs und/oder auf falsche Namen... und entferne gefundene Bilder aus dem Array...</em>');
-@define('MLORPHAN_MTASK_PNCASE_REVERSECHECK_ACTION', '<em>Letzte Gegenprüfung gegen Datenbankinhalte... und mögliche quickblog Bild tags...</em>');
+@define('MLORPHAN_MTASK_PNCASE_REVERSECHECK_ACTION', '<em>Letzte Überprüfung gegen Datenbankinhalte... und mögliche quickblog Bild tags...</em>');
 @define('MLORPHAN_MTASK_PNCASE_TITLE', 'Wiederholende Überprüfung der gefundenen verwaisten Bild-IDs');
 @define('MLORPHAN_MTASK_PNCASE_NOTE', '<em>die möglicherweise immer noch in Einträgen oder Statischen Seiten vorkommen. Dies sind wahrscheinlich unbekannte regex pattern Treffer, oder Pfade die nicht zu diesem Blog gehören.</em><br>Wenn Sie möchten, überprüfen Sie folgende Einträge und starten Sie dieses Skript erneut.<br>Dazu kopieren Sie sich die "s9ymdb" Bild ID, zB. "49", und suchen dann in den beiden textareas (source) der genannten Eintrags-ID (zb. 8) nach einem "s9ymdb:49" tag. Schauen Sie sich die Pfade genau an. Sollten diese doch auf ihren Blog verweisen, überprüfen Sie anschließend anhand der IMG ID 49 in ihrer Mediathek, den dazugehörigen Bildnamen. Dann wissen Sie, ob in ihrem Blog Eintrag etwas falsch verdrahtet bzw. zugewiesen ist, das einer Erneuerung bedarf.');
 @define('MLORPHAN_MTASK_PNCASE_EXCLUDING', 'Ausschließen dieser Bild-IDs: (<b>%s</b>) vom Waisen-Array.');
