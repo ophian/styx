b6db9af2329066ae0843619f7fea4012aabdb8e2
Ian <aristophian@googlemail.com>
2022-07-01 12:51:05 +0200

    remoterss: Convert utf8_en/decode to mb_convert_encoding for PHP 8.2 / 9.0

--- plugins/serendipity_plugin_remoterss/serendipity_plugin_remoterss.php
+++ plugins/serendipity_plugin_remoterss/serendipity_plugin_remoterss.php
@@ -277,7 +277,7 @@ class serendipity_plugin_remoterss extends serendipity_plugin
         $propbag->add('description',   PLUGIN_REMOTERSS_BLAHBLAH);
         $propbag->add('stackable',     true);
         $propbag->add('author',        'Udo Gerhards, Richard Thomas Harrison, Ian Styx');
-        $propbag->add('version',       '1.31');
+        $propbag->add('version',       '1.32');
         $propbag->add('requirements',  array(
             'serendipity' => '3.1',
             'smarty'      => '3.1',
@@ -1001,7 +1001,11 @@ class serendipity_plugin_remoterss extends serendipity_plugin
                 } elseif (function_exists('recode')) {
                     $out = recode('iso-8859-1..' . LANG_CHARSET, $string);
                 } elseif (LANG_CHARSET == 'UTF-8') {
-                    return utf8_encode($string);
+                    if (!function_exists('mb_convert_encoding')) {
+                        return @utf8_encode($string); // Deprecation in PHP 8.2, removal in PHP 9.0
+                    } else {
+                        return mb_convert_encoding($string, 'UTF-8', 'ISO-8859-1'); // string, to, from
+                    }
                 } else {
                     return $string;
                 }
@@ -1009,7 +1013,11 @@ class serendipity_plugin_remoterss extends serendipity_plugin
 
             case 'UTF-8':
             default:
-                $out = utf8_decode($string);
+                if (!function_exists('mb_convert_encoding')) {
+                    $out = @utf8_decode($string); // Deprecation in PHP 8.2, removal in PHP 9.0
+                } else {
+                    $out = mb_convert_encoding($string, 'ISO-8859-1', 'UTF-8'); // string, to, from
+                }
                 return $out;
         }
     }
