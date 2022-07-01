97010e35a07f7406caa3de2027a998d2f66ecc79
Ian <aristophian@googlemail.com>
2022-07-01 12:51:32 +0200

    wfwcomment: Convert utf8_en/decode to mb_convert_encoding for PHP 8.2 / 9.0

--- wfwcomment.php
+++ wfwcomment.php
@@ -11,24 +11,50 @@ if ($_REQUEST['cid'] != '' && $HTTP_RAW_POST_DATA != '') {
         preg_match('@<dc:creator[^>]*>(.*)</dc:creator[^>]*>@i', $HTTP_RAW_POST_DATA, $name);
     }
 
+    // this means API comments, which probably are trackbacks, are meant to be ISO-8859-1 7bit only ??
+
     if (isset($name[1]) && !empty($name[1])) {
         if (preg_match('@^(.*)\((.*)\)@i', $name[1], $names)) {
-            $comment['name'] = utf8_decode($names[2]);
-            $comment['email'] = utf8_decode($names[1]);
+            if (!function_exists('mb_convert_encoding')) {
+                $comment['name'] = @utf8_decode($names[2]); // Deprecation in PHP 8.2, removal in PHP 9.0
+            } else {
+                $comment['name'] = mb_convert_encoding($names[2], 'ISO-8859-1', 'UTF-8'); // string, to, from
+            }
+            if (!function_exists('mb_convert_encoding')) {
+                $comment['email'] = @utf8_decode($names[1]); // Deprecation in PHP 8.2, removal in PHP 9.0
+            } else {
+                $comment['name'] = mb_convert_encoding($names[1], 'ISO-8859-1', 'UTF-8'); // string, to, from
+            }
         } else {
-            $comment['name'] = utf8_decode($name[1]);
+            if (!function_exists('mb_convert_encoding')) {
+                $comment['name'] = @utf8_decode($name[1]); // Deprecation in PHP 8.2, removal in PHP 9.0
+            } else {
+                $comment['name'] = mb_convert_encoding($name[1], 'ISO-8859-1', 'UTF-8'); // string, to, from
+            }
         }
     }
 
     if (preg_match('@<link[^>]*>(.*)</link[^>]*>@i', $HTTP_RAW_POST_DATA, $link)) {
-        $comment['url'] = utf8_decode($link[1]);
+        if (!function_exists('mb_convert_encoding')) {
+            $comment['url'] = @utf8_decode($link[1]); // Deprecation in PHP 8.2, removal in PHP 9.0
+        } else {
+            $comment['url'] = mb_convert_encoding($link[1], 'ISO-8859-1', 'UTF-8'); // string, to, from
+        }
     }
 
     if (preg_match('@<description[^>]*>(.*)</description[^>]*>@ims', $HTTP_RAW_POST_DATA, $description)) {
         if (preg_match('@^<!\[CDATA\[(.*)\]\]>@ims', $description[1], $cdata)) {
-            $comment['comment'] = utf8_decode($cdata[1]);
+            if (!function_exists('mb_convert_encoding')) {
+                $comment['comment'] = @utf8_decode($cdata[1]); // Deprecation in PHP 8.2, removal in PHP 9.0
+            } else {
+                $comment['comment'] = mb_convert_encoding($cdata[1], 'ISO-8859-1', 'UTF-8'); // string, to, from
+            }
         } else {
-            $comment['comment'] = utf8_decode($description[1]);
+            if (!function_exists('mb_convert_encoding')) {
+                $comment['comment'] = @utf8_decode($description[1]); // Deprecation in PHP 8.2, removal in PHP 9.0
+            } else {
+                $comment['comment'] = mb_convert_encoding($description[1], 'ISO-8859-1', 'UTF-8'); // string, to, from
+            }
         }
 
         if (!empty($comment['comment'])) {
