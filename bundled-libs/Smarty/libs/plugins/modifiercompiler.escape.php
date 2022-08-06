635cb16fce3e7eca798fedb78baf61d206230b51
Ian <aristophian@googlemail.com>
2022-08-06 15:17:32 +0200

    smarty: Improve indent and readability in cc0c2c4cb5ba4a5ea7c82faa4b53b2a2a147088f

--- bundled-libs/Smarty/libs/plugins/modifiercompiler.escape.php
+++ bundled-libs/Smarty/libs/plugins/modifiercompiler.escape.php
@@ -46,8 +46,8 @@ function smarty_modifiercompiler_escape($params, Smarty_Internal_TemplateCompile
             case 'htmlall':
                 if (Smarty::$_MBSTRING) {
                     return 'htmlspecialchars_decode(mb_convert_encoding(htmlentities(' . $params[ 0 ] . ', ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML401, ' .
-                                var_export($char_set, true) . ', ' . var_export($double_encode, true) .
-                                '), ' . var_export($char_set, true) . '))'; // PHP 8.2 sets HTML-ENTITIES deprecated and PHP 8.1 gave up ENT_COMPAT for the triplet default
+                        var_export($char_set, true) . ', ' . var_export($double_encode, true) .'), ' .
+                        var_export($char_set, true) . '))'; // PHP 8.2 sets HTML-ENTITIES deprecated and PHP 8.1 gives up ENT_COMPAT for the triplet default
                 }
                 // no MBString fallback
                 return 'htmlentities(' . $params[ 0 ] . ', ENT_QUOTES, ' . var_export($char_set, true) . ', ' .
