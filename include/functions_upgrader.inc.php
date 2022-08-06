5c72e4b4888f280e374160db76217aeeff28d73e
Ian <aristophian@googlemail.com>
2022-08-06 13:11:25 +0200

    upgrader API: Extend 654360b3b2c6c5550b6a8ff19a7c09d9884c03c1 for 4e88ebb5f1aa544c0961c863e00de113804d19a2

--- include/functions_upgrader.inc.php
+++ include/functions_upgrader.inc.php
@@ -496,6 +496,7 @@ $dead_files_400 = array(
     'bundled-libs/Smarty/libs/SmartyBC.class.php',
     'bundled-libs/Smarty/libs/sysplugins/smarty_internal_compile_include_php.php',
     'bundled-libs/Smarty/libs/sysplugins/smarty_internal_compile_private_php.php',
+    'bundled-libs/Smarty/libs/sysplugins/smarty_internal_resource_registered.php',
 );
 
 /**
