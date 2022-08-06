6f1ad785ab7b77b3945ef15ce18832c74bd1db57
Ian <aristophian@googlemail.com>
2022-08-06 14:48:44 +0200

    smarty: Bump dev version .29 for d7a2a631c2838539addf957f0063faafbb665832

--- bundled-libs/Smarty/libs/Smarty.class.php
+++ bundled-libs/Smarty/libs/Smarty.class.php
@@ -111,7 +111,7 @@ class Smarty extends Smarty_Internal_TemplateBase
     /**
      * smarty version
      */
-    const SMARTY_VERSION = '3.1.39-dev-28';
+    const SMARTY_VERSION = '3.1.39-dev-29';
     /**
      * define variable scopes
      */
