fbe43f9bc95447d00df2d0d68b67e2ad8a29a736
Ian <aristophian@googlemail.com>
2022-08-20 17:46:28 +0200

    functions API: Add new serendipity_toDateTimeMapper() function

--- include/functions.inc.php
+++ include/functions.inc.php
@@ -245,7 +245,7 @@ function serendipity_emptyArray($array) {
 }
 
 /**
- * Get the Referer calling function name for the current HTTP Request
+ * Get the Referrer calling function name for the current HTTP Request
  *
  * @access public
  * @return string parent level function name
@@ -296,7 +296,190 @@ function serendipity_gzCompression() {
  */
 function serendipity_validateDate($date, $format = 'Y-m-d') {
     $d = DateTime::createFromFormat($format, $date);
-    return $d && $d->format($format) === $date; // bee strict
+    return $d && $d->format($format) === $date; // be strict
+}
+
+/**
+ * Convert input entry strftime() dates to use DateTime Interface successor
+ *
+ * Locale-formatted strftime using \IntlDateFormatter (PHP 8.1 ++ compatible)
+ * This provides a cross-platform alternative to strftime() for when it will be removed from PHP.
+ * Note that output can be slightly different between libc sprintf and this function as it is using ICU.
+ *
+ * @access public
+ * @param  string $format Date format
+ * @param  integer|string|DateTime $timestamp Timestamp
+ * @return string
+ *
+ * Origin Usage:
+ * use function \PHP81_BC\strftime;
+ * echo strftime('%A %e %B %Y %X', new \DateTime('2021-09-28 00:00:00'), 'fr_FR');
+ *
+ * @author BohwaZ <https://bohwaz.net/>
+ * @see https://gist.github.com/bohwaz/42fc223031e2b2dd2585aab159a20f30
+ */
+function serendipity_toDateTimeMapper(string $format, $timestamp = null, ?string $locale = null): string
+{
+	if (null === $timestamp) {
+		$timestamp = new \DateTime;
+	}
+	elseif (is_numeric($timestamp)) {
+		$timestamp = date_create('@' . $timestamp);
+
+		if ($timestamp) {
+			$timestamp->setTimezone(new \DateTimezone(date_default_timezone_get()));
+		}
+	}
+	elseif (is_string($timestamp)) {
+		$timestamp = date_create($timestamp);
+	}
+
+	if (!($timestamp instanceof \DateTimeInterface)) {
+		throw new \InvalidArgumentException('The given $timestamp argument is neither a valid UNIX timestamp, a valid date-time string nor a DateTime object.');
+	}
+
+	$locale = substr((string) $locale, 0, 5);
+
+	$intl_formats = [
+		'%a' => 'EEE',	// An abbreviated textual representation of the day	Sun through Sat (sadly adding ,. in [de] see below)
+		'%A' => 'EEEE',	// A full textual representation of the day	Sunday through Saturday
+		'%b' => 'MMM',	// Abbreviated month name, based on the locale	Jan through Dec
+		'%B' => 'MMMM',	// Full month name, based on the locale	January through December
+		'%h' => 'MMM',	// Abbreviated month name, based on the locale (an alias of %b)	Jan through Dec
+	];
+
+	$intl_formatter = function (\DateTimeInterface $timestamp, string $format) use ($intl_formats, $locale) {
+		$tz = $timestamp->getTimezone();
+		$date_type = \IntlDateFormatter::FULL;
+		$time_type = \IntlDateFormatter::FULL;
+		$pattern = '';
+
+		// %c = Preferred date and time stamp based on locale
+		// Example: Tue Feb 5 00:45:10 2009 for February 5, 2009 at 12:45:10 AM
+		if ($format == '%c') {
+			$date_type = \IntlDateFormatter::LONG;
+			$time_type = \IntlDateFormatter::SHORT;
+		}
+		// %x = Preferred date representation based on locale, without the time
+		// Example: 02/05/09 for February 5, 2009
+		elseif ($format == '%x') {
+			$date_type = \IntlDateFormatter::SHORT;
+			$time_type = \IntlDateFormatter::NONE;
+		}
+		// Localized time format
+		elseif ($format == '%X') {
+			$date_type = \IntlDateFormatter::NONE;
+			$time_type = \IntlDateFormatter::MEDIUM;
+		}
+		else {
+			$pattern = $intl_formats[$format];
+		}
+
+		return (new \IntlDateFormatter($locale, $date_type, $time_type, $tz, null, $pattern))->format($timestamp);
+	};
+
+	// Same order as https://www.php.net/manual/en/function.strftime.php
+	$translation_table = [
+		// Day (Later on remove the dot from short abbreviations eg for DE language Mo. Di. etc. in calendar, and comments sidebar plugins)
+		'%a' => $intl_formatter,
+		'%A' => $intl_formatter,
+		'%d' => 'd',
+		'%e' => function ($timestamp) {
+			return sprintf('% 2u', $timestamp->format('j'));
+		},
+		'%j' => function ($timestamp) {
+			// Day number in year, 001 to 366
+			return sprintf('%03d', $timestamp->format('z')+1);
+		},
+		'%u' => 'N',
+		'%w' => 'w',
+
+		// Week
+		'%U' => function ($timestamp) {
+			// Number of weeks between date and first Sunday of year
+			$day = new \DateTime(sprintf('%d-01 Sunday', $timestamp->format('Y')));
+			return sprintf('%02u', 1 + ($timestamp->format('z') - $day->format('z')) / 7);
+		},
+		'%V' => 'W',
+		'%W' => function ($timestamp) {
+			// Number of weeks between date and first Monday of year
+			$day = new \DateTime(sprintf('%d-01 Monday', $timestamp->format('Y')));
+			return sprintf('%02u', 1 + ($timestamp->format('z') - $day->format('z')) / 7);
+		},
+
+		// Month
+		'%b' => $intl_formatter,
+		'%B' => $intl_formatter,
+		'%h' => $intl_formatter,
+		'%m' => 'm',
+
+		// Year
+		'%C' => function ($timestamp) {
+			// Century (-1): 19 for 20th century
+			return floor($timestamp->format('Y') / 100);
+		},
+		'%g' => function ($timestamp) {
+			return substr($timestamp->format('o'), -2);
+		},
+		'%G' => 'o',
+		'%y' => 'y',
+		'%Y' => 'Y',
+
+		// Time
+		'%H' => 'H',
+		'%k' => function ($timestamp) {
+			return sprintf('% 2u', $timestamp->format('G'));
+		},
+		'%I' => 'h',
+		'%l' => function ($timestamp) {
+			return sprintf('% 2u', $timestamp->format('g'));
+		},
+		'%M' => 'i',
+		'%p' => 'A', // AM PM (this is reversed on purpose!)
+		'%P' => 'a', // am pm
+		'%r' => 'h:i:s A', // %I:%M:%S %p
+		'%R' => 'H:i', // %H:%M
+		'%S' => 's',
+		'%T' => 'H:i:s', // %H:%M:%S
+		'%X' => $intl_formatter, // Preferred time representation based on locale, without the date
+
+		// Timezone
+		'%z' => 'O',
+		'%Z' => 'T',
+
+		// Time and Date Stamps
+		'%c' => $intl_formatter,
+		'%D' => 'm/d/Y',
+		'%F' => 'Y-m-d',
+		'%s' => 'U',
+		'%x' => $intl_formatter,
+	];
+
+	$out = preg_replace_callback('/(?<!%)(%[a-zA-Z])/', function ($match) use ($translation_table, $timestamp) {
+		if ($match[1] == '%n') {
+			return "\n";
+		}
+		elseif ($match[1] == '%t') {
+			return "\t";
+		}
+
+		if (!isset($translation_table[$match[1]])) {
+			throw new \InvalidArgumentException(sprintf('Format "%s" is unknown in time format', $match[1]));
+		}
+
+		$replace = $translation_table[$match[1]];
+
+		if (is_string($replace)) {
+			return $timestamp->format($replace);
+		}
+		else {
+			return $replace($timestamp, $match[1]);
+		}
+	}, $format);
+
+	$out = str_replace('%%', '%', $out);
+
+	return $out;
 }
 
 /**
@@ -386,10 +569,18 @@ function serendipity_strftime($format, $timestamp = null, $useOffset = true, $us
                 } elseif ($useOffset) {
                     $timestamp = serendipity_serverOffsetHour($timestamp);
                 }
-                if (PHP_VERSION_ID >= 80100) {
+                /*
+                  strftime is infected by thread unsafe locales, which is plenty of reason to deprecate it, with additional pro reasons for doing so being its disparate functionality among different os-es and libc's.
+                  Deprecation also doesn't mean removal, which won't happen until PHP 9, giving developers plenty of time to move to a saner threadsafe locale API based on intl/icu.
+                  cheers Derick
+                */
+                if ((PHP_VERSION_ID >= 80100 && PHP_VERSION_ID < 80200) || (PHP_VERSION_ID >= 80200 && !extension_loaded('intl'))) {
                     $out = @strftime($format, $timestamp); // temporary disable deprecation notice with PHP 8.1 until found better solution with %A, %d. %B %Y alike formats, on frontend calls. Using date() replacement is doing well with generic formats like "%Y-%m-%d %H:%M:%S".
+                } elseif (PHP_VERSION_ID >= 80200) {
+                    // ICU71 is fixed up from PHP 8.2
+                    $out = serendipity_toDateTimeMapper($format, $timestamp, WYSIWYG_LANG);
                 } else {
-                    $out = strftime($format, $timestamp);
+                    $out = strftime($format, $timestamp); // legacy default
                 }
                 break;
 
@@ -406,7 +597,7 @@ function serendipity_strftime($format, $timestamp = null, $useOffset = true, $us
         }
     }
 
-    if ($is_win_utf && (empty($serendipity['calendar']) || $serendipity['calendar'] == 'gregorian')) {
+    if ($is_win_utf && PHP_VERSION_ID < 80200 && (empty($serendipity['calendar']) || $serendipity['calendar'] == 'gregorian')) {
         if (!function_exists('mb_convert_encoding')) {
             $out = @utf8_encode($out); // Deprecation in PHP 8.2, removal in PHP 9.0
         } else {
