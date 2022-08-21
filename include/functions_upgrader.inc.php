5030e068d4b44da667312ecfef4f7b55652a510e
Ian <aristophian@googlemail.com>
2022-08-21 09:47:49 +0200

    upgrader API: Add items removal task for 918a844aa2fb1f4defe679785796920fe157e3e7 and 392622b1be0ffc9fc05414f689b8f93c7282c499

--- include/functions_upgrader.inc.php
+++ include/functions_upgrader.inc.php
@@ -497,6 +497,8 @@ $dead_files_400 = array(
     'bundled-libs/Smarty/libs/sysplugins/smarty_internal_compile_include_php.php',
     'bundled-libs/Smarty/libs/sysplugins/smarty_internal_compile_private_php.php',
     'bundled-libs/Smarty/libs/sysplugins/smarty_internal_resource_registered.php',
+    'themes/2k11/oldie.css',
+    'themes/next/oldie.css'
 );
 
 /**
