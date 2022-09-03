a2c852b9019e5ededd482b154054f06e9b6cc941
Ian <aristophian@googlemail.com>
2022-09-03 14:44:41 +0200

    smarty: Bump 4.2.0 DEV version to .12 for e8bada6bdcb13fa0b67457a515c8906409a01ba9 and ec0250affe305b0f42cbaec93e2596eef5aee078

--- bundled-libs/Smarty/libs/Smarty.class.php
+++ bundled-libs/Smarty/libs/Smarty.class.php
@@ -111,7 +111,7 @@ class Smarty extends Smarty_Internal_TemplateBase
     /**
      * smarty version
      */
-    const SMARTY_VERSION = '4.2.0-dev-11';
+    const SMARTY_VERSION = '4.2.0-dev-12';
     /**
      * define variable scopes
      */
