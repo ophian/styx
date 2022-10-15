c1d191c0485e405459a6eab52450bc8d29a8dcad
Ian <aristophian@googlemail.com>
2022-10-15 13:31:01 +0200

    personal: Define random USERCONF_NEW_PASSWDEX_TOOLTIP_INFO

--- include/admin/personal.inc.php
+++ include/admin/personal.inc.php
@@ -12,6 +12,8 @@ if (!serendipity_checkPermission('personalConfiguration')) {
     return;
 }
 
+define('USERCONF_NEW_PASSWDEX_TOOLTIP_INFO', ' - ' . strtolower(WORD_OR) . " -<br>\n" . sprintf('<span class="newrex" title="' . USERCONF_PASSWORD_RANDOM . '"><span class="icon-info-circled" aria-hidden="true"></span>%s</span>', serendipity_generate_password(20)));// no space with %s !
+
 $from = array();
 
 if ($serendipity['GET']['adminAction'] == 'save' && serendipity_checkFormToken()) {
