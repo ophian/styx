e00010397b0042431942681ff9b8da2ce56349f8
Ian <aristophian@googlemail.com>
2022-10-19 11:42:42 +0200

    importer::wordpress: Fix b4fd6bf3bc6596546262344626d0d38a93308674

--- include/admin/importers/wordpress.inc.php
+++ include/admin/importers/wordpress.inc.php
@@ -134,7 +134,7 @@ class Serendipity_Import_WordPress extends Serendipity_Import
                 $data = array('right_publish' => (!isset($users[$x]['user_level']) || $users[$x]['user_level'] >= 1) ? 1 : 0,
                               'realname'      => $users[$x]['display_name'] ?? $users[$x]['user_nicename'] ?? $users[$x]['user_login'],
                               'username'      => in_array($users[$x]['user_login'], $ul) ? 'wp_' . $users[$x]['user_login'] : $users[$x]['user_login'],/*try to not allow non-unique usernames*/
-                              'password'      => serendipity_generate_password(20)); // WP uses MD5 or a salted MD5. So we have to create a new Styx password and keep it in an array to inform imported users later per email (if available)
+                              'password'      => serendipity_hash(serendipity_generate_password(20))); // WP uses MD5 or a salted MD5. So we have to create a new Styx password and keep it in an array to inform imported users later per email (if available)
 
                 if (isset($users[$x]['user_level']) && $users[$x]['user_level'] <= 1) {
                     $data['userlevel'] = USERLEVEL_EDITOR;
