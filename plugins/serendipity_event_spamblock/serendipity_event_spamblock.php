8903ae11efb840c9a112feb37059a8a38ffb330a
Ian <aristophian@googlemail.com>
2022-11-16 11:30:20 +0100

    spamblock: Fix entrytitle <=> comment filter verification for htmlcomments

--- plugins/serendipity_event_spamblock/serendipity_event_spamblock.php
+++ plugins/serendipity_event_spamblock/serendipity_event_spamblock.php
@@ -28,7 +28,7 @@ class serendipity_event_spamblock extends serendipity_event
             'smarty'      => '3.1.0',
             'php'         => '7.1.0'
         ));
-        $propbag->add('version',       '2.62');
+        $propbag->add('version',       '2.63');
         $propbag->add('event_hooks',    array(
             'frontend_saveComment' => true,
             'external_plugin'      => true,
@@ -1080,7 +1080,7 @@ class serendipity_event_spamblock extends serendipity_event
                         // Check if entry title is the same as comment body or a combination of blog title and entry title (bot spam)
                         if (serendipity_db_bool($this->get_config('entrytitle', 'true'))) {
                             // Remove the blog name from the comment which might be in <title>
-                            $comment = str_replace($serendipity['blogTitle'], '', $addData['comment']);
+                            $comment = str_replace($serendipity['blogTitle'], '', strip_tags($addData['comment']));
                             $comment = str_replace($eventData['title'], '', $comment);
                             // Now blog- and entry title was stripped from comment.
                             // Remove special letters, that might have been between them:
