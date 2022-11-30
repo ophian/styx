bc36774adabfaebb0be67e9c009dd37faa48f375
Ian <aristophian@googlemail.com>
2022-11-30 14:00:08 +0100

    spamblock: Add https check for maximum number of links before rejecting
    For today 95% of traffic running over Google services is https.
    So probably even the bad link over https nowadays.

--- plugins/serendipity_event_spamblock/serendipity_event_spamblock.php
+++ plugins/serendipity_event_spamblock/serendipity_event_spamblock.php
@@ -28,7 +28,7 @@ class serendipity_event_spamblock extends serendipity_event
             'smarty'      => '3.1.0',
             'php'         => '7.1.0'
         ));
-        $propbag->add('version',       '2.67');
+        $propbag->add('version',       '2.68');
         $propbag->add('event_hooks',    array(
             'frontend_saveComment' => true,
             'external_plugin'      => true,
@@ -1234,6 +1234,8 @@ class serendipity_event_spamblock extends serendipity_event
 
                         // Check for maximum number of links before rejecting
                         $link_count = substr_count(strtolower($addData['comment']), 'http://');
+                        $add2_count = substr_count(strtolower($addData['comment']), 'https://');
+                        $link_count = $link_count + $add2_count;
                         if ($links_reject > 0 && $link_count > $links_reject) {
                             $this->log($logfile, $eventData['id'], 'REJECTED', PLUGIN_EVENT_SPAMBLOCK_REASON_LINKS_REJECT, strip_tags(json_encode($addData)));
                             $eventData = array('allow_comments' => false);
@@ -1621,7 +1623,9 @@ if (isset($serendipity['GET']['cleanspamsg'])) {
         // Check for word filtering
         if ($filter_type = $this->get_config('contentfilter_activate', 'moderate')) {
 
-            if ($ftc) $filter_type = 'reject';
+            if ($ftc) {
+                $filter_type = 'reject';
+            }
 
             // Filter authors names
             $filter_authors = explode(';', $this->get_config('contentfilter_authors', $this->filter_defaults['authors']));
@@ -1724,6 +1728,8 @@ if (isset($serendipity['GET']['cleanspamsg'])) {
         if ($ftc) {
             // Check for maximum number of links before rejecting
             $link_count = substr_count(strtolower($addData['comment']), 'http://');
+            $add2_count = substr_count(strtolower($addData['comment']), 'https://');
+            $link_count = $link_count + $add2_count;
             $links_reject = $this->get_config('links_reject', 20);
             if ($links_reject > 0 && $link_count > $links_reject) {
                 $this->log($logfile, $eventData['id'], 'REJECTED', PLUGIN_EVENT_SPAMBLOCK_REASON_LINKS_REJECT, strip_tags(json_encode($addData)));
