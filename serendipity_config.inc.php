3a0992951d4a8ab7ce3b1ef2088c095b3d3a1338
Ian <aristophian@googlemail.com>
2022-11-30 14:01:43 +0100

    config: Release 4.0-rc1

--- serendipity_config.inc.php
+++ serendipity_config.inc.php
@@ -52,7 +52,7 @@ if (defined('USE_MEMSNAP')) {
 }
 
 // The version string
-$serendipity['version'] = '4.0-beta2';
+$serendipity['version'] = '4.0-rc1';
 $serendipity['edition'] = 'Styx';
 
 // Setting this to 'false' will enable debugging output.
