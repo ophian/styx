8636c30878cc0c0cae46c8f6e8a19bd71f46860b
Ian <aristophian@googlemail.com>
2023-03-15 11:21:09 +0100

    config: Release internal 4.1-beta1

--- serendipity_config.inc.php
+++ serendipity_config.inc.php
@@ -52,7 +52,7 @@ if (defined('USE_MEMSNAP')) {
 }
 
 // The version string
-$serendipity['version'] = '4.1-alpha2';
+$serendipity['version'] = '4.1-beta1';
 $serendipity['edition'] = 'Styx';
 
 // Setting this to 'false' will enable debugging output.
