65ee306148113326e11c6679a7c122cd433f4f9f
Ian <aristophian@googlemail.com>
2023-03-27 14:10:31 +0200

    b53: Improve 780cdc5e69d18da26ec719683df301bf0eb65c43

--- templates/b53/lang_info_en.inc.php
+++ templates/b53/lang_info_en.inc.php
@@ -5,10 +5,12 @@
  * Convert or save as ANSI (ISO-8859-1) or your native charset, analogue to the non-UTF-8 lang constant files.
  **/
 
-$info['theme_info_summary'] = 'A "Bootstrap 5.3" based CSS HTML5 Framework Theme by Ian Styx. Based on Styx "boot" theme engine.';
+$info['theme_info_summary'] = 'A "Bootstrap 5.3" based CSS HTML5 Framework Theme by Ian Styx. Based on the Styx "boot" pool theme engine.';
 
-$info['theme_info_desc'] = 'This is a basic interpretation for Bootstrap 5.3, including new bootstrap dark/light mode handler.
-Bootstrap 5.3 is a huge improvement for the Series 5 and currently is delivered as an alpha2 state, which is near to its final.
-This color handling actually is not finally finished yet and will probably last until v6 arrives. Bootstrap currently is limited
-by backwards compatibility in changing key components like buttons and more. It is also limited by color manipulation in vanilla
-CSS vs Sass.';
+$info['theme_info_desc'] = 'This is a basic interpretation for brand new Bootstrap 5.3, including the dark/light or auto mode implementation handler.
+Upcoming Bootstrap 5.3 is a huge improvement for the Series 5 and currently is delivered as an alpha2 state, which is near to its final.
+The color handling frameset regarding native vs. variable color modes, in special for the new dark/light mode implementation, actually is not finally
+finished yet and will probably last until v.6 arrives, since Bootstrap colors are limited by backwards compatibility in changing key components like
+buttons and more. It also is limited by color manipulation in vanilla CSS vs Sass.<br>
+For early state, this "b53" theme is currently implemented standalone regarding the v.5.3 bootstrap assets. Serendipity Styx will hand over
+these assets in general to the templates b5/ assets dir, when time has come and all other b5 themes were found working.';
