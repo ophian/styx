780cdc5e69d18da26ec719683df301bf0eb65c43
Ian <aristophian@googlemail.com>
2023-03-27 13:17:55 +0200

    b53: Add lang_info_en file for theme_info_summary and theme_info_desc

---
+++ templates/b53/lang_info_en.inc.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * The info.txt native charset (translation) file as an array.
+ * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['lang'].
+ * Convert or save as ANSI (ISO-8859-1) or your native charset, analogue to the non-UTF-8 lang constant files.
+ **/
+
+$info['theme_info_summary'] = 'A "Bootstrap 5.3" based CSS HTML5 Framework Theme by Ian Styx. Based on Styx "boot" theme engine.';
+
+$info['theme_info_desc'] = 'This is a basic interpretation for Bootstrap 5.3, including new bootstrap dark/light mode handler.
+Bootstrap 5.3 is a huge improvement for the Series 5 and currently is delivered as an alpha2 state, which is near to its final.
+This color handling actually is not finally finished yet and will probably last until v6 arrives. Bootstrap currently is limited
+by backwards compatibility in changing key components like buttons and more. It is also limited by color manipulation in vanilla
+CSS vs Sass.';
