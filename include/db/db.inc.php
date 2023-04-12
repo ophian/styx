19b8e3036bc1e56e10127e716b86a0f8991ab37d
Ian <aristophian@googlemail.com>
2023-04-12 10:10:23 +0200

    db: Fix 74061c2b9cc532b74667625b16737c0d0075d560 for PHP 8.1
    PHP 8.2 works with both, so we can decide later what to do about it.
    
    Fixes #40

--- include/db/db.inc.php
+++ include/db/db.inc.php
@@ -70,9 +70,9 @@ function serendipity_db_insert($table, $values, $action = 'execute') {
         if (strlen($vals)) {
             $vals .= ', ';
         }
-        // See db::serendipity_set_config_var() mentioned issue that arrays were build for the web as strings and only highly late PHP versions as of PHP 8.1/2 are able to type cast to INTs.
-        if (PHP_VERSION_ID < 80100 && $table == 'config' && $k === array_key_last($values) && is_int($v)) {
-            $vals .= "$v"; // it is an integer field and PHP below 8.1 will throw a MYSQLI ERROR EXCEPTION which cannot be suppressed by @, since then the value is not stored!
+        // See db::serendipity_set_config_var() mentioned issue that arrays were build for the web as strings and only highly late PHP versions as of PHP 8.2 are able to type cast to INTs.
+        if (PHP_VERSION_ID < 80200 && $table == 'config' && $k === array_key_last($values) && is_int($v)) {
+            $vals .= "$v"; // it is an integer field and PHP below 8.2 will throw a MYSQLI ERROR EXCEPTION which cannot be suppressed by @, since then the value is not stored!
         } else {
             $vals .= '\'' . serendipity_db_escape_string($v) . '\'';
         }
