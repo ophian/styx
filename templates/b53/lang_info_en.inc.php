064acc6eb424300c18c50805d4399b51c688acb2
Ian <aristophian@googlemail.com>
2023-04-13 10:56:57 +0200

    b53: State alpha3 in description

--- templates/b53/lang_info_en.inc.php
+++ templates/b53/lang_info_en.inc.php
@@ -8,7 +8,7 @@
 $info['theme_info_summary'] = 'A "Bootstrap 5.3" based CSS HTML5 Framework Theme by Ian Styx. Based on the Styx "boot" pool theme engine.';
 
 $info['theme_info_desc'] = 'This is a basic interpretation for brand new Bootstrap 5.3, including the dark/light or auto mode implementation handler.
-Upcoming Bootstrap 5.3 is a huge improvement for the Series 5 and currently is delivered as an alpha2 state, which is near to its final.
+Upcoming Bootstrap 5.3 is a huge improvement for the Series 5 and currently is delivered as an alpha3 state, which is near to its final.
 The color handling frameset regarding native vs variable color modes, in special for the new dark/light mode implementation, actually is not finally
 finished yet and will probably last until v.6 arrives, since Bootstrap colors are limited by backwards compatibility in changing key components like
 buttons and more. It also is limited by color manipulation in vanilla CSS vs Sass.<br>
