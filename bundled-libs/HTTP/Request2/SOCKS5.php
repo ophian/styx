7ef9f5f28b2a154c038e7e268a5f386d187b1e8b
Ian <aristophian@googlemail.com>
2023-07-14 14:01:35 +0200

    Revert "bundld-libs: Add HTTP/Request2 PHP 8.2: Sensitive Parameter value redaction support" - Too early!!
    This reverts commit dd4872c0e1fc00fdc8d604f95ae612ad0a71dd66.

--- bundled-libs/HTTP/Request2/SOCKS5.php
+++ bundled-libs/HTTP/Request2/SOCKS5.php
@@ -92,7 +92,7 @@ class HTTP_Request2_SOCKS5 extends HTTP_Request2_SocketWrapper
      * @throws HTTP_Request2_MessageException
      * @link http://tools.ietf.org/html/rfc1929
      */
-    protected function performAuthentication($username, #[\SensitiveParameter] $password)
+    protected function performAuthentication($username, $password)
     {
         $request  = pack('C2', 1, strlen($username)) . $username
                     . pack('C', strlen($password)) . $password;
