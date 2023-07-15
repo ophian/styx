8cfb190065716931cb52d987716b218d4fed4182
Ian <aristophian@googlemail.com>
2023-07-15 12:22:21 +0200

    functions API: Minor consistency fix

--- include/functions.inc.php
+++ include/functions.inc.php
@@ -332,8 +332,7 @@ function serendipity_validateDate($date, $format = 'Y-m-d') {
  * @author BohwaZ <https://bohwaz.net/>
  * @see https://gist.github.com/bohwaz/42fc223031e2b2dd2585aab159a20f30
  */
-function serendipity_toDateTimeMapper(string $format, $timestamp = null, ?string $locale = null): string
-{
+function serendipity_toDateTimeMapper(string $format, $timestamp = null, ?string $locale = null): string {
 	if (null === $timestamp) {
 		$timestamp = new \DateTime;
 	}
