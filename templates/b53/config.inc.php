3ad0e5c50026d94baf88fb9232e021ad16057634
Ian <aristophian@googlemail.com>
2023-07-21 19:32:36 +0200

    b53: Disable load_language() while not in use yet

--- templates/b53/config.inc.php
+++ templates/b53/config.inc.php
@@ -1,7 +1,7 @@
 <?php
 if (IN_serendipity !== true) { die ("Don't hack!"); }
 
-@serendipity_plugin_api::load_language(dirname(__FILE__));
+//@serendipity_plugin_api::load_language(dirname(__FILE__));
 
 $serendipity['smarty']->assign(array('currpage'  => "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'],
                                      'currpage2' => $_SERVER['REQUEST_URI']));
