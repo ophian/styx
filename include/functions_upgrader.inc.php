65549b46d30bfb5f0a99eabf941f41df173d7a19
Ian <aristophian@googlemail.com>
2023-09-28 17:20:52 +0200

    upgrader API: Set update_table_authorgroups() as known to fail on CREATE UNIQUE INDEX authorgroup_idx duplicate error resumes

--- include/functions_upgrader.inc.php
+++ include/functions_upgrader.inc.php
@@ -650,7 +650,7 @@ function serendipity_fixPlugins($case) {
 function update_table_authorgroups() {
     global $serendipity;
 
-    return serendipity_db_query("CREATE UNIQUE INDEX authorgroup_idx ON {$serendipity['dbPrefix']}authorgroups (groupid, authorid)");
+    return serendipity_db_query("CREATE UNIQUE INDEX authorgroup_idx ON {$serendipity['dbPrefix']}authorgroups (groupid, authorid)", true, 'both', false, false, false, true); // set single true and last expectError true, since table is known to fail when unique key is a duplicate on error resumes for example
 }
 
 /**
