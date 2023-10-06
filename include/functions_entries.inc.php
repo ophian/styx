8f3cbdd9f1771a4d4ff816721825a48fa47d7833
Ian <aristophian@googlemail.com>
2023-10-06 14:37:55 +0200

    entries API: Prep d86d86f2a9110ec099f3d8c692e3080b77386b10 for upcoming categorytemplates v.4.2.0 plugin UPDATE
    Since we want it to happen the correct way and not hardcode it in core, even if it is a unique event only.

--- include/functions_entries.inc.php
+++ include/functions_entries.inc.php
@@ -1907,7 +1907,7 @@ function serendipity_printArchives() {
     // when the entrycat table is JOINed by the categorytemplates plugin for the purpose of "hidden" (template) categories.
     // This 'frontend_fetcharchives' hook exists singularly for the categorytemplates plugin, see https://github.com/ophian/styx/commit/6eca73434d01772a89523508b768c26c22d3fb91
     if (empty($distinct) && !empty($cond['joins']) && $cond['joins'] == "LEFT JOIN {$serendipity['dbPrefix']}entrycat AS ec ON (ec.entryid IS NULL OR ec.entryid = e.id)") {
-        $distinct = 'DISTINCT';
+        $distinct = $cond['distinct'] ?? ''; // 'DISTINCT ec.entryid,'
     }
 
     $q = "SELECT $distinct e.timestamp
