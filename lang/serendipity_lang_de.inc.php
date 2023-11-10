45c2d965a012230b8106fdba1b2894766e9b72b5
Ian <aristophian@googlemail.com>
2023-11-10 09:01:42 +0100

    lang: [de] translate 6b1ec77da2fc127a02dc1aee898653085591aaee and part of 78113cccd632f46bb275b59782c9f3da730e9972

--- lang/serendipity_lang_de.inc.php
+++ lang/serendipity_lang_de.inc.php
@@ -1176,8 +1176,8 @@
 @define('DASHBOARD_INFO_HEADER', 'Auf einen Blick');
 @define('DASHBOARD_INFO_CONTENT', 'Abkürzungen');
 @define('DASHBOARD_INFO_EMPTY', 'Wir haben nicht genug Daten, um etwas Nützliches zu zeigen. Es sind keine anhängigen Kommentare, Zukunfts- oder Entwurfseinträge vorhanden.');
-@define('DASHBOARD_SYSTEM_TICKER', 'Remote System Notification');
-@define('DASHBOARD_SYSTEM_TICKER_DESC', 'Allows hideable remote system notifications in the backend dashboard panel.');
+@define('DASHBOARD_SYSTEM_TICKER', 'Remote-Systembenachrichtigungen');
+@define('DASHBOARD_SYSTEM_TICKER_DESC', 'Erlaubt versteckbare Remote-Systembenachrichtigungen in der Backend (dashboard) Übersichtsseite.');
 @define('DASHBOARD_SYSINFO_GREETING', 'Hallo Administrator ! Die folgenden Meldungen benötigen kurz Ihre administrative Aufmerksamkeit:');
 @define('DASHBOARD_SYSINFO_HIDE_NOTE', 'Abhaken für: &raquo;&nbsp;<em>Ich habe verstanden! Zeige nie wieder!</em>&nbsp;&laquo;');
 @define('COMMENTS_PENDING', 'Zu bewilligende Kommentare');
