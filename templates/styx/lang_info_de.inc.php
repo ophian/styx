b2f3e64843f2a1327904449366f50f46b85924ac
Ian <aristophian@googlemail.com>
2023-12-09 12:08:27 +0100

    styx: Remove the example and commit change log in info_lang
    It is the Serendipity Styx default backend since long !

--- templates/styx/lang_info_de.inc.php
+++ templates/styx/lang_info_de.inc.php
@@ -5,7 +5,7 @@
  * Convert or save as ANSI (ISO-8859-1) or your native charset
  **/
 
-$info['theme_info_summary'] = 'Das Styx Standard Backend ("Beispiel") Default Theme';
+$info['theme_info_summary'] = 'Das Styx Standard Backend Theme';
 
 $info['theme_info_desc'] = '<u>ACHTUNG&colon;</u> Dieses Theme hat kein eigenes Frontend!<br>
 Dieses Standard Backend Theme verweist auf die Styx Kern Backend Templates im "default/admin" Unterverzeichnis.
@@ -14,35 +14,14 @@ Es zeigt auf einfache Weise, wie Sie damit in der Lage sind, nur diejenigen Date
 $info['theme_info_backend'] = 'Dieses Standard Backend-Theme verweist auf die Styx Kern Backend Templates im "default/admin" Unterverzeichnis.
 Als Serendipity Styx Standard Backend "Beispiel" zeigt es die Möglichkeit, ein eigenes und mehr oder weniger template-leeres Backend Theme zu erstellen das als "Fallback" die "default" Backend Template Dateien nutzt.
 Es zeigt auf einfache Weise, wie Sie damit in der Lage sind, nur diejenigen Dateien zu ändern oder hinzuzufügen, die notwendig sind, um Ihre momentanen Bedürfnisse zu erfüllen.
-Momentan enthält es eine angepasste index-Template-Datei, die außer dem neuen "Dark Mode" nur bestimmte irrelevante Informationen und Assets auf der Login-Seite entfernt, wenn Sie nicht angemeldet sind.
-Dieses Standard Backend verfügt über eine eigene styles Auszeichnung, analog zur "default/admin/styles.css" Datei, die folgende, fortlaufend aktualisierte Verbesserungen im Backend vornimmt:
-<pre>
-* 2017-08-21 - Neu hinzugekommen ist ein Bugfix für die Backend-Ansichten mit "rtl" (right-to-left), auf rechts gedrehtes Schrift-Attribut im &lt;html&gt; Element.
-* 2019-09-08 - Die IE8/9 workarounds wurden entfernt.
-* 2020-03-25 - Hervorhebung der Styx message styles.
-* 2020-06-06 - Verkleinertes font-size der entry_info styles der Eintrags Liste.
-* 2020-08-31 - Verbesserung der Login Seite und Update des fullsize preview.
-* 2020-10-31 - SVG icon für die eingesprungenen Plugin fieldset legends des Eintragsformulares.
-* 2020-12-06 - Erlaube Media Screen iPhone 5/SE mit 320px Größen für media filter HTML datetime Felder.
-* 2021-02 / 2021-03 - Kleinere Verbesserungen im den CSS styles.
-* 2021-03-26 - Entries/Comments Filter Toolbars werden ab 360px Screen Breiten nach rechts verschoben.
-* 2021-03-29 - Erweiterung mit rechts gefloateten Toolbar Gruppen für Plugins/Themes/Eigene-Einstellungen mit "all" klappbaren Konfigurations Gruppen Containern.
-* 2021-04-01 - Cleanup für media inputs in Konfigurationsseiten.
-* 2021-04-28 - Mozilla: Fix des rechts fehlenden blau-schimmer Schattens bei bestimmten Klapp-bar-Buttons
-* 2021-05-05 - Verbesserte Eingabe-/Radiofelder für die (blaue) Standard-Mozilla(89+)/Chromium-Darstellung
-* 2021-06-19 - Styx Dark Mode
-* 2021-12-06 - Erweiterung für Styx login Page Dark Mode
-* 2022-07-15 - Verbesserte media properties styles inclusive EXIF data und responsive fluids
-* 2022-08-28 - Info-Schaltflächen in Beschriftungen mit etwas Abstand zum unteren Rand versehen
-* 2022-10-17 - Verbesserter Abstand für Backend Benachrichtigungen die #serendipity_iframe voranstehen
-* 2022-12-08 - Erweiterung für Styx modemaintain login info
-</pre>
+Momentan enthält es nur eine angepasste index und eine upgrader.inc Template Datei, die, außer dem neuen "Dark Mode", nur bestimmte irrelevante Informationen und Assets auf der Login-Seite entfernen, wenn Sie nicht angemeldet sind.
+Dieses Standard Backend verfügt über eine eigene styles Auszeichnungs-Datei, über die stetig Verbesserungen im Backend vorgenommen werden.
 
-DAS FOLGENDE IST DIE BESCHREIBUNG DES FALLBACK BACKENDS&colon;<br>
+<p>DAS FOLGENDE IST DIE BESCHREIBUNG DES FALLBACK BACKENDS&colon;<br>
 Dieses Theme beherbergt die Styx-Kern Backend Templates im Unterverzeichnis "default/admin".
 Die Template-Dateien in diesem Verzeichnis bilden und erstellen das Aussehen der kompletten Admin-Oberfläche.
 Sie enthalten auch einige Workflow- und Logikfunktionen sowie eigene Javascript-Bibliotheken.
 Wenn Sie ein eigenes Backend-Theme verwenden möchten, kopieren Sie das Verzeichnis "admin" in ihr Theme.
 Ändern Sie dort die Datei "info.txt", um eine Zeile "Backend&colon; Yes" hinzuzufügen, und wählen Sie das neue Backend-Theme in der neu geladenen Themenliste aus.
 Ab sofort können Sie die Dateien und Stile des eigenen Backend-Themes bearbeiten und an Ihre erweiterten Bedürfnisse anpassen.<br>
-<u><b>Bitte beachten Sie:</b></u> Dies ist nur für erfahrene Benutzer empfehlenswert und komplett abseits der Update-Funktionen!';
+<u><b>Bitte beachten Sie:</b></u> Dies ist nur für erfahrene Benutzer empfehlenswert und komplett abseits der Update-Funktionen!</p>';
--- templates/styx/lang_info_en.inc.php
+++ templates/styx/lang_info_en.inc.php
@@ -5,7 +5,7 @@
  * Convert or save as ANSI (ISO-8859-1) or your native charset, analogue to the non-UTF-8 lang constant files.
  **/
 
-$info['theme_info_summary'] = 'The Styx Standard Backend ("example") default Theme';
+$info['theme_info_summary'] = 'The Styx Standard Backend Theme';
 
 $info['theme_info_desc'] = '<u>Please Note&colon;</u> This theme has no existing frontend!<br>
 This Standard Backend-Theme relegates to the Styx core backend templates in the "default/admin" subdirectory.
@@ -14,34 +14,13 @@ As the Styx Standard Backend Theme it proves the ability that you can simply add
 $info['theme_info_backend'] = 'This Standard Backend-Theme relegates to the Styx core backend templates in the "default/default" subdirectory.
 As the Serendipity Styx Standard Backend "example" it proves the ability that you can simply add a more or less template-empty backend theme, which uses the fallback to the "default" backend template files.
 With this in mind you are able to just change and add those files only, which are necessary to fill your needs.
-For the moment it solely includes an index template file, which removes irrelevant informations and assets when not logged in, beside adding the new "Dark Mode".
-This Standard Backend has its own style declarations, analogous to the "default/admin/styles.css" file, which make the following continuously evolving enhancements to the backend:
-<pre>
-* 2017-08-21 - Newly fixes a UI breaking issue with the rtl (right-to-left) direction attribute in the &lt;html&gt; element.
-* 2019-09-08 - Removed the IE8/9 workarounds.
-* 2020-03-25 - Sign out Styx message styles with a "marked" left border.
-* 2020-06-06 - Smaller font-size of entry_info styles of entries list.
-* 2020-08-31 - Improved login page and update fullsize preview.
-* 2020-10-31 - Add SVG icon to the plugin added fieldset legends of entryform.
-* 2020-12-06 - Allow iPhone 5/SE with 320px sizes for media filter datetime pick-fields.
-* 2021-02 / 2021-03 - Small Improvements to the CSS styles.
-* 2021-03-26 - Entries/Comments filter toolbars are shifted to the right up from 360px screen width.
-* 2021-03-29 - Extended with a right floated toolbar group for Plugins/Themes/Personal-Preferences configurations, including at least a "toggle all" (containers) button.
-* 2021-04-01 - Cleanup for media inputs in configuration pages.
-* 2021-04-28 - Improved length of certain toggle bars for the blue shimmering shadow
-* 2021-05-05 - Improved input / radio field styles for ugly Mozilla(89+)/Chromium default (blue) outsign
-* 2021-06-19 - Styx Dark Mode
-* 2021-12-06 - Add Styx login Page Dark Mode
-* 2022-07-15 - Improve media properties styles including EXIF data and for responsive fluids
-* 2022-08-28 - Add a tad bottom margin space to info buttons in labels
-* 2022-10-17 - Improved margin for backend messages followed by #serendipity_iframe
-* 2022-12-08 - Improved margins for Styx modemaintain login info
-</pre>
+For the moment it solely includes an index template and upgrader.inc file, which remove irrelevant informations and assets when not logged in, beside adding the new "Dark Mode".
+This Standard Backend has its own style declarations which make various continuously evolving enhancements to the backend.
 
-THE FOLLOWING IS THE DESCRIPTION OF THE RELEGATED BACKEND&colon;<br>
+<p>THE FOLLOWING IS THE DESCRIPTION OF THE RELEGATED BACKEND&colon;<br>
 The template files in that directory build and create the look of the complete admin interface.
 They also contain some workflow and logic, as well as the javascript libraries.
 If you want to use your own backend theme, copy the "admin" directory to a theme.
 In there, change the "info.txt" file to add a "Backend&colon; Yes" line and select the new backend theme in the newly loaded theme list.
 From now on, you can edit the files and styles of own backend theme and adapt it to your extending needs.<br>
-<u><b>Please Note&colon;</b></u> This only is recommended for highly experienced users and completely off the update track!';
+<u><b>Please Note&colon;</b></u> This only is recommended for highly experienced users and completely off the update track!</p>';
