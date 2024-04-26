ophian/styx

commit c5acd1a976de6632934e2e8103ec9e9efe66813b

core: Return to htmlspecialchars()

include/admin/entries.inc.php
@@ -201,7 +201,7 @@ switch($serendipity['GET']['adminAction']) {
                   'serendipity[action]'      => 'admin',
                   'serendipity[adminModule]' => 'entries',
                   'serendipity[adminAction]' => 'save',
-                  'serendipity[timestamp]'   => serendipity_specialchars($entry['timestamp'])
+                  'serendipity[timestamp]'   => htmlspecialchars($entry['timestamp'])
                 ),
                 $entry,
                 $errors
@@ -218,7 +218,7 @@ switch($serendipity['GET']['adminAction']) {
         serendipity_deleteEntry((int)$serendipity['GET']['id']);
         $data['switched_output'] = true;
         $data['is_doDelete']     = true;
-        $data['del_entry']       = sprintf(RIP_ENTRY, $entry['id'] . ' - ' . serendipity_specialchars($entry['title']));
+        $data['del_entry']       = sprintf(RIP_ENTRY, $entry['id'] . ' - ' . htmlspecialchars($entry['title']));
         // no break [PSR-2] - extends editSelect
 
     case 'doMultiDelete':
@@ -236,7 +236,7 @@ switch($serendipity['GET']['adminAction']) {
                     $entry = serendipity_fetchEntry('id', $id, true, 1);
                     serendipity_deleteEntry($id);
                     $data['is_doMultiDelete'] = true;
-                    $data['del_entry'][]      = sprintf(RIP_ENTRY, $entry['id'] . ' - ' . serendipity_specialchars($entry['title']));
+                    $data['del_entry'][]      = sprintf(RIP_ENTRY, $entry['id'] . ' - ' . htmlspecialchars($entry['title']));
                 }
             }
         }
@@ -278,22 +278,22 @@ switch($serendipity['GET']['adminAction']) {
             if (isset($serendipity['GET']['filter'])) {
                 // Don't check against Cookies when requested per categories list selected entries, or by overviews draft shortcut link, particularly for the author ID
                 if (!isset($serendipity['GET']['catref']) && isset($serendipity['COOKIE']['entrylist_filter_' . $f_import]) && !isset($serendipity['GET']['filter'][$f_import])) {
-                    $serendipity['COOKIE']['entrylist_filter_' . $f_import] = serendipity_specialchars(strip_tags($serendipity['COOKIE']['entrylist_filter_' . $f_import]));
+                    $serendipity['COOKIE']['entrylist_filter_' . $f_import] = htmlspecialchars(strip_tags($serendipity['COOKIE']['entrylist_filter_' . $f_import]));
                     $serendipity['GET']['filter'][$f_import] =& $serendipity['COOKIE']['entrylist_filter_' . $f_import];
                 }
                 $serendipity['GET']['filter'][$f_import] = $serendipity['GET']['filter'][$f_import] ?? '';
-                $data["get_filter_$f_import"] = serendipity_specialchars(strip_tags($serendipity['GET']['filter'][$f_import]));
+                $data["get_filter_$f_import"] = htmlspecialchars(strip_tags($serendipity['GET']['filter'][$f_import]));
             }
         }
 
         foreach($sort_import AS $s_import) {
             if (isset($serendipity['GET']['sort'])) {
                 if (isset($serendipity['COOKIE']['entrylist_sort_' . $s_import]) && !isset($serendipity['GET']['sort'][$s_import])) {
-                    $serendipity['COOKIE']['entrylist_sort_' . $s_import] = serendipity_specialchars(strip_tags($serendipity['COOKIE']['entrylist_sort_' . $s_import]));
+                    $serendipity['COOKIE']['entrylist_sort_' . $s_import] = htmlspecialchars(strip_tags($serendipity['COOKIE']['entrylist_sort_' . $s_import]));
                     $serendipity['GET']['sort'][$s_import] =& $serendipity['COOKIE']['entrylist_sort_' . $s_import];
                 }
                 $serendipity['GET']['sort'][$s_import] = $serendipity['GET']['sort'][$s_import] ?? '';
-                $data["get_sort_$s_import"] = serendipity_specialchars(strip_tags($serendipity['GET']['sort'][$s_import]));
+                $data["get_sort_$s_import"] = htmlspecialchars(strip_tags($serendipity['GET']['sort'][$s_import]));
             }
         }
 
@@ -424,11 +424,11 @@ switch($serendipity['GET']['adminAction']) {
             $qString = '?serendipity[adminModule]=entries&amp;serendipity[adminAction]=editSelect';
             foreach((array)$serendipity['GET']['sort'] AS $k => $v) {
                 if (is_null($v)) continue;
-                $qString .= '&amp;serendipity[sort]['. serendipity_specialchars(strip_tags($k)) .']='. serendipity_specialchars(strip_tags($v));
+                $qString .= '&amp;serendipity[sort]['. htmlspecialchars(strip_tags($k)) .']='. htmlspecialchars(strip_tags($v));
             }
             if (isset($serendipity['GET']['filter'])) {
                 foreach((array)$serendipity['GET']['filter'] AS $k => $v) {
-                    $qString .= '&amp;serendipity[filter]['. serendipity_specialchars(strip_tags($k)) .']='. serendipity_specialchars(strip_tags($v));
+                    $qString .= '&amp;serendipity[filter]['. htmlspecialchars(strip_tags($k)) .']='. htmlspecialchars(strip_tags($v));
                 }
             }
             $data['linkFirst']    = $qString . '&amp;serendipity[page]=' . 0;
@@ -471,14 +471,14 @@ switch($serendipity['GET']['adminAction']) {
 
                 $smartentry = array(
                     'id'            => $ey['id'],
-                    'title'         => serendipity_specialchars($ey['title']),
+                    'title'         => htmlspecialchars($ey['title']),
                     'timestamp'     => (int)$ey['timestamp'],
                     'last_modified' => (int)$ey['last_modified'],
                     'isdraft'       => serendipity_db_bool($ey['isdraft']),
                     'ep_is_sticky'  => (isset($ey['properties']['ep_is_sticky']) && serendipity_db_bool($ey['properties']['ep_is_sticky']) ? true : false),
                     'ep_is_locked'  => !empty($ey['properties']['ep_entrypassword']) ? true : false,
                     'pubdate'       => date('c', (int)$ey['timestamp']),
-                    'author'        => serendipity_specialchars($ey['author']),
+                    'author'        => htmlspecialchars($ey['author']),
                     'cats'          => $entry_cats,
                     'preview'       => ((serendipity_db_bool($ey['isdraft']) || (!$serendipity['showFutureEntries'] && $ey['timestamp'] >= serendipity_serverOffsetHour())) ? true : false),
                     'archive_link'  => serendipity_archiveURL($ey['id'], $ey['title'], 'serendipityHTTPPath', true, array('timestamp' => $ey['timestamp'])),
@@ -514,7 +514,7 @@ switch($serendipity['GET']['adminAction']) {
         $data['is_delete']       = true;
         $data['newLoc']          = $newLoc;
         // for Smarty, printf had to turn into sprintf!!
-        $data['rip_entry']       = sprintf(DELETE_SURE, $entry['id'] . ' - ' . serendipity_specialchars($entry['title']));
+        $data['rip_entry']       = sprintf(DELETE_SURE, $entry['id'] . ' - ' . htmlspecialchars($entry['title']));
         break;
 
     case 'multidelete':
@@ -522,7 +522,7 @@ switch($serendipity['GET']['adminAction']) {
             return; // blank content page, but default token check parameter is presenting a XSRF message when false
         }
         if (!isset($serendipity['POST']['multiDelete']) || !is_array($serendipity['POST']['multiDelete'])) {
-            echo '<div class="msg_notice"><span class="icon-attention-circled" aria-hidden="true"></span> ' . sprintf(MULTICHECK_NO_ITEM, serendipity_specialchars(($_SERVER['HTTP_REFERER'] ?? ''), ENT_QUOTES | ENT_HTML401)) . '</div>'."\n";
+            echo '<div class="msg_notice"><span class="icon-attention-circled" aria-hidden="true"></span> ' . sprintf(MULTICHECK_NO_ITEM, htmlspecialchars(($_SERVER['HTTP_REFERER'] ?? ''), ENT_QUOTES | ENT_HTML401)) . '</div>'."\n";
             break;
         }
 
@@ -532,7 +532,7 @@ switch($serendipity['GET']['adminAction']) {
             $ids .= (int)$id . ',';
             $entry = serendipity_fetchEntry('id', $id, true, 1);
             $data['is_multidelete'] = true;
-            $data['rip_entry'][]    = sprintf(DELETE_SURE, $entry['id'] . ' - ' . serendipity_specialchars($entry['title']));
+            $data['rip_entry'][]    = sprintf(DELETE_SURE, $entry['id'] . ' - ' . htmlspecialchars($entry['title']));
         }
         $newLoc = '?' . serendipity_setFormToken('url') . '&amp;serendipity[action]=admin&amp;serendipity[adminModule]=entries&amp;serendipity[adminAction]=doMultiDelete&amp;serendipity[id]=' . $ids;
         $data['switched_output'] = true;

comment.php
@@ -147,7 +147,7 @@ if ($type == 'trackback') {
         $serendipity['smarty']->assign(
             array(
                 'is_comment_added'   => true,
-                'comment_url'        => serendipity_specialchars($_GET['url']) . '&amp;serendipity[entry_id]=' . $id,
+                'comment_url'        => htmlspecialchars($_GET['url']) . '&amp;serendipity[entry_id]=' . $id,
                 'comment_string'     => explode('%s', COMMENT_ADDED_CLICK)
             )
         );
@@ -218,7 +218,7 @@ if ($type == 'trackback') {
                 $serendipity['smarty']->assign(
                     array(
                         'is_comment_notadded' => true,
-                        'comment_url'         => serendipity_specialchars($_SERVER['HTTP_REFERER']),
+                        'comment_url'         => htmlspecialchars($_SERVER['HTTP_REFERER']),
                         'comment_string'      => explode('%s', COMMENT_NOT_ADDED_CLICK)
                     )
                 );
@@ -227,7 +227,7 @@ if ($type == 'trackback') {
             $serendipity['smarty']->assign(
                 array(
                     'is_comment_empty' => true,
-                    'comment_url'      => serendipity_specialchars($_SERVER['HTTP_REFERER']),
+                    'comment_url'      => htmlspecialchars($_SERVER['HTTP_REFERER']),
                     'comment_string'   => explode('%s', EMPTY_COMMENT)
                 )
             );

include/admin/category.inc.php
@@ -69,7 +69,7 @@ if (isset($_POST['SAVE']) && serendipity_checkFormToken()) {
             if (is_array($r)) {
                 $r = serendipity_db_query("SELECT category_name FROM {$serendipity['dbPrefix']}category
                                             WHERE categoryid = ". (int)$parentid);
-                $data['subcat'] = sprintf(ALREADY_SUBCATEGORY, serendipity_specialchars($r[0]['category_name']), serendipity_specialchars($name));
+                $data['subcat'] = sprintf(ALREADY_SUBCATEGORY, htmlspecialchars($r[0]['category_name']), htmlspecialchars($name));
             } else {
                 $_sort_order = $serendipity['POST']['cat']['sort_order'] ?? 0;
                 $_hide_sub   = $serendipity['POST']['cat']['hide_sub']   ?? 0;
@@ -236,7 +236,7 @@ if ($serendipity['GET']['adminAction'] == 'view') {
                                     : (
                                         (serendipity_checkPermission('adminEntriesMaintainOthers') && serendipity_checkPermission('adminCategoriesMaintainOthers'))
                                         ? GROUP . ': <span class="icon-users chief" title="' . USERLEVEL_CHIEF_DESC . '" aria-hidden="true"></span> +'
-                                        : AUTHOR . ': ' .serendipity_specialchars($serendipity['serendipityUser'])
+                                        : AUTHOR . ': ' .htmlspecialchars($serendipity['serendipityUser'])
                                     );
     }
 }

include/admin/comments.inc.php
@@ -289,7 +289,7 @@ $and = $searchString = ''; // init default
 /* Compress the filters into an "AND" SQL query, and a querystring */
 foreach($filters AS $filter) {
     $and          .= (!empty($serendipity['GET']['filter'][$filter]) ? "AND c.". $filter ." LIKE '%". serendipity_db_escape_string($serendipity['GET']['filter'][$filter]) ."%'" : "");
-    $searchString .= (!empty($serendipity['GET']['filter'][$filter]) ? "&amp;serendipity[filter][". $filter ."]=". serendipity_specialchars($serendipity['GET']['filter'][$filter]) : "");
+    $searchString .= (!empty($serendipity['GET']['filter'][$filter]) ? "&amp;serendipity[filter][". $filter ."]=". htmlspecialchars($serendipity['GET']['filter'][$filter]) : "");
 }
 
 // init default

include/admin/images.inc.php
@@ -163,7 +163,7 @@ switch ($serendipity['GET']['adminAction']) {
                 $image = serendipity_fetchImageFromDatabase($id);
                 $messages[] = serendipity_deleteImage($id);
                 if (isset($image['id'])) {
-                    $messages[] = sprintf('<span class="msg_notice"><span class="icon-info-circled" aria-hidden="true"></span> ' . RIP_ENTRY . "</span>\n", $image['id'] . ' - ' . serendipity_specialchars($image['realname']));
+                    $messages[] = sprintf('<span class="msg_notice"><span class="icon-info-circled" aria-hidden="true"></span> ' . RIP_ENTRY . "</span>\n", $image['id'] . ' - ' . htmlspecialchars($image['realname']));
                 }
             }
         }
@@ -196,7 +196,7 @@ switch ($serendipity['GET']['adminAction']) {
             return; // blank content page, but default token check parameter is presenting a XSRF message when false
         }
         if (!isset($serendipity['POST']['multiSelect']) || (empty($serendipity['POST']['multiSelect']) || !is_array($serendipity['POST']['multiSelect'])) && isset($_POST['gallery_insert'])) {
-            echo '<div class="msg_notice"><span class="icon-attention-circled" aria-hidden="true"></span> ' . sprintf(MULTICHECK_NO_ITEM, serendipity_specialchars(($_SERVER['HTTP_REFERER'] ?? ''), ENT_QUOTES | ENT_HTML401)) . '</div>'."\n";
+            echo '<div class="msg_notice"><span class="icon-attention-circled" aria-hidden="true"></span> ' . sprintf(MULTICHECK_NO_ITEM, htmlspecialchars(($_SERVER['HTTP_REFERER'] ?? ''), ENT_QUOTES | ENT_HTML401)) . '</div>'."\n";
             return; // blank content page exit
         }
         $_multiSelectImages = $serendipity['POST']['multiSelect'];
@@ -208,9 +208,9 @@ switch ($serendipity['GET']['adminAction']) {
             #if (is_object($serendipity['logger'])) { $serendipity['logger']->debug("ML_SELECTMULTIGALLERYITEMS: Images MediaProperties: " . print_r($file['props'],true)); }
             serendipity_plugin_api::hook_event('media_getproperties_cached', $file['props']['base_metadata'], $file['realfile']);
             // serendipity_fetchMediaProperties() returns either with subgroup key properties or without, by case having saved the media items properties page before. Do not trust viewed items, since they may be copy-placed EXIF data of the image itself not having been saved.
-            $file['prop_imagecomment'] = serendipity_specialchars(($file['props']['base_property']['ALL']['COMMENT1'] ?? ($file['props']['base_property']['COMMENT1'] ?? '')));
-            $file['prop_alt']          = serendipity_specialchars(($file['props']['base_property']['ALL']['ALT']      ?? ($file['props']['base_property']['ALT']      ?? '')));
-            $file['prop_title']        = serendipity_specialchars(($file['props']['base_property']['ALL']['TITLE']    ?? ($file['props']['base_property']['TITLE']    ?? '')));
+            $file['prop_imagecomment'] = htmlspecialchars(($file['props']['base_property']['ALL']['COMMENT1'] ?? ($file['props']['base_property']['COMMENT1'] ?? '')));
+            $file['prop_alt']          = htmlspecialchars(($file['props']['base_property']['ALL']['ALT']      ?? ($file['props']['base_property']['ALT']      ?? '')));
+            $file['prop_title']        = htmlspecialchars(($file['props']['base_property']['ALL']['TITLE']    ?? ($file['props']['base_property']['TITLE']    ?? '')));
             unset($file['props']); // we don't need this bloat, except the three above
             unset($file['thumb_header']); // img (encoded) header data will make json_encode() fail and return nothing
             unset($file['header']);
@@ -242,11 +242,11 @@ switch ($serendipity['GET']['adminAction']) {
             return; // blank content page, but default token check parameter is presenting a XSRF message when false
         }
         if (!isset($serendipity['POST']['multiCheck']) || (!is_array($serendipity['POST']['multiCheck']) && (isset($_POST['toggle_move']) || isset($_POST['toggle_delete'])))) {
-            echo '<div class="msg_notice"><span class="icon-attention-circled" aria-hidden="true"></span> ' . sprintf(MULTICHECK_NO_ITEM, serendipity_specialchars(($_SERVER['HTTP_REFERER'] ?? ''), ENT_QUOTES | ENT_HTML401)) . '</div>'."\n";
+            echo '<div class="msg_notice"><span class="icon-attention-circled" aria-hidden="true"></span> ' . sprintf(MULTICHECK_NO_ITEM, htmlspecialchars(($_SERVER['HTTP_REFERER'] ?? ''), ENT_QUOTES | ENT_HTML401)) . '</div>'."\n";
             return; // blank content page exit
         }
         if (is_array($serendipity['POST']['multiCheck']) && isset($serendipity['POST']['oldDir']) && empty($serendipity['POST']['newDir']) && isset($_POST['toggle_move'])) {
-            echo '<div class="msg_notice"><span class="icon-attention-circled" aria-hidden="true"></span> ' . sprintf(MULTICHECK_NO_DIR, serendipity_specialchars(($_SERVER['HTTP_REFERER'] ?? ''), ENT_QUOTES | ENT_HTML401)) . '</div>'."\n";
+            echo '<div class="msg_notice"><span class="icon-attention-circled" aria-hidden="true"></span> ' . sprintf(MULTICHECK_NO_DIR, htmlspecialchars(($_SERVER['HTTP_REFERER'] ?? ''), ENT_QUOTES | ENT_HTML401)) . '</div>'."\n";
             return; // blank content page exit
         }
         // case bulk multimove (leave the faked oldDir being send as an empty dir string)
@@ -256,7 +256,7 @@ switch ($serendipity['GET']['adminAction']) {
             unset($serendipity['POST']['multiCheck']); // since used later for delete
 
             // oldDir is relative to Uploads/, since we can not specify a directory of a ML bulk move directly
-            $nDir = serendipity_specialchars((string)str_replace('//', '/', $serendipity['POST']['newDir'])); // relative to Uploads/
+            $nDir = htmlspecialchars((string)str_replace('//', '/', $serendipity['POST']['newDir'])); // relative to Uploads/
             // set and check for a given trailing slash! (see media directory renames)
             $nDir = (!empty($nDir) && $nDir != '/') ? rtrim($nDir, '/') . '/' : $nDir;
             // $nDir "set empty" check for the fake-named "uploadRoot" directory is done via functions_images.inc, since we need it for comparison checks before conversion
@@ -293,7 +293,7 @@ switch ($serendipity['GET']['adminAction']) {
         foreach($serendipity['POST']['multiCheck'] AS $idx => $id) {
             $ids .= (int)$id . ',';
             $image = serendipity_fetchImageFromDatabase($id);
-            $data['rip_image'][] = sprintf(DELETE_SURE, $image['id'] . ' - ' . serendipity_specialchars($image['realname']));
+            $data['rip_image'][] = sprintf(DELETE_SURE, $image['id'] . ' - ' . htmlspecialchars($image['realname']));
         }
         if (!isset($serendipity['adminFile'])) {
             $serendipity['adminFile'] = 'serendipity_admin.php';
@@ -374,7 +374,7 @@ switch ($serendipity['GET']['adminAction']) {
     // add single image variations per image
     case 'variations':
         if (serendipity_generateVariations($serendipity['GET']['fid'])) {
-            $data['messages'] = '<span class="msg_success"><span class="icon-ok-circled" aria-hidden="true"></span> ' . SYNC_OPTION_BUILDVARIATIONS . ' (#'.serendipity_specialchars($serendipity['GET']['fid']) . '). ' . DONE.'! ' . "</span>\n";
+            $data['messages'] = '<span class="msg_success"><span class="icon-ok-circled" aria-hidden="true"></span> ' . SYNC_OPTION_BUILDVARIATIONS . ' (#'.htmlspecialchars($serendipity['GET']['fid']) . '). ' . DONE.'! ' . "</span>\n";
         } else {
             $data['messages'] = '<div class="msg_notice"><span class="icon-attention-circled" aria-hidden="true"></span> Generating additional variations by this image not possible or available!' . "</span>\n";
         }
@@ -398,7 +398,7 @@ switch ($serendipity['GET']['adminAction']) {
         $messages   = array();
         $authorid   = 0; // Only use access-control based on media directories, not images themselves
         $messages[] = '<span class="msg_notice"><span class="icon-info-circled" aria-hidden="true"></span> ' . ADDING_IMAGE . "</span>\n";
-        $_imageurl  = serendipity_specialchars(($serendipity['POST']['imageurl'] ?? ''));
+        $_imageurl  = htmlspecialchars(($serendipity['POST']['imageurl'] ?? ''));
 
         // First find out whether to fetch a download hotlink or accept an upload file
         $pattern = '~^(?:ht|f)tps?://[a-z0-9.-_\/](?:(?!.{3}+\?|#|\+).)+\.(?:jpe?g|png|gif)~Ui'; // each protocol, a negative look behind to not match malicious URIs and the 4 most common img extensions
@@ -565,7 +565,7 @@ switch ($serendipity['GET']['adminAction']) {
                     $tfile = str_replace(' ', '_', basename($tfile)); // keep serendipity_uploadSecure(URL) whitespace convert behaviour, when using serendipity_makeFilename()
                     $lastd = strrpos($tfile, '.');
                     $tfile = str_replace('.', '-', substr($tfile, 0, $lastd)) . substr($tfile, $lastd); // Replace possible filename dots by a hyphen
-                    $tfile = serendipity_specialchars($tfile); // needed to prevent ability for uploader to inject javascript https://github.com/s9y/Serendipity/commit/f295a3b123bd7840ae65ccb2050ee93e5fbbcd93#diff-96c5729a7a3cb8af240c8d9fee9f023fR
+                    $tfile = htmlspecialchars($tfile); // needed to prevent ability for uploader to inject javascript https://github.com/s9y/Serendipity/commit/f295a3b123bd7840ae65ccb2050ee93e5fbbcd93#diff-96c5729a7a3cb8af240c8d9fee9f023fR
                     $tfile = serendipity_uploadSecure(serendipity_makeFilename($tfile));
 
                     if (serendipity_isActiveFile($tfile)) {
@@ -624,7 +624,7 @@ switch ($serendipity['GET']['adminAction']) {
 
                     // Accept file
                     if (is_uploaded_file($uploadtmp) && serendipity_checkMediaSize($uploadtmp) && move_uploaded_file($uploadtmp, $target)) {
-                        $uploadfile = serendipity_specialchars($uploadfile); // @see $tfile L 439 - and we DO WANT the ORIGIN NAME here!
+                        $uploadfile = htmlspecialchars($uploadfile); // @see $tfile L 439 - and we DO WANT the ORIGIN NAME here!
                         $messages[] = sprintf('<span class="msg_success"><span class="icon-ok-circled" aria-hidden="true"></span> ' . FILE_UPLOADED . "</span>\n", "<b>$uploadfile</b>", "<em class=\"media_msg t\">$target</em>");
                         @umask(0000);
                         @chmod($target, 0664);
@@ -776,7 +776,7 @@ switch ($serendipity['GET']['adminAction']) {
         $data['groups']       = $groups;
         $data['use_dir']      = $use_dir;
         $data['formtoken']    = serendipity_setFormToken();
-        $data['dir']          = serendipity_specialchars($serendipity['GET']['dir']);
+        $data['dir']          = htmlspecialchars($serendipity['GET']['dir']);
         $data['rgroups']      = (isset($read_groups[0]) ? true : false);
         $data['wgroups']      = (isset($write_groups[0]) ? true : false);
         $data['read_groups']  = $read_groups;
@@ -788,9 +788,9 @@ switch ($serendipity['GET']['adminAction']) {
             return;
         }
         $data['case_directoryDelete'] = true;
-        $data['dir']          = serendipity_specialchars($serendipity['GET']['dir']);
+        $data['dir']          = htmlspecialchars($serendipity['GET']['dir']);
         $data['formtoken']    = serendipity_setFormToken();
-        $data['basename_dir'] = basename(serendipity_specialchars($serendipity['GET']['dir']));
+        $data['basename_dir'] = basename(htmlspecialchars($serendipity['GET']['dir']));
         break;
 
     case 'directoryDoCreate':
@@ -808,7 +808,7 @@ switch ($serendipity['GET']['adminAction']) {
 
         /* TODO: check if directory already exist */
         if (is_dir($nd) || @mkdir($nd)) {
-            $data['print_DIRECTORY_CREATED'] = sprintf(DIRECTORY_CREATED, serendipity_specialchars($new_dir));
+            $data['print_DIRECTORY_CREATED'] = sprintf(DIRECTORY_CREATED, htmlspecialchars($new_dir));
             @umask(0000);
             @chmod($nd, 0777);
             serendipity_cleanCache();
@@ -901,7 +901,7 @@ switch ($serendipity['GET']['adminAction']) {
         if (isset($image_selector_addvars) && is_array($image_selector_addvars)) {
             // These variables may come from serendipity_admin_image_selector.php to show embedded upload form
             foreach($image_selector_addvars AS $imgsel_key => $imgsel_val) {
-                $form_hidden .= '        <input type="hidden" name="serendipity[' . serendipity_specialchars($imgsel_key) . ']" value="' . serendipity_specialchars($imgsel_val) . '">' . "\n";
+                $form_hidden .= '        <input type="hidden" name="serendipity[' . htmlspecialchars($imgsel_key) . ']" value="' . htmlspecialchars($imgsel_val) . '">' . "\n";
             }
         }
 
@@ -932,7 +932,7 @@ switch ($serendipity['GET']['adminAction']) {
         }
 
         if (empty($serendipity['adminFile_redirect'])) {
-            $serendipity['adminFile_redirect'] = serendipity_specialchars($_SERVER['HTTP_REFERER']);
+            $serendipity['adminFile_redirect'] = htmlspecialchars($_SERVER['HTTP_REFERER']);
         }
 
         $data['case_rotateCW'] = true;
@@ -951,7 +951,7 @@ switch ($serendipity['GET']['adminAction']) {
         }
 
         if (empty($serendipity['adminFile_redirect'])) {
-            $serendipity['adminFile_redirect'] = serendipity_specialchars($_SERVER['HTTP_REFERER']);
+            $serendipity['adminFile_redirect'] = htmlspecialchars($_SERVER['HTTP_REFERER']);
         }
 
         $data['case_rotateCCW'] = true;
@@ -1047,7 +1047,7 @@ switch ($serendipity['GET']['adminAction']) {
         serendipity_plugin_api::hook_event('media_getproperties_cached', $media['file']['props']['base_metadata'], $media['file']['realfile']);
 
         if ($file['is_image']) {
-            $file['finishJSFunction'] = $file['origfinishJSFunction'] = 'serendipity.serendipity_imageSelector_done(\'' . serendipity_specialchars($serendipity['GET']['textarea']) . '\')';
+            $file['finishJSFunction'] = $file['origfinishJSFunction'] = 'serendipity.serendipity_imageSelector_done(\'' . htmlspecialchars($serendipity['GET']['textarea']) . '\')';
 
             if (!empty($serendipity['GET']['filename_only']) && $serendipity['GET']['filename_only'] !== 'true') {
                 $file['fast_select'] = true;

include/admin/importers/b2evolution.inc.php
@@ -101,7 +101,7 @@ class Serendipity_Import_b2evolution extends Serendipity_Import
         }
 
         if (!$b2db || mysqli_connect_error()) {
-            return sprintf(COULDNT_CONNECT, serendipity_specialchars($this->data['host']));
+            return sprintf(COULDNT_CONNECT, htmlspecialchars($this->data['host']));
         }
 
         if (!@mysqli_select_db($b2db, $this->data['name'])) {

include/admin/importers/geeklog.inc.php
@@ -103,7 +103,7 @@ class Serendipity_Import_geeklog extends Serendipity_Import
         }
 
         if (!$gdb || mysqli_connect_error()) {
-            return sprintf(COULDNT_CONNECT, serendipity_specialchars($this->data['host']));
+            return sprintf(COULDNT_CONNECT, htmlspecialchars($this->data['host']));
         }
 
         if (!@mysqli_select_db($gdb, $this->data['name'])) {

commit 5eee4efc94e4e2c3b6341107133a916a07acbdd7

entries: Fix/Improve type regression error from c5acd1a976de6632934e2e8103ec9e9efe66813b

include/admin/entries.inc.php
@@ -201,7 +201,7 @@ switch($serendipity['GET']['adminAction']) {
                   'serendipity[action]'      => 'admin',
                   'serendipity[adminModule]' => 'entries',
                   'serendipity[adminAction]' => 'save',
-                  'serendipity[timestamp]'   => htmlspecialchars($entry['timestamp'])
+                  'serendipity[timestamp]'   => htmlspecialchars((string) $entry['timestamp'])
                 ),
                 $entry,
                 $errors