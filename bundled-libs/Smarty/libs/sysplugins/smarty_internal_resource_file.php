51fcb1dc76428415e23c65a46afe7efee67a09b0
Ian <aristophian@googlemail.com>
2024-08-21 11:25:45 +0200

    smarty: Extend b8818ea0007b735012a124473b8faeada2392f21

--- bundled-libs/Smarty/libs/sysplugins/smarty_internal_resource_file.php
+++ bundled-libs/Smarty/libs/sysplugins/smarty_internal_resource_file.php
@@ -25,7 +25,7 @@ class Smarty_Internal_Resource_File extends Smarty_Resource
      *
      * @throws \SmartyException
      */
-    public function populate(Smarty_Template_Source $source, Smarty_Internal_Template $_template = null)
+    public function populate(Smarty_Template_Source $source, ?Smarty_Internal_Template $_template = null)
     {
         $source->filepath = $this->buildFilepath($source, $_template);
         if ($source->filepath !== false) {
