df52fd37ea098fdf2467645dc9a98b4aab576f72
Ian <aristophian@googlemail.com>
2024-08-28 12:13:46 +0200

    serendipity_styx JS: Some star dust to 730fe4ca5af8d2fd3a62930c9fd7e7f76202f4c7 noting

--- templates/default/admin/serendipity_styx.js.php
+++ templates/default/admin/serendipity_styx.js.php
@@ -516,7 +516,8 @@
             //console.log('nolink img = '+img); // if not inside a container of what ever "p, div, span..." the picture/source element is magically removed by CKEDITOR when landing in your textarea
         }
 
-        // see ID renaming and so to keep the POST name="serendipity[body]", but set element ID to id="serendipity_textarea_body" for further POST/GET data processing
+        // see ID renaming and so to keep the POST name="serendipity[body]",
+        // but set the textarea var parameter to the element ID id="serendipity_textarea_body" for further POST/GET data processing
         textarea = textarea === 'serendipity[body]' ? 'serendipity_textarea_body' : textarea;
         textarea = textarea === 'serendipity[extended]' ? 'serendipity_textarea_extended' : textarea; // ditto for second
 
