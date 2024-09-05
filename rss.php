e89f9e862dce557d350caac3611faa3c003f1d9e
Ian <aristophian@googlemail.com>
2024-09-05 12:33:21 +0200

    rss: Remove atom-1.0 cleanup loop for bca2060c22f7295077550c794027b1dd7a6acb42

--- rss.php
+++ rss.php
@@ -329,11 +329,6 @@ switch($version) {
         // For people wanting extra RFC compliance
         // header('Content-Type: application/atom+xml; charset=utf-8');
         $namespace_hook = 'frontend_display:atom-1.0:namespace';
-        // help out for HTML5 w/o /> to XML to /> in special for picture container source tags
-        foreach ($entries AS &$entry) {
-            $entry['feed_body'] = preg_replace('/\<(img|source|br|hr|(?!colgroup)col)([^>]*)(?<!\/)>/', '<\1\2\3/>', str_replace('&amp;shy;', '', $entry['feed_body']));
-            $entry['feed_ext']  = preg_replace('/\<(img|source|br|hr|(?!colgroup)col)([^>]*)(?<!\/)>/', '<\1\2\3/>', str_replace('&amp;shy;', '', $entry['feed_ext']));
-        }
         break;
 }
 
