6f4a871016cee505847bbac04485c448d5e80156
Ian <1105372+ophian@users.noreply.github.com>
2024-10-13 14:33:57 +0200

    smarty API: Fix 'wysiwyg_comment' data variable conditional
    for case wysiwyg OFF with allowHtmlComment ON, which is not supported. While 'allowHtmlComment' is an additional feature/extension of using RichText editor.

--- include/functions_smarty.inc.php
+++ include/functions_smarty.inc.php
@@ -1176,7 +1176,7 @@ function serendipity_smarty_init($vars = array()) {
                 'template'                  => $serendipity['template'],
                 'templatePath'              => $serendipity['templatePath'],
                 'template_backend'          => $serendipity['template_backend'],
-                'wysiwyg_comment'           => $serendipity['allowHtmlComment'] ?? false,
+                'wysiwyg_comment'           => ($serendipity['allowHtmlComment'] && $serendipity['wysiwyg']) ?? false,
                 'use_autosave'              => (isset($serendipity['use_autosave']) && serendipity_db_bool($serendipity['use_autosave'])) ? 'true' : 'false',
 
                 'dateRange'                 => (!empty($serendipity['range']) ? $serendipity['range'] : array())
