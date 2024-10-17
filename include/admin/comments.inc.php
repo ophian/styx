de43200c8d4625d201446a21090967af790a9d59
Ian <1105372+ophian@users.noreply.github.com>
2024-10-17 11:35:16 +0200

    comments: Fix 7e721c65e9d8944f8a36945b12d7bc7998537c0b and 2b13864725573e1ebfd0e1c93d3027ffd696eb0b which missed this was a js snippet

--- include/admin/comments.inc.php
+++ include/admin/comments.inc.php
@@ -489,8 +489,6 @@ if (is_array($sql)) {
     }
 }
 
-$data['wysiwyg_comment'] = ($serendipity['allowHtmlComment'] && $serendipity['wysiwyg']) ? 'true' : 'false'; // for the codesnippet info redirector
-
 $data['comments']      = $comments;
 $data['errormsg']      = $errormsg;
 $data['msg']           = $msg;
