32034fe0950f1b33e64c51a4a0aacacdd3d8b1f6
Ian <1105372+ophian@users.noreply.github.com>
2024-10-31 10:59:30 +0100

    config: Release Serendipity Styx 4.5.0 version

--- serendipity_config.inc.php
+++ serendipity_config.inc.php
@@ -52,7 +52,7 @@ if (defined('USE_MEMSNAP')) {
 }
 
 // The version string
-$serendipity['version'] = '4.5-rc2';
+$serendipity['version'] = '4.5.0';
 $serendipity['edition'] = 'Styx';
 
 // Setting this to 'false' will enable debugging output.
