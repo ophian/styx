072f08685f9ed556747c97df92ede0af5365ec67
Ian <1105372+ophian@users.noreply.github.com>
2025-01-15 14:45:19 +0100

    smarty API: Refactor allowHtmlComment assignment for pre-install and normal condition
    Removes the wysiwyg check, since it is either or. Dont mix w/ admin wysiwyg variable.

--- include/functions_smarty.inc.php
+++ include/functions_smarty.inc.php
@@ -1258,8 +1258,7 @@ function serendipity_smarty_init(?iterable $vars = []) : bool  {
                 'template'                  => $serendipity['template'],
                 'templatePath'              => $serendipity['templatePath'],
                 'template_backend'          => $serendipity['template_backend'],
-                // on some systems a none set !isset() check isn't available and means to be an empty string '' ...
-                'wysiwyg_comment'           => (($serendipity['allowHtmlComment'] === true) && ((!isset($serendipity['wysiwyg']) || $serendipity['wysiwyg'] !== false)|| $serendipity['wysiwyg'] === true)),
+                'wysiwyg_comment'           => ($serendipity['allowHtmlComment'] === true),
                 'use_autosave'              => (isset($serendipity['use_autosave']) && serendipity_db_bool($serendipity['use_autosave'])) ? 'true' : 'false',
 
                 'dateRange'                 => (!empty($serendipity['range']) ? $serendipity['range'] : array())
