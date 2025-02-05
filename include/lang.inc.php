061b2bcb1a050437b3d73d1d7e82abc5792ad69f
Ian <1105372+ophian@users.noreply.github.com>
2025-02-05 13:28:56 +0100

    lang API: Simplify language encoding to UTF-8 only

--- include/lang.inc.php
+++ include/lang.inc.php
@@ -50,22 +50,11 @@ if (!defined('serendipity_LANG_LOADED') || serendipity_LANG_LOADED !== true) {
 if (!defined('serendipity_MB_LOADED') && defined('serendipity_LANG_LOADED')) {
     // Needs to be included here because we need access to constant LANG_CHARSET defined in languages (not available for compat.inc.php)
 
-    if (function_exists('mb_language')) {
-        // Normally mb_language() is used for encoding e-mail messages.
-        // Valid languages are "Japanese", "ja", "English", "en" and "uni" (UTF-8). mb_send_mail() uses this setting to encode e-mail.
-        // Language and its setting is: ISO-2022-JP/Base64 for Japanese, UTF-8/Base64 for uni, ISO-8859-1/quoted printable for English.
-        if (defined('LANG_CHARSET') && LANG_CHARSET == 'UTF-8') {
-            @mb_language('uni');
-        } else if ($serendipity['lang'] == 'ja') {
-            @mb_language($serendipity['lang']);
-        } else {
-            @mb_language('en');
-        }
-    }
-
-    if (function_exists('mb_internal_encoding')) {
-        @mb_internal_encoding(LANG_CHARSET);
-    }
+    // Normally mb_language() is used for encoding e-mail messages.
+    // Valid languages are "Japanese", "ja", "English", "en" and "uni" (UTF-8). mb_send_mail() uses this setting to encode e-mail.
+    // Language and its setting is: ISO-2022-JP/Base64 for Japanese, UTF-8/Base64 for uni, ISO-8859-1/quoted printable for English
+    @mb_language('uni');
+    @mb_internal_encoding(LANG_CHARSET);
 
     define('serendipity_MB_LOADED', true);
 }
