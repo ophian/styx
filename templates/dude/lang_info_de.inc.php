c5fe512b9446927fc32d4c6aa8ed8998f9677a90
Ian <1105372+ophian@users.noreply.github.com>
2025-04-21 17:20:51 +0200

    dude: Move UTF-8 lang files into root level
    Styx 5.0 task for all core plugins/themes, so all additional added plugins/themes shall keep it a little longer.

--- templates/dude/lang_info_de.inc.php
+++ templates/dude/lang_info_de.inc.php
@@ -1,11 +1,11 @@
 <?php
 /*
- * The info.txt charset (translation) file as an array.
+ * The info.txt UTF-8 charset (translation) file as an array.
  * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['lang'].
- * Convert or save as ANSI (ISO-8859-1) or your native charset
+ * Convert or save as UTF-8 file without BOM.
  **/
 
 $info['theme_info_summary'] = 'Modernes und ganzseitiges 2-Seitenleisten grid/flexible System Theme.';
 
-$info['theme_info_desc'] = 'Fast seitenfüllend, mit einer Inhaltsbreite von 96 Prozent. Und ohne zusätzliches Javascript, zB. für die Anpassung der Seitenleisten auf kleineren Geräten, oder einer globalen Navigationsleiste.
-Trotzdem voll Responsiv <u>und</u> Mobilgeräte freundlich! Ein höchst modernes Beispiel für die Macht von CSS. Schnell und leicht anpassbar.';
+$info['theme_info_desc'] = 'Fast seitenfÃ¼llend, mit einer Inhaltsbreite von 96 Prozent. Und ohne zusÃ¤tzliches Javascript, zB. fÃ¼r die Anpassung der Seitenleisten auf kleineren GerÃ¤ten, oder einer globalen Navigationsleiste.
+Trotzdem voll Responsiv <u>und</u> MobilgerÃ¤te freundlich! Ein hÃ¶chst modernes Beispiel fÃ¼r die Macht von CSS. Schnell und leicht anpassbar.';
