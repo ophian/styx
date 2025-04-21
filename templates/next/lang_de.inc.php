76d33abc652e285a241316b802e5ba8c74a43efc
Ian <1105372+ophian@users.noreply.github.com>
2025-04-21 17:21:16 +0200

    next: Move UTF-8 lang files into root level
    Styx 5.0 task for all core plugins/themes, so all additional added plugins/themes shall keep it a little longer.

--- templates/next/lang_de.inc.php
+++ templates/next/lang_de.inc.php
@@ -1,16 +1,16 @@
 <?php
 // Theme-Optionen
-@define('NEXT_INSTR', '<b>Hinweis:</b> Next mag <em>aussehen</em>, als sei es im Prinzip wie 2k11 - unter der Haube ist es (technisch gesehen) ein völlig anderes Theme. Es ist daher <strong>nicht</strong> kompatibel mit dem user.css-Generator für 2k11.<br>Um Next updatesicher um eigene Styles zu erweitern kann eine Datei <code>/templates/next/user.css</code> angelegt werden; diese wird automagisch durch Serendipity eingebunden.');
-@define('NEXT_HEADER_IMG','Bannergrafik im Kopfbereich einbinden? (1296 Pixel breit; Feld leeren um Ausgabe zu unterdrücken)');
+@define('NEXT_INSTR', '<b>Hinweis:</b> Next mag <em>aussehen</em>, als sei es im Prinzip wie 2k11 - unter der Haube ist es (technisch gesehen) ein vÃ¶llig anderes Theme. Es ist daher <strong>nicht</strong> kompatibel mit dem user.css-Generator fÃ¼r 2k11.<br>Um Next updatesicher um eigene Styles zu erweitern kann eine Datei <code>/templates/next/user.css</code> angelegt werden; diese wird automagisch durch Serendipity eingebunden.');
+@define('NEXT_HEADER_IMG','Bannergrafik im Kopfbereich einbinden? (1296 Pixel breit; Feld leeren um Ausgabe zu unterdrÃ¼cken)');
 @define('NEXT_USE_CORENAV', 'Globale Navigation einbinden?');
 @define('NEXT_WEBFONTS', 'Einen von Google gehosteten Webfont nutzen?');
 @define('NEXT_NOWEBFONT', 'Keinen Webfont einbinden');
 @define('NEXT_USERSTYLES', 'user.css einbinden? (Muss selbst im Themeverzeichnis angelegt werden!)');
-@define('NEXT_REFCOMMENTS', 'Link zum übergeordneten Kommentar hinzufügen, wenn der Kommentar eine Antwort ist?');
+@define('NEXT_REFCOMMENTS', 'Link zum Ã¼bergeordneten Kommentar hinzufÃ¼gen, wenn der Kommentar eine Antwort ist?');
 // Sprachkonstanten
 @define('NEXT_PLINK_TEXT', 'Link');
 @define('NEXT_PLINK_TITLE', 'Permanenter Link zu diesem Kommentar');
 @define('NEXT_PLACE_SEARCH', 'Suchbegriff(e)');
 @define('NEXT_SEND_MAIL', 'Senden');
 @define('NEXT_REPLYORIGIN', 'Antwort auf');
-@define('NEXT_NAVTEXT', 'Menü');
+@define('NEXT_NAVTEXT', 'MenÃ¼');
--- templates/next/lang_info_de.inc.php
+++ templates/next/lang_info_de.inc.php
@@ -1,11 +1,11 @@
 <?php
 /*
- * The info.txt charset (translation) file as an array.
+ * The info.txt UTF-8 charset (translation) file as an array.
  * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['lang'].
- * Convert or save as ANSI (ISO-8859-1) or your native charset
+ * Convert or save as UTF-8 file without BOM.
  **/
 
 $info['theme_info_summary'] = 'Ein "mobile first" agierendes "Nachfolge"-Template des 2k11 Frontends';
 
-$info['theme_info_desc'] = 'In Bezug auf die mehr oder weniger abgedeckten Möglichkeiten des vorherigen "Standard"-Themes "2k11", ist "Next" nur ein vereinfachtes Nachfolgemodell.
+$info['theme_info_desc'] = 'In Bezug auf die mehr oder weniger abgedeckten MÃ¶glichkeiten des vorherigen "Standard"-Themes "2k11", ist "Next" nur ein vereinfachtes Nachfolgemodell.
 Es ist nur <em>modernisiert</em> und <em>nachfolgend</em> im Sinne von Styling und Selektor Einstellungen.';
