e7b183337f7650130d1321a463c507b0616f6c06
Ian <1105372+ophian@users.noreply.github.com>
2025-04-21 17:19:23 +0200

    boot: Move UTF-8 lang files into root level
    Styx 5.0 task for all core plugins/themes, so all additional added plugins/themes shall keep it a little longer.

--- templates/boot/lang_de.inc.php
+++ templates/boot/lang_de.inc.php
@@ -1,5 +1,5 @@
 <?php
 @define('USE_CORENAV', 'Globale Navigation einbinden?');
-@define('BS_PLINK_TEXT', 'Link');
-@define('BS_PLINK_TITLE', 'Permanenter Link zu diesem Kommentar');
-@define('BS_REPLYORIGIN', 'Antwort auf');
+@define('B5_PLINK_TEXT', 'Link');
+@define('B5_PLINK_TITLE', 'Permanenter Link zu diesem Kommentar');
+@define('B5_REPLYORIGIN', 'Antwort auf');
