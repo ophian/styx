7db491c6ed3c88057e42e19dd2ef3dd0cad3c75e
Ian <1105372+ophian@users.noreply.github.com>
2025-04-21 17:20:06 +0200

    clean-blog: Move UTF-8 lang files into root level
    Styx 5.0 task for all core plugins/themes, so all additional added plugins/themes shall keep it a little longer.

--- templates/clean-blog/lang_de.inc.php
+++ templates/clean-blog/lang_de.inc.php
@@ -4,22 +4,22 @@
 @define('SEARCH_WHAT', 'Wonach soll gesucht werden?'); //used on quicksearch modal
 @define('SEARCH', 'Suche');
 @define('TOGGLE_NAV', 'Navigation'); //only seen by screen readers
-@define('CLOSE', 'Schließen'); //close button on search form
+@define('CLOSE', 'SchlieÃŸen'); //close button on search form
 @define('READ_MORE', 'Mehr lesen');
 
 //Option groups and instructions
 @define('THEME_HEADERS', 'Header-Bilder');
-@define('HEADERS_USE_VARIATION', 'Ausgabe als Bild-Variations Format, wenn erhältlich?');
+@define('HEADERS_USE_VARIATION', 'Ausgabe als Bild-Variations Format, wenn erhÃ¤ltlich?');
 @define('THEME_SOCIAL_LINKS', 'Social-Links');
 @define('THEME_PAGE_OPTIONS', 'Seiten-Optionen');
 @define('THEME_NAVIGATION', 'Navigations-Optionen');
 @define('THEME_README', 'Lies mich');
-@define('THEME_EP_YES', '<p class="msg_success msg-0">Das Plugin Erweiterte Eigenschaften für Einträge (serendipity_event_entryproperties) wird benötigt. Es ist installiert und aktiv.</p>');
-@define('THEME_EP_NO', '<p class="msg_error msg-0">Das Plugin Erweiterte Eigenschaften für Einträge (serendipity_event_entryproperties) wird benötigt. Es ist entweder nicht installiert oder inaktiv. Bitte das Plugin installieren, um alle Features dieses Themes voll zu nutzen.</p>');
-@define('THEME_INSTRUCTIONS', '<p>Dieses Theme nutzt breite Header-Bilder für unterschiedliche Seitentypen. Standardbilder werden mit dem Theme geliefert, diese sind 1900px breit. Diese Standardbilder können unten in den Einstellungen ausgetauscht werden. Zusätzlich zu den Standardbildern können für jeden Eintrag und jede statische Seite eigene Header-Bilder definiert werden.</p><p>Das Design dieses Theme sieht die Nutzung eines sekundären Untertitels oder einer Verfasserzeile direkt unter dem Eintragstitel vor. Dieser Untertitel und das Eintragsbild können im Abschnitt "Erweiterte Optionen" beim Anlegen oder Bearbeiten einer Seite geändert werden. Optional kann man das Theme so einstellen, dass die ersten 70 Zeichen des Eintrags als Untertitel verwendet werden, wenn kein Untertitel angegeben wurde.</p><p>Dieses Theme bietet keine Seitenleisten, kann jedoch optional die Seitenleisten-Plugins Kategorien und Tags in der Archiv-Seite ausgeben. Siehe "' . THEME_PAGE_OPTIONS . '" unten.</p><p>Falls das Avatar-Plugin (serendipity_event_gravatar) verwendet wird, sollte dessen Option "Smarty Tag erzeugen" für eine optimale Darstellung auf "Ja" gesetzt werden.</p><p>Die Option "Erweiteres Smarty" in serendipity_event_freetag sollte für eine schönere Darstellung im Eintragsfuß ebenfalls auf "Ja" gesetzt sein.</p>');
+@define('THEME_EP_YES', '<p class="msg_success msg-0">Das Plugin Erweiterte Eigenschaften fÃ¼r EintrÃ¤ge (serendipity_event_entryproperties) wird benÃ¶tigt. Es ist installiert und aktiv.</p>');
+@define('THEME_EP_NO', '<p class="msg_error msg-0">Das Plugin Erweiterte Eigenschaften fÃ¼r EintrÃ¤ge (serendipity_event_entryproperties) wird benÃ¶tigt. Es ist entweder nicht installiert oder inaktiv. Bitte das Plugin installieren, um alle Features dieses Themes voll zu nutzen.</p>');
+@define('THEME_INSTRUCTIONS', '<p>Dieses Theme nutzt breite Header-Bilder fÃ¼r unterschiedliche Seitentypen. Standardbilder werden mit dem Theme geliefert, diese sind 1900px breit. Diese Standardbilder kÃ¶nnen unten in den Einstellungen ausgetauscht werden. ZusÃ¤tzlich zu den Standardbildern kÃ¶nnen fÃ¼r jeden Eintrag und jede statische Seite eigene Header-Bilder definiert werden.</p><p>Das Design dieses Theme sieht die Nutzung eines sekundÃ¤ren Untertitels oder einer Verfasserzeile direkt unter dem Eintragstitel vor. Dieser Untertitel und das Eintragsbild kÃ¶nnen im Abschnitt "Erweiterte Optionen" beim Anlegen oder Bearbeiten einer Seite geÃ¤ndert werden. Optional kann man das Theme so einstellen, dass die ersten 70 Zeichen des Eintrags als Untertitel verwendet werden, wenn kein Untertitel angegeben wurde.</p><p>Dieses Theme bietet keine Seitenleisten, kann jedoch optional die Seitenleisten-Plugins Kategorien und Tags in der Archiv-Seite ausgeben. Siehe "' . THEME_PAGE_OPTIONS . '" unten.</p><p>Falls das Avatar-Plugin (serendipity_event_gravatar) verwendet wird, sollte dessen Option "Smarty Tag erzeugen" fÃ¼r eine optimale Darstellung auf "Ja" gesetzt werden.</p><p>Die Option "Erweiteres Smarty" in serendipity_event_freetag sollte fÃ¼r eine schÃ¶nere Darstellung im EintragsfuÃŸ ebenfalls auf "Ja" gesetzt sein.</p>');
 @define('THEME_ENTRY_PROPERTIES_HEADING', 'Eintragseigenschaften des Clean Blog-Themes');
-@define('THEME_CUSTOM_FIELD_HEADING', 'Freie Felder für Einträge');
-@define('THEME_CUSTOM_FIELD_DEFINITION', 'Diese optionalen Felder sind nur verfügbar, wenn das Theme (clean blog) verwendet wird. Das Ereignis-Plugin serendipity_event_entryproperties (Erweiterte Eigenschaften für Einträge) muss installiert sein, um diese Felder zu verwenden. Der Eintrags-Untertitel wird unter dem Titel des Eintrags ausgegeben und sollte eine kurze Zusammenfassung des Eintrags enthalten. Das Header-Bild eines Eintrags überschreibt das Standard-Header-Bild für Einträge.');
+@define('THEME_CUSTOM_FIELD_HEADING', 'Freie Felder fÃ¼r EintrÃ¤ge');
+@define('THEME_CUSTOM_FIELD_DEFINITION', 'Diese optionalen Felder sind nur verfÃ¼gbar, wenn das Theme (clean blog) verwendet wird. Das Ereignis-Plugin serendipity_event_entryproperties (Erweiterte Eigenschaften fÃ¼r EintrÃ¤ge) muss installiert sein, um diese Felder zu verwenden. Der Eintrags-Untertitel wird unter dem Titel des Eintrags ausgegeben und sollte eine kurze Zusammenfassung des Eintrags enthalten. Das Header-Bild eines Eintrags Ã¼berschreibt das Standard-Header-Bild fÃ¼r EintrÃ¤ge.');
 @define('THEME_ENTRY_SUBTITLE', 'Untertitel des Eintrags');
 @define('THEME_ENTRY_HEADER_IMAGE', 'Header-Bild der Eintrags');
 @define('THEME_DEMO_AVAILBLE', 'Eine komplette englische Anleitung zur <a href="http://www.optional-necessity.com/demo/cleanblog/archives/8-Using-the-Clean-Blog-theme.html">Konfiguration und Benutzung von Clean Blog</a> findet sich in der <a href="http://www.optional-necessity.com/demo/cleanblog/">Clean Blog-Demo</a>.');
@@ -27,19 +27,19 @@
 // Header Images
 @define('DEFAULT_HEADER_IMAGE', 'Standard-Header-Bild');
 @define('DEFAULT_HEADER_IMAGE_DESC', 'Dieses Bild wird auf Seiten verwendet, die kein eigenes Header-Bild definieren.');
-@define('ENTRY_DEFAULT_HEADER_IMAGE', 'Standard-Header-Bild für Einträge');
-@define('ENTRY_DEFAULT_HEADER_IMAGE_DESC', 'Dieses Bild wird für Einträge verwendet, die kein eigenes Header-Bild definieren.');
-@define('STATICPAGE_DEFAULT_HEADER_IMAGE', 'Standard-Header-Bild für statische Seiten');
-@define('STATICPAGE_DEFAULT_HEADER_IMAGE_DESC', 'Dieses Bild wird für statische Seiten verwendet, die kein eigenes Header-Bild definieren.');
-@define('STATICPAGE_SPECIFIC_HEADER_IMAGE', 'Header-Bild für statische Seiten (optional)');
-@define('CONTACTFORM_HEADER_IMAGE', 'Header-Bild für das Kontaktformular');
-@define('ARCHIVE_HEADER_IMAGE', 'Header-Bild für die Archivseite');
+@define('ENTRY_DEFAULT_HEADER_IMAGE', 'Standard-Header-Bild fÃ¼r EintrÃ¤ge');
+@define('ENTRY_DEFAULT_HEADER_IMAGE_DESC', 'Dieses Bild wird fÃ¼r EintrÃ¤ge verwendet, die kein eigenes Header-Bild definieren.');
+@define('STATICPAGE_DEFAULT_HEADER_IMAGE', 'Standard-Header-Bild fÃ¼r statische Seiten');
+@define('STATICPAGE_DEFAULT_HEADER_IMAGE_DESC', 'Dieses Bild wird fÃ¼r statische Seiten verwendet, die kein eigenes Header-Bild definieren.');
+@define('STATICPAGE_SPECIFIC_HEADER_IMAGE', 'Header-Bild fÃ¼r statische Seiten (optional)');
+@define('CONTACTFORM_HEADER_IMAGE', 'Header-Bild fÃ¼r das Kontaktformular');
+@define('ARCHIVE_HEADER_IMAGE', 'Header-Bild fÃ¼r die Archivseite');
 
 //Page Options
 @define('USE_GOOGLEFONTS', 'Google Webfonts einbinden?');
 @define('HOME_LINK_TEXT', 'Text des Home-Links');
-@define('ENTRY_DATE_FORMAT', 'Datumsformat für Einträge');
-@define('COMMENT_TIME_FORMAT', 'Zeitformat für Kommentare und Trackbacks');
+@define('ENTRY_DATE_FORMAT', 'Datumsformat fÃ¼r EintrÃ¤ge');
+@define('COMMENT_TIME_FORMAT', 'Zeitformat fÃ¼r Kommentare und Trackbacks');
 @define('WORDS', 'Textfassung');
 @define('TIMESTAMP', 'Zeitstempel');
 @define('SUBTITLE_USE_ENTRYBODY', 'Zeige die ersten 70 Zeichen des Eintrages als Vorschau wenn Eintrags-Eigenschaften Untertitel nicht leer');
@@ -75,7 +75,7 @@
 @define('ELAPSED_DAYS_AGO', 'Vor %s Tagen');
 @define('ELAPSED_MONTHS_AGO', 'Vor %s Monaten');
 @define('ELAPSED_YEARS_AGO', 'Vor %s Jahren'); //not currently using this, but defining just in case
-@define('ELAPSED_ABOUT_ONE_HOUR_AGO', 'Vor ungefähr einer Stunde'); // greater than 45 minutes, less than 90 minutes
-@define('ELAPSED_ABOUT_ONE_MONTH_AGO', 'Vor ungefähr einem Monat'); // greater than 30 days, less than 60 days
-@define('ELAPSED_ABOUT_ONE_YEAR_AGO', 'Vor ungefähr einem Jahr'); // greater than one year, less than 2 years
+@define('ELAPSED_ABOUT_ONE_HOUR_AGO', 'Vor ungefÃ¤hr einer Stunde'); // greater than 45 minutes, less than 90 minutes
+@define('ELAPSED_ABOUT_ONE_MONTH_AGO', 'Vor ungefÃ¤hr einem Monat'); // greater than 30 days, less than 60 days
+@define('ELAPSED_ABOUT_ONE_YEAR_AGO', 'Vor ungefÃ¤hr einem Jahr'); // greater than one year, less than 2 years
 @define('ELAPSED_OVER_YEARS_AGO', 'Vor mehr als %s Jahren');// greater than 2 years
