b5c9745a37a64a1c47083c9717f1754941fda160
Ian <1105372+ophian@users.noreply.github.com>
2025-04-21 17:21:49 +0200

    pure: Move UTF-8 lang files into root level
    Styx 5.0 task for all core plugins/themes, so all additional added plugins/themes shall keep it a little longer.

--- templates/pure/UTF-8/lang_de.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-@define('USE_CORENAV', 'Globale Navigation einbinden?');
-@define('NAV_MENU', 'navigation');
-@define('PURE_PLINK_TEXT', 'Link');
-@define('PURE_PLINK_TITLE', 'Permanenter Link zu diesem Kommentar');
-@define('PURE_REPLYORIGIN', 'Antwort auf');
-@define('PURE_USE_HIGHLIGHT', 'Lade code highlight js in der Eintrags Liste');
-@define('PURE_USE_HIGHLIGHT_DESC', 'Normalerweise wird dieses Javascript nur auf Einzelseiten für die Hervorhebung von code Beispielen in Kommentaren geladen. Sollten Sie aber solche auch in Ihren Einträgen verwenden, möchten Sie es vielleicht auch in der Eintragsliste aktiv geladen haben.');
--- templates/pure/lang_info_de.inc.php
+++ templates/pure/lang_info_de.inc.php
@@ -1,25 +1,25 @@
 <?php
 /*
- * The info.txt charset (translation) file as an array.
+ * The info.txt UTF-8 charset (translation) file as an array.
  * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['lang'].
- * Convert or save as ANSI (ISO-8859-1) or your native charset
+ * Convert or save as UTF-8 file without BOM.
  **/
 
 $info['theme_info_summary'] = 'Das Styx Frontend Standard Theme (ab 2020). Ein ganzseitiges modernes Grid/Flexing Theme im 3-2-1-Spalten Modus.';
 
 $info['theme_info_desc'] = 'Dieses Theme definiert den Standard seit Styx 3.0, der im Kern definiert und benutzt wird.
-In Bezug auf die schier unendlichen Möglichkeiten von Serendipity, ist es wohl mit das am besten verknüpfte und verzahnte Theme,
-welches die meisten dieser Möglichkeiten in einfacher Weise innerhalb des eigenen Rahmens abdeckt, so dass man diese leicht durchschauen kann.<br>
-Ein "Heilige Gral" moderner Theme HTML5 Strukturen. Strukturell voll responsiv ("reagierend"), mit automatischer Anpassung der Schriftgrößen <u>und</u> Mobilgeräte freundlich!<br>
+In Bezug auf die schier unendlichen MÃ¶glichkeiten von Serendipity, ist es wohl mit das am besten verknÃ¼pfte und verzahnte Theme,
+welches die meisten dieser MÃ¶glichkeiten in einfacher Weise innerhalb des eigenen Rahmens abdeckt, so dass man diese leicht durchschauen kann.<br>
+Ein "Heilige Gral" moderner Theme HTML5 Strukturen. Strukturell voll responsiv ("reagierend"), mit automatischer Anpassung der SchriftgrÃ¶ÃŸen <u>und</u> MobilgerÃ¤te freundlich!<br>
 <br>
 <u><b>Eigenes Theme erstellen</b></u><br>
-Kopieren Sie ein vorhandenes Theme oder fügen Sie einen neuen und eindeutigen Verzeichnisnamen in das Verzeichnis "templates/" ein, zB. "glory".
-Fügen Sie eine <b>info.txt</b> Datei mit den Komponenten von Pure hinzu, um damit zu beginnen.
-Ändern Sie den Namen, zB. zu "Johns Glory" und fügen Sie das aktuelle Datum als "jjjj-mm-tt" hinzu.<br>
-Wenn Sie Ihr neues Theme auf Pure (2020) zurückgreifen lassen wollen, geben Sie der info.txt Datei nun eine "Engine&colon; Pure"-Zeile hinzu, zB. für ein reines CSS Kind-Theme.
-Wenn Sie diese Zeile nicht nutzen, muss das eigene Theme entweder alle Template-Dateien selbst enthalten, oder Ihr Theme wird auf die "Serendipity Default" Template Dateien zurückgreifen.<br>
+Kopieren Sie ein vorhandenes Theme oder fÃ¼gen Sie einen neuen und eindeutigen Verzeichnisnamen in das Verzeichnis "templates/" ein, zB. "glory".
+FÃ¼gen Sie eine <b>info.txt</b> Datei mit den Komponenten von Pure hinzu, um damit zu beginnen.
+Ã„ndern Sie den Namen, zB. zu "Johns Glory" und fÃ¼gen Sie das aktuelle Datum als "jjjj-mm-tt" hinzu.<br>
+Wenn Sie Ihr neues Theme auf Pure (2020) zurÃ¼ckgreifen lassen wollen, geben Sie der info.txt Datei nun eine "Engine&colon; Pure"-Zeile hinzu, zB. fÃ¼r ein reines CSS Kind-Theme.
+Wenn Sie diese Zeile nicht nutzen, muss das eigene Theme entweder alle Template-Dateien selbst enthalten, oder Ihr Theme wird auf die "Serendipity Default" Template Dateien zurÃ¼ckgreifen.<br>
 <br>
-Setzen Sie entsprechende Zeilen "Require Serendipity&colon; 3.0" für eine Voraussetzung, "Backend&colon; Yes",
-wenn Sie ein eigenes Backend nutzen und "Responsive&colon; Yes" und "Mobile&colon; Yes", wenn es den heutigen Ansprüchen für responsives und mobiles Websitedesign genügt.<br>
+Setzen Sie entsprechende Zeilen "Require Serendipity&colon; 3.0" fÃ¼r eine Voraussetzung, "Backend&colon; Yes",
+wenn Sie ein eigenes Backend nutzen und "Responsive&colon; Yes" und "Mobile&colon; Yes", wenn es den heutigen AnsprÃ¼chen fÃ¼r responsives und mobiles Websitedesign genÃ¼gt.<br>
 <br>
-Zurück in der Template-Liste, laden Sie die Seite neu und wählen Sie Ihr neues Theme anhand des angegebenen Namens.';
+ZurÃ¼ck in der Template-Liste, laden Sie die Seite neu und wÃ¤hlen Sie Ihr neues Theme anhand des angegebenen Namens.';
