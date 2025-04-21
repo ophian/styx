10b9a571412e0dc6b02dcc4cbe25ab324969c007
Ian <1105372+ophian@users.noreply.github.com>
2025-04-21 17:22:19 +0200

    skeleton: Move UTF-8 lang files into root level
    Styx 5.0 task for all core plugins/themes, so all additional added plugins/themes shall keep it a little longer.

--- templates/skeleton/UTF-8/lang_de.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-@define('USE_CORENAV', 'Globale Navigation einbinden?');
-@define('USE_GOOGLEFONTS', 'Google Webfonts einbinden?');
-@define('SKELETON_PLACE_SEARCH', 'Suchbegriff(e)');
--- templates/skeleton/lang_info_de.inc.php
+++ templates/skeleton/lang_info_de.inc.php
@@ -1,11 +1,11 @@
 <?php
 /*
- * The info.txt charset (translation) file as an array.
+ * The info.txt UTF-8 charset (translation) file as an array.
  * Save as lang_info_xx.inc.php and replace 'xx' with your short lang term, defined and set in $serendipity['lang'].
- * Convert or save as ANSI (ISO-8859-1) or your native charset
+ * Convert or save as UTF-8 file without BOM.
  **/
 
 $info['theme_info_summary'] = 'Ein CSS Theme basierend auf dem CSS Framework Skeleton http://getskeleton.com.';
 
 $info['theme_info_desc'] = 'Skeleton ist schlicht & einfach, eher typografisch orientiert.
-Das Theme nutzt HTML5 und ist Responsiv. Es besitzt nur eine einzige Seitenleiste (auf der rechten Seite) für größere Bildschirmgrößen und verwendet den Kern Navigation.';
+Das Theme nutzt HTML5 und ist Responsiv. Es besitzt nur eine einzige Seitenleiste (auf der rechten Seite) fÃ¼r grÃ¶ÃŸere BildschirmgrÃ¶ÃŸen und verwendet den Kern Navigation.';
