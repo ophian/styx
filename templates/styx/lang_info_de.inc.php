444a0bd5d2491099dc187045eb274d33bd9c9f24
Ian <1105372+ophian@users.noreply.github.com>
2025-07-18 19:37:16 +0200

    styx: Fix lang [de] typo

--- templates/styx/lang_info_de.inc.php
+++ templates/styx/lang_info_de.inc.php
@@ -21,7 +21,7 @@ Dieses Standard Backend verfügt über eigene stylesheet Auszeichnungs-Dateien, 
 Dieses Theme beherbergt die Styx-Kern Backend Templates im Unterverzeichnis "default/admin".
 Die Template-Dateien in diesem Verzeichnis bilden und erstellen das Aussehen der kompletten Admin-Oberfläche.
 Sie enthalten auch einige Workflow- und Logikfunktionen sowie eigene Javascript-Bibliotheken.
-Wenn Sie ein eigenes Backend-Theme verwenden möchten, kopieren Sie das Verzeichnis "admin" in ihr Theme.
+Wenn Sie ein eigenes Backend-Theme verwenden möchten, kopieren Sie das Verzeichnis "admin" in Ihr Theme.
 Ändern Sie dort die Datei "info.txt", um eine Zeile "Backend&colon; Yes" hinzuzufügen, und wählen Sie das neue Backend-Theme in der neu geladenen Themenliste aus.
 Ab sofort können Sie die Dateien und Stile des eigenen Backend-Themes bearbeiten und an Ihre erweiterten Bedürfnisse anpassen.<br>
 <u><b>Bitte beachten Sie:</b></u> Dies ist nur für erfahrene Benutzer empfehlenswert und komplett abseits der Update-Funktionen!</p>';
