6a7962457e50d2755d484b75cacd9ebe41aa2e34
Ian <1105372+ophian@users.noreply.github.com>
2025-11-28 19:47:25 +0100

    spamblock: Fix a possible frontend 'frontend_comment' error on NULL with strtolower()
    most probably the $_SESSION var...

--- plugins/serendipity_event_spamblock/serendipity_event_spamblock.php
+++ plugins/serendipity_event_spamblock/serendipity_event_spamblock.php
@@ -30,7 +30,7 @@ class serendipity_event_spamblock extends serendipity_event
             'smarty'      => '4.1',
             'php'         => '8.2'
         ));
-        $propbag->add('version',       '2.91');
+        $propbag->add('version',       '2.92');
         $propbag->add('event_hooks',    array(
             'frontend_saveComment' => true,
             'external_plugin'      => true,
@@ -1399,7 +1399,7 @@ class serendipity_event_spamblock extends serendipity_event
                     if ($_show_captcha) {
                         $selector_id = 'captcha_'.$unique_sid; // for possible adduser form on main page and in sidebar
                         echo '                                <div class="serendipity_commentDirection serendipity_comment_captcha">'."\n";
-                        if (!isset($serendipity['POST']['preview']) || strtolower($serendipity['POST']['captcha']) != @strtolower($_SESSION['spamblock']['captcha'])) {
+                        if (!isset($serendipity['POST']['preview']) || strtolower($serendipity['POST']['captcha'] ?? '') != @strtolower($_SESSION['spamblock']['captcha'] ?? '')) {
                             echo '                                    ' . PLUGIN_EVENT_SPAMBLOCK_CAPTCHAS_USERDESC . "<br />\n";
                             echo '                                    ' . $this->show_captcha($use_gd) . "<br />\n";
                             echo '                                    <label for="'.$selector_id.'">'. PLUGIN_EVENT_SPAMBLOCK_CAPTCHAS_USERDESC3 . "</label>\n";
