afd9485d685099662bf6b120b80b56b4ab52975c
Ian <1105372+ophian@users.noreply.github.com>
2026-01-02 20:04:29 +0100

    images API: Fix _getImageSize() for empty case AVIF getimagesize($file)

--- include/functions_images.inc.php
+++ include/functions_images.inc.php
@@ -4257,7 +4257,7 @@ function serendipity_getImageSize(string $file, string $ft_mime = '', string $su
     } else if ($ft_mime == 'text/calendar') { // see compat '@getimagesize.*Error reading from@i' exception for forced debug bypassed error processing because it's @-silenced.
          $fdim = '';
     } else {
-        $fdim = @getimagesize($file);
+        $fdim = !empty($file) ? @getimagesize($file) : $file; // $file (AVIF) may be empty if too big by restriction and fail with error 500
     }
 
     if (is_array($fdim)) {
