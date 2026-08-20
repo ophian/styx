68fcf0e8c4b981e1be98045bb6fcc969fd749631
Ian <1105372+ophian@users.noreply.github.com>
2026-08-20 18:08:57 +0200

    pure: Set constants for 319a40300661cdb9df82349274fdc46521c68aaa

--- templates/pure/lang_de.inc.php
+++ templates/pure/lang_de.inc.php
@@ -6,3 +6,7 @@
 @define('PURE_REPLYORIGIN', 'Antwort auf');
 @define('PURE_USE_HIGHLIGHT', 'Lade code highlight js in der Eintrags Liste');
 @define('PURE_USE_HIGHLIGHT_DESC', 'Normalerweise wird dieses Javascript nur auf Einzelseiten für die Hervorhebung von code Beispielen in Kommentaren geladen. Sollten Sie aber solche auch in Ihren Einträgen verwenden, möchten Sie es vielleicht auch in der Eintragsliste aktiv geladen haben.');
+
+@define('PURE_USE_FLOATBOX', 'Erlaube "Quick Access Panel" float box');
+@define('PURE_USE_FLOATBOX_DESC', 'Deaktivieren Sie diese Funktion, um das Eintrags-Floater-Fenster auszublenden, das immer unter dem letzten Element der Seitenleiste platziert ist und schnellen Zugriff auf verschiedene Bereiche bietet, wie z. B. „Weiter“, „Zurück“, „Oben“, „Unten“ und „Jeder Eintrag“ usw. Sie werden dieses Feld als nützliche Hilfe zu schätzen wissen, wenn Sie viele lange Beiträge verfassen. Per Design ist dies nur zugänglich, wenn Sie mit Desktop Bildschirmgrößen über 1024px arbeiten.');
+
--- templates/pure/lang_en.inc.php
+++ templates/pure/lang_en.inc.php
@@ -7,3 +7,7 @@
 @define('PURE_USE_HIGHLIGHT', 'Load code highlight js for entries list');
 @define('PURE_USE_HIGHLIGHT_DESC', 'Normally this will automatically load only in single entry views for comments, but if you have code snips in your entries you might want to allow this within entries too.');
 @define('PURE_PLACE_SEARCH', 'Search term(s)');
+
+@define('PURE_USE_FLOATBOX', 'Allow “Quick Access Panel” float box');
+@define('PURE_USE_FLOATBOX_DESC', 'Disable this feature to hide the entry floater window, which is sticky positioned underneath the last item of the sidebar and provides quick access to various sections, such as “Next”, “Back”, “Top”, “Bottom”, and “Each Entry”, etc. You\’ll find this feature particularly helpful when writing many long posts. Per design this works only on screen viewports over 1024px, like desktops.');
+
